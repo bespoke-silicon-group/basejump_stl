89f0f6997b476d8e3747eeb927fc95b3451f640c
luzh <luzh@ucsd.edu>
2013-01-15 20:39:00 -0800

    Change naming conventions in testbench

--- config_net/config_net_tb.v
+++ config_net/config_net_tb.v
@@ -11,38 +11,38 @@ module config_net_tb;
 
   `define input_vec_bits   36
   `define input_vec_init   `input_vec_bits'b1_1000_00000111_1100_0_111111111111111110
-  //                                          data       id  len v         reset
+  //                                          data       id  len v             reset
 
-  reg                             clk_i;
-  reg  [`input_vec_bits - 1 : 0]  input_vec;
-  reg                             bit_i;
-  wire                            clk_o_0;
-  wire                            bit_o_0;
-  wire [`tb_data_bits_p - 1 : 0]  config_data;
+  reg                             tb_clk_i;
+  reg  [`input_vec_bits - 1 : 0]  tb_input_vec;
+  reg                             tb_bit_i;
+  wire                            tb_clk_o_0;
+  wire                            tb_bit_o_0;
+  wire [`tb_data_bits_p - 1 : 0]  tb_data_o;
 
   config_node     #(.id_p(`tb_id_p),
                     .data_bits_p(`tb_data_bits_p),
                     .default_p(`tb_default_p) )
-    config_node_dut(.clk_i(clk_i),
-                    .bit_i(bit_i),
-                    .data_o(config_data),
-                    .bit_o(bit_o_0),
-                    .clk_o(clk_o_0) );
+    config_node_dut(.clk_i(tb_clk_i),
+                    .bit_i(tb_bit_i),
+                    .data_o(tb_data_o),
+                    .bit_o(tb_bit_o_0),
+                    .clk_o(tb_clk_o_0) );
   initial begin
-    clk_i = 1;
-    bit_i = 1;
-    input_vec = 0;
-    #1 input_vec = `input_vec_init;
-    #2 bit_i = input_vec[0];
+    tb_clk_i = 1;
+    tb_bit_i = 1;
+    tb_input_vec = 0;
+    #1 tb_input_vec = `input_vec_init;
+    #2 tb_bit_i = tb_input_vec[0];
   end
 
   always #5 begin
-    clk_i = !clk_i; // flip clock every 5 ns, period 10 ns
+    tb_clk_i = ~tb_clk_i; // flip clock every 5 ns, period 10 ns
   end
 
-  always @ (posedge clk_i) begin
-    input_vec = {1'b0, input_vec[`input_vec_bits - 1 : 1]};
-    bit_i = input_vec[0];
+  always @ (posedge tb_clk_i) begin
+    tb_input_vec = {1'b0, tb_input_vec[`input_vec_bits - 1 : 1]};
+    tb_bit_i = tb_input_vec[0];
   end
 
   initial begin
