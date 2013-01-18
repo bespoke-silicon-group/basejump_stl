dec9c7c2c7c38e94c24b8704d04d494aba502b5b
luzh <luzh@ucsd.edu>
2013-01-17 16:24:12 -0800

    Use localparam instead of define

--- config_net/config_node.v
+++ config_net/config_node.v
@@ -12,36 +12,34 @@ module config_node
    );
 
   // local parameters same for all nodes in the configuration chain
-  `define len_width_c       8     // number of bits to represent #bits in the configuration packet, excluding the valid bit
-  `define id_width_c        8     // number of bits to represent the ID of a node, should be able to keep the max ID in the whole chain
+  localparam id_width_lp        = 8;    // number of bits to represent the ID of a node, should be able to keep the max ID in the whole chain
+  localparam len_width_lp       = 8;    // number of bits to represent #bits in the configuration packet, excluding the valid bit
 
-  `define data_frame_len_c  8     // bit '0' is inserted every data_frame_len_c in data bits
-  `define data_packet_len_c (data_bits_p + (data_bits_p / `data_frame_len_c) + 1)
-                                  // +1 means the end, or msb of a data_packet is always framing bit '0'
-                                  // if data_bits_p is a multiple of data_frame_len_c, "00" is expected at the end of a packet
+  localparam frame_bit_size_lp  = 1;    // 
+  localparam data_frame_len_lp  = 8;    // bit '0' is inserted every data_frame_len_lp in data bits
+  localparam data_packet_len_lp = (data_bits_p + (data_bits_p / data_frame_len_lp) + frame_bit_size_lp);
+                                        // + frame_bit_size_lp means the end, or msb of a data_packet is always framing bits
+                                        // if data_bits_p is a multiple of data_frame_len_lp, "00" is expected at the end of a packet
 
-  //                                           0                 0                  0
-  `define shift_width_c  (`data_packet_len_c + 1 + `id_width_c + 1 + `len_width_c + 1) // shift register width of this node
+  localparam shift_width_lp     =  (data_packet_len_lp + frame_bit_size_lp + id_width_lp + frame_bit_size_lp + len_width_lp + frame_bit_size_lp); // shift register width of this node
 
-
-  logic [`shift_width_c - 1 : 0] shift_n, shift_r;
-  logic [`id_width_c - 1 : 0]    node_id;
+  logic [shift_width_lp - 1 : 0] shift_n, shift_r;
+  logic [id_width_lp - 1 : 0]    node_id;
   logic                          reset;
-
   logic                          valid;
   logic                          match;
   logic                          data_en;
 
-  logic [`len_width_c - 1 : 0] packet_len;
-  logic [`len_width_c - 1 : 0] count_n, count_r;
+  logic [len_width_lp - 1 : 0] packet_len;
+  logic [len_width_lp - 1 : 0] count_n, count_r;
   logic                        count_non_zero;
 
-  logic [`data_packet_len_c - 1 : 0] data_packet;
+  logic [data_packet_len_lp - 1 : 0] data_packet;
   logic [data_bits_p - 1 : 0] data_n, data_r;
 
 
-  assign count_n = (valid == 1) ? packet_len : ((count_non_zero == 1) ? (count_r - 1) : count_r);
-  assign shift_n = {bit_i, shift_r[`shift_width_c - 1 : 1]};
+  assign count_n = (valid) ? packet_len : ((count_non_zero) ? (count_r - 1) : count_r);
+  assign shift_n = {bit_i, shift_r[shift_width_lp - 1 : 1]};
 
   always_ff @ (posedge clk_i) begin
     if (reset) begin
@@ -57,22 +55,22 @@ module config_node
   end
 
 
-  assign reset = & shift_r[1 + `id_width_c + 1 + `len_width_c + 1 - 1 : 0];
-  assign valid = (count_non_zero == 0) ? (~shift_r[0]) : 1'b0;
-  assign packet_len = shift_r[`len_width_c + 1 - 1 : 1];
-  assign node_id = shift_r[`id_width_c + 1 + `len_width_c + 1 - 1 : 1 + `len_width_c + 1];
-  assign data_packet = shift_r[`shift_width_c - 1 : 1 + `id_width_c + 1 + `len_width_c + 1];
+  assign reset = & shift_r[frame_bit_size_lp + id_width_lp + frame_bit_size_lp + len_width_lp + frame_bit_size_lp - 1 : 0];
+  assign valid = (~count_non_zero) ? (~shift_r[0]) : 1'b0;
+  assign packet_len = shift_r[len_width_lp + frame_bit_size_lp - 1 : frame_bit_size_lp];
+  assign node_id = shift_r[id_width_lp + frame_bit_size_lp + len_width_lp + frame_bit_size_lp - 1 : frame_bit_size_lp + len_width_lp + frame_bit_size_lp];
+  assign data_packet = shift_r[shift_width_lp - 1 : frame_bit_size_lp + id_width_lp + frame_bit_size_lp + len_width_lp + frame_bit_size_lp];
 
   genvar i;
   generate
-    for(i = 0; i < `data_packet_len_c - 1; i++) begin // the end, or msb of a data_packet is always '0' which is discarded
-      if((i + 1) % (`data_frame_len_c + 1)) begin
-        assign data_n[i - i / (`data_frame_len_c + 1)] = data_packet[i];
+    for(i = 0; i < data_packet_len_lp - frame_bit_size_lp; i++) begin // the end, or msb of a data_packet is always '0' which is discarded
+      if((i + 1) % (data_frame_len_lp + frame_bit_size_lp)) begin
+        assign data_n[i - i / (data_frame_len_lp + frame_bit_size_lp)] = data_packet[i];
       end
     end
   endgenerate
 
-  assign match = (node_id == id_p) ? 1'b1 : 1'b0;
+  assign match = node_id == id_p;
   assign data_en = valid & match;
   assign count_non_zero = | count_r;
 
@@ -81,7 +79,7 @@ module config_node
 
 //synopsys translate_off
   initial begin
-    $display("\t\tshift_width_c = %d\n", `shift_width_c);
+    $display("\t\tshift_width_lp = %d\n", shift_width_lp);
     $display("\t\t\t\ttime, \tclk_i, \tbit_i, \tshift_r, \treset, \tvalid, \tpacket_len, \tnode_id, \tdata_n, \tmatch, \tdata_en, \tdata_r, \tcount_r");
     $monitor("%d,   \t %b, \t  %b, \t  %b,  \t  %b, \t  %b, \t  %b, \t  %d,     \t  %d,  \t %b, \t %b,   \t  %b, \t  %b, \t   %d",
              $time, clk_i, bit_i,  shift_r, bit_o,  reset,  valid,  packet_len, node_id, match, data_en, data_n, data_r, count_r);
