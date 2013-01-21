3a1ff33155c601cf570dcce3f093d58db763c7a7
luzh <luzh@ucsd.edu>
2013-01-20 16:52:56 -0800

    Used node_packet_t struct in config_node.v

--- config_net/config_node.v
+++ config_net/config_node.v
@@ -12,29 +12,39 @@ module config_node
    );
 
   // local parameters same for all nodes in the configuration chain
-  localparam frame_bit_size_lp  = 1;    // 
-  localparam data_frame_len_lp  = 8;    // bit '0' is inserted every data_frame_len_lp in data bits
-  localparam id_width_lp        = data_frame_len_lp;  // number of bits to represent the ID of a node, should be able to keep the max ID in the whole chain
-  localparam len_width_lp       = data_frame_len_lp;  // number of bits to represent #bits in the configuration packet, excluding the valid bit
+  localparam frame_bit_size_lp  = 1;  // 
+  localparam data_frame_len_lp  = 8;  // bit '0' is inserted every data_frame_len_lp in data bits
+  localparam id_width_lp        = 8;  // number of bits to represent the ID of a node, should be able to keep the max ID in the whole chain
+  localparam len_width_lp       = 8;  // number of bits to represent #bits in the configuration packet, excluding the valid bit
 
-  localparam data_packet_len_lp = (data_bits_p + (data_bits_p / data_frame_len_lp) + frame_bit_size_lp);
-                                        // + frame_bit_size_lp means the end, or msb of a data_packet is always framing bits
-                                        // if data_bits_p is a multiple of data_frame_len_lp, "00" is expected at the end of a packet
+  localparam data_rx_len_lp     = (data_bits_p + (data_bits_p / data_frame_len_lp) + frame_bit_size_lp);
+                                     // + frame_bit_size_lp means the end, or msb of received data is always framing bits
+                                     // if data_bits_p is a multiple of data_frame_len_lp, "00" is expected at the end of received data
 
-  localparam shift_width_lp     = (data_packet_len_lp + frame_bit_size_lp + id_width_lp + frame_bit_size_lp + len_width_lp + frame_bit_size_lp); // shift register width of this node
+  localparam shift_width_lp     = (data_rx_len_lp + frame_bit_size_lp + id_width_lp + frame_bit_size_lp + len_width_lp + frame_bit_size_lp);
+                                     // shift register width of this node
 
-  logic [shift_width_lp - 1 : 0] shift_n, shift_r;
+  typedef struct packed {
+    bit [data_rx_len_lp - 1 : 0]       rx;
+    bit                                f1;
+    bit [id_width_lp - 1 : 0]          id;
+    bit [frame_bit_size_lp - 1 : 0]    f0;
+    bit [len_width_lp - 1 : 0]        len;
+    bit                             valid;
+  } node_packet_t;
+
+  node_packet_t shift_n, shift_r;
   logic [id_width_lp - 1 : 0]    node_id;
   logic                          reset;
   logic                          valid;
   logic                          match;
   logic                          data_en;
 
-  logic [len_width_lp - 1 : 0] packet_len;
+  logic [len_width_lp - 1 : 0] packet_len; // This packet length does NOT count the valid bit ==>
   logic [len_width_lp - 1 : 0] count_n, count_r;
   logic                        count_non_zero;
 
-  logic [data_packet_len_lp - 1 : 0] data_packet;
+  logic [data_rx_len_lp - 1 : 0] data_rx;
   logic [data_bits_p - 1 : 0] data_n, data_r;
 
 
@@ -56,16 +66,16 @@ module config_node
 
 
   assign reset = & shift_r[0 +: frame_bit_size_lp * 2 + data_frame_len_lp];
-  assign valid = (~count_non_zero) ? (~shift_r[0]) : 1'b0;
-  assign packet_len  = shift_r[frame_bit_size_lp     +: len_width_lp];
-  assign node_id     = shift_r[frame_bit_size_lp * 2 +  len_width_lp +: id_width_lp];
-  assign data_packet = shift_r[frame_bit_size_lp * 3 +  len_width_lp +  id_width_lp +: data_packet_len_lp];
+  assign valid = (~count_non_zero) ? (~shift_r.valid) : 1'b0;
+  assign packet_len  = shift_r.len;
+  assign node_id     = shift_r.id;
+  assign data_rx     = shift_r.rx;
 
   genvar i;
   generate
-    for(i = 0; i < data_packet_len_lp - frame_bit_size_lp; i++) begin // the end, or msb of a data_packet is always '0' which is discarded
+    for(i = 0; i < data_rx_len_lp - frame_bit_size_lp; i++) begin // the end, or msb of a transferred data is always '0' which is discarded
       if((i + 1) % (data_frame_len_lp + frame_bit_size_lp)) begin
-        assign data_n[i - i / (data_frame_len_lp + frame_bit_size_lp)] = data_packet[i];
+        assign data_n[i - i / (data_frame_len_lp + frame_bit_size_lp)] = data_rx[i];
       end
     end
   endgenerate
