7bbdcc359588d0d8c8bd1d2ce86ad1c122627bd1
luzh <luzh@ucsd.edu>
2013-01-27 18:20:12 -0800

    Create a localparam of test vector

--- config_net/gen_tb.py
+++ config_net/gen_tb.py
@@ -16,7 +16,7 @@ indent = "  " # indentation
 
 total_inst_nodes = 3 # scan chain nodes ==> to be determined by input file
 l_inst_name = ["Node1", "Node2", "Node3"]
-l_inst_id = [5, 12, 39]
+l_inst_id = [5, 12, 39] # id must be unique and can be represented in $id_width_lp bits
 l_inst_data_bits = [16, 8, 10]
 l_inst_default = [10, 0, 15]
 
@@ -37,7 +37,7 @@ reset_len_lp = 10 # scan chain reset signal length in bits
 #
 global_clk = "clk_i"
 global_clk_period = 10 # time units
-sim_time = 3500 # time units
+sim_time = 500 # time units
 
 #
 l_inst_bit_i = ["bit_i"] # list of inputs bit_i for all nodes; the first one is "bit_i".
@@ -64,7 +64,7 @@ def dec2bin(dec, n): # ==> Only works on non-negative number
 
 def insert_frame_bits(data):
   framed_data = ""
-  data = data[::-1] # reverse the string; in python data[0] is the msb
+  data = data[::-1] # reverse the string; in python data[0] is the msb of a string
   for idx in range(1, len(data) + 1): # 1, 2, 3 ... len(data)
     if idx % data_frame_len_lp == 0:
       framed_data = frame_bit + "_" + data[idx - 1] + framed_data
@@ -99,26 +99,16 @@ def write_cnode(file, id, data_bits, default, clk_i, bit_i, data_o, bit_o):
                            .bit_o(" + bit_o + ") );\n ")
 
 # ========== ==========
-tb_file = open(tb_file_name, 'w')
-tb_file.write("module config_net_tb;\n\n")
-
-write_localparam(tb_file, "len_width_lp     ", str(len_width_lp))
-write_localparam(tb_file, "id_width_lp      ", str(id_width_lp))
-write_localparam(tb_file, "frame_bit_size_lp", str(frame_bit_size_lp))
-write_localparam(tb_file, "data_frame_len_lp", str(data_frame_len_lp))
-write_localparam(tb_file, "reset_len_lp     ", str(reset_len_lp))
-
-tb_file.write("\n//\n")
-write_logic(tb_file, global_clk);
-
+# create test string and calculate total bits
 test_idx = 0
+test_vector_bits = 0
 for test_id in l_test_id:
   inst_idx = l_inst_id.index(test_id)
   send_data = insert_frame_bits(l_test_data[test_idx])
   data_bits = l_inst_data_bits[inst_idx]
   send_data_bits = data_bits + (data_bits / data_frame_len_lp) + frame_bit_size_lp
   packet_len = send_data_bits + frame_bit_size_lp + id_width_lp + frame_bit_size_lp + len_width_lp + frame_bit_size_lp
-  #test_packet = send_data + frame_bit + dec2bin(id) + frame_bit + dec2bin(packet_len) + valid_bit # valid bit
+  test_vector_bits += packet_len
   test_packet = send_data +\
                 "_" + frame_bit +\
                 "_" + dec2bin(test_id, id_width_lp) +\
@@ -127,24 +117,53 @@ for test_id in l_test_id:
                 "_" + valid_bit
   l_test_packet.append(test_packet)
   test_idx += 1;
-  print test_packet
-  print "==>"
+  print test_packet #==>
+  print "test_vector_bits = " + str(test_vector_bits) #==>
+  print "==>" #==>
 
-print dec2bin(0, 1)
-print dec2bin(1, 2)
-print dec2bin(10, 3)
-print dec2bin(15, 3)
-print dec2bin(255, 10)
-print dec2bin(256, 11)
-print "== == framed data == =="
-print insert_frame_bits("10")
-print insert_frame_bits("1010")
-print insert_frame_bits("0101010")
-print insert_frame_bits("10101010")
-print insert_frame_bits("11111110000000")
-print insert_frame_bits("1111111100000000")
-print insert_frame_bits("1110001111111100000000")
-print insert_frame_bits("011110001111111100000000")
+# create reset string
+reset_string = ""
+for bit in range(0, reset_len_lp): # 0, 1, ..., reset_len_lp - 1
+  reset_string += '1'
+
+test_vector_bits += reset_len_lp
+# the whole test vector begins with reset string
+test_vector = reset_string
+# the first test packet in l_test_packet is fed into the configuration network first
+for packet in l_test_packet:
+  test_vector = packet + "__" + test_vector
+
+# calculate the shift register width of the whole scan chain
+shift_chain_width = 0
+for data_bits in l_inst_data_bits:
+  send_data_bits = data_bits + (data_bits / data_frame_len_lp) + frame_bit_size_lp
+  shift_chain_width += send_data_bits + frame_bit_size_lp +\
+                       id_width_lp + frame_bit_size_lp +\
+                       len_width_lp + frame_bit_size_lp
+  print "shift_chain_width = " + str(shift_chain_width) # ==>
+
+# revise simulation time to ensure all test bits walks through the whole scan chain
+sim_time += (test_vector_bits + shift_chain_width) * global_clk_period
+
+print "test_vector      = " + test_vector # ==>
+print "test_vector_bits = " + str(test_vector_bits) #==>
+print "shift_chain_width = " + str(shift_chain_width) # ==>
+print "sim time = " + str(sim_time) # ==>
+
+tb_file = open(tb_file_name, 'w')
+tb_file.write("module config_net_tb;\n\n")
+
+# write localparam
+write_localparam(tb_file, "len_width_lp     ", str(len_width_lp))
+write_localparam(tb_file, "id_width_lp      ", str(id_width_lp))
+write_localparam(tb_file, "frame_bit_size_lp", str(frame_bit_size_lp))
+write_localparam(tb_file, "data_frame_len_lp", str(data_frame_len_lp))
+write_localparam(tb_file, "reset_len_lp     ", str(reset_len_lp))
+tb_file.write(indent + "// double underscore __ separates test packet for each node\n")
+write_localparam(tb_file, "test_vector      ", str(test_vector_bits) + "'b" + test_vector)
+
+tb_file.write("\n//\n")
+write_logic(tb_file, global_clk);
 
 for node_id in l_inst_id:
   index = l_inst_id.index(node_id)
