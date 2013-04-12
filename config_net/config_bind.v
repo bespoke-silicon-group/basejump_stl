d2ab724a16e789016d12c5ad5f7f4bb00e23a66a
luzh <luzh@ucsd.edu>
2013-04-11 20:59:50 -0700

    Rename bind_node.v -> config_bind.v

--- config_net/config_bind.v
+++ config_net/config_bind.v
@@ -1,6 +1,6 @@
 `include "config_defs.v"
 
-module bind_node
+module config_bind
   #(parameter             // node specific parameters 
     id_p = -1,            // unique ID of this node
     data_bits_p = -1,     // number of bits of configurable register associated with this node
