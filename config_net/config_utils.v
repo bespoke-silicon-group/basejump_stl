002cd20a34eda3ce6f9885ff901e7e2e62d78ac5
luzh <luzh@ucsd.edu>
2013-04-17 12:04:24 -0700

    Redefine eConfigIDTypes and adapt sc_spec.in to chip_3way

--- config_net/config_utils.v
+++ config_net/config_utils.v
@@ -4,18 +4,15 @@
 
 `include "config_defs.v"
 
-`define TILE_MAX_X 1 //==>
-`define TILE_MAX_Y 1
-
 // WARNING: All items here are created specially for the GreenDroid chip.
 // When the config_node design is used in other projects, they must be revised.
 
-// An element of this enum type will be used as the config_id_gen macro's
-// `type` parapeter. Types should be defined so that each of them can be used
+// An element of this enum type will be used as the config_id_gen function's
+// `idtype` parapeter. Types should be defined so that each of them can be used
 // to calculate a unique id by config_id_gen macro.
-typedef enum {tGF28_RAM_PINS} eConfigTypes;
+typedef enum {tCFG_DMEM, tCFG_IMEM} eConfigIDTypes;
 
-// An id_type is to distinguish config_nodes in the same (tile_x, tile_y) tile.
+// An idtype is to distinguish config_nodes in the same (tile_x, tile_y) tile.
 // This struct can be extended to provide more fields of identification.
 // Do not forget to modify the config_id_gen function accordingly.
 //
@@ -26,9 +23,12 @@ typedef enum {tGF28_RAM_PINS} eConfigTypes;
 // $clog2(3) = 2
 // $clog2(4) = 2
 // $clog2(5) = 3
-localparam tile_x_bits_lp  = (`TILE_MAX_X < 2) ? 1 : $clog2(`TILE_MAX_X);
-localparam tile_y_bits_lp  = (`TILE_MAX_Y < 2) ? 1 : $clog2(`TILE_MAX_Y);
-localparam id_type_bits_lp = id_width_lp - tile_x_bits_lp - tile_y_bits_lp;
+// There is no bits for `tile_x` in the config_node `id` field when
+// TILE_MAX_X = 1, meaning all config_nodes are on the same X tile.
+// Same rule applies to Y.
+localparam tile_x_bits_lp = $clog2(`TILE_MAX_X);
+localparam tile_y_bits_lp = $clog2(`TILE_MAX_Y);
+localparam idtype_bits_lp = id_width_lp - tile_x_bits_lp - tile_y_bits_lp;
 
 // Tasks and functions without the optional keyword `automatic` are static,
 // with all declared items being statically allocated. These items shall be
@@ -45,15 +45,15 @@ function automatic integer config_id_gen;
   input integer xpos, ypos;
   // Data type 'enum of type int' is not supported in constant function.
   // However, 'enum of type int' can still be passed to this function as a parameter and enum will be converted into int.
-  input integer ctype;
+  input integer idtype;
 
   // Assertions are not supported in constant functions; need for a way to throw exception here.
-  //assert (xpos  < `TILE_MAX_X)              else $error("Error: xpos (%0d) is not less than TILE_MAX_X (%0d)!",  xpos,  `TILE_MAX_X);
-  //assert (ypos  < `TILE_MAX_Y)              else $error("Error: ypos (%0d) is not less than TILE_MAX_Y (%0d)!",  ypos,  `TILE_MAX_Y);
-  //assert (ctype < $pow(2, id_type_bits_lp)) else $error("Error: ctype (%0d) does not fit in id_type_bits (%0d)!", ctype, id_type_bits_lp);
+  //assert (xpos   < `TILE_MAX_X)             else $error("Error: xpos (%0d) is not less than TILE_MAX_X (%0d)!", xpos, `TILE_MAX_X);
+  //assert (ypos   < `TILE_MAX_Y)             else $error("Error: ypos (%0d) is not less than TILE_MAX_Y (%0d)!", ypos, `TILE_MAX_Y);
+  //assert (idtype < $pow(2, idtype_bits_lp)) else $error("Error: idtype (%0d) does not fit in idtype_bits (%0d)!", idtype, idtype_bits_lp);
 
-  config_id_gen = (ctype << (tile_y_bits_lp + tile_x_bits_lp))
-                | ( ypos << (tile_y_bits_lp + tile_x_bits_lp))
+  config_id_gen = (idtype << (tile_y_bits_lp + tile_x_bits_lp))
+                | ( ypos  << (                 tile_x_bits_lp))
                 | ( xpos );
 
 endfunction
