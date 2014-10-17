f89514c8584e00f562eddc1530a12095cadfcda1
Michael Taylor <prof.taylor@gmail.com>
2014-10-17 00:04:17 -0700

    use bsg_scan_xor for more optimized implementation

--- bsg_misc/bsg_gray_to_binary.v
+++ bsg_misc/bsg_gray_to_binary.v
@@ -12,16 +12,23 @@ module bsg_gray_to_binary #(parameter width_p = -1)
    // or alternatively
    // the entertaining:
    //   assign binary_o[width_p-1:0] = ({1'b0, binary_o[width_p-1:1]} ^ gray_i[width_p-1:0]);
- 
+
+/*
    assign binary_o[width_p-1] = gray_i[width_p-1];
 
    generate
       genvar i;
 
       for (i = 0; i < width_p-1; i=i+1)
-	begin
-	   assign binary_o[i] = binary_o[i+1] ^ gray_i[i];
-	end
+        begin
+           assign binary_o[i] = binary_o[i+1] ^ gray_i[i];
+        end
 
    endgenerate
+ */
+
+   // logarithmic depth of the above
+
+   bsg_scan_xor #(.width_p(width_p)) scan_xor(.i(gray_i),.o(binary_o));
+
 endmodule
