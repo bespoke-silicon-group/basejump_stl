dffbe805a688945f4d81b16d2502c80628bac0cb
Michael Taylor <prof.taylor@gmail.com>
2014-10-27 15:16:32 -0700

    add BSG_SAFE_CLOG2: has current functionality for x=1 case add BSG_MIN: power of two checker

--- bsg_misc/bsg_defines.v
+++ bsg_misc/bsg_defines.v
@@ -1,2 +1,6 @@
 `define BSG_MAX(x,y) (((x)>(y)) ? (x) : (y))
 `define BSG_MIN(x,y) (((x)<(y)) ? (x) : (y))
+
+// maps 1 --> 1 instead of to 0
+`define BSG_SAFE_CLOG2(x) ( ((x)==1) ? 1 : $clog2((x)))
+`define BSG_IS_POW2(x) ( (1 << $clog2(x)) == (x))
