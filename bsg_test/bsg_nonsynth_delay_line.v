d0e86c946788006511a638d9cd32451c6e5df24e
Michael Taylor <prof.taylor@gmail.com>
2014-10-27 18:16:39 -0700

    rename file; it is general purpose

---
+++ bsg_test/bsg_nonsynth_delay_line.v
@@ -0,0 +1,11 @@
+module bsg_nonsynth_delay_line # (parameter width_p=1
+				  ,parameter delay_p=-1)
+
+   (input  [width_p-1:0]   i
+    , output logic [width_p-1:0] o
+    );
+
+   always @(i)
+     o <=  #(delay_p) i;
+
+endmodule
