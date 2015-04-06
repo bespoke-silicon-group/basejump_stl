8da76c93b06e487ce6e0183bbd8881a24caa1453
Michael Taylor <prof.taylor@gmail.com>
2015-04-06 16:08:22 -0700

    parameter to allow enq/deq on full; use explicit memory

--- bsg_dataflow/bsg_two_fifo.v
+++ bsg_dataflow/bsg_two_fifo.v
@@ -21,8 +21,13 @@
 //
 
 module bsg_two_fifo #(parameter width_p="inv"
-                      , parameter ready_THEN_valid_p=0
-		      , parameter verbose_p=0)
+                      , parameter verbose_p=0
+                      // whether we should allow simultaneous enque and deque on full
+                      , parameter allow_enq_deq_on_full_p=0
+                      // necessarily, if we allow enq on ready low, then
+                      // we are not using a ready/valid protocol
+                      , parameter ready_THEN_valid_p=allow_enq_deq_on_full_p
+                      )
    (input clk_i
     , input reset_i
 
@@ -40,12 +45,23 @@ module bsg_two_fifo #(parameter width_p="inv"
    wire deq_i = yumi_i;
    wire enq_i;
 
-   logic [1:0][width_p-1:0] data_r;
-
    logic                  head_r,  tail_r;
    logic                  empty_r, full_r;
 
-   assign data_o    = data_r[head_r];
+   bsg_mem_1r1w #(.width_p(width_p)
+                  ,.els_p(2)
+		  ,.read_write_same_addr_p(allow_enq_deq_on_full_p)
+                  ) mem_1r1w
+     (.w_clk_i   (clk_i  )
+      ,.w_reset_i(reset_i)
+      ,.w_v_i    (enq_i  )
+      ,.w_addr_i (tail_r )
+      ,.w_data_i (data_i )
+      ,.r_v_i    (~empty_r)
+      ,.r_addr_i (head_r )
+      ,.r_data_o (data_o )
+      );
+
    assign v_o       = ~empty_r;
    assign ready_o   = ~full_r;
 
@@ -66,10 +82,7 @@ module bsg_two_fifo #(parameter width_p="inv"
         else
           begin
              if (enq_i)
-               begin
-                  data_r[tail_r] <= data_i;
-                  tail_r         <= ~tail_r;
-               end
+               tail_r         <= ~tail_r;
 
              if (deq_i)
                head_r         <= ~head_r;
@@ -78,8 +91,12 @@ module bsg_two_fifo #(parameter width_p="inv"
              empty_r             <= (   empty_r & ~enq_i)
                                     | (~full_r  &  deq_i & ~enq_i);
 
-             full_r              <= (  ~empty_r &  enq_i & ~deq_i)
-                                    | ( full_r  & ~deq_i);
+             if (allow_enq_deq_on_full_p)
+               full_r              <= (  ~empty_r &  enq_i & ~deq_i)
+                                      | ( full_r  & ~(deq_i^enq_i));
+             else
+               full_r              <= (  ~empty_r &  enq_i & ~deq_i)
+                                      | ( full_r  & ~deq_i);
           end // else: !if(reset_i)
      end // always_ff @
 
@@ -91,27 +108,33 @@ module bsg_two_fifo #(parameter width_p="inv"
              assert ({empty_r, deq_i} !== 2'b11)
                else $error("invalid deque on empty fifo ", empty_r, deq_i);
 
-             assert ({full_r,enq_i}   !== 2'b11)
-               else $error("invalid enque on full fifo ", full_r, enq_i);
+             if (allow_enq_deq_on_full_p)
+               begin
+                  assert ({full_r,enq_i,deq_i}   !== 3'b110)
+                    else $error("invalid enque on full fifo ", full_r, enq_i);
+               end
+             else
+               assert ({full_r,enq_i}   !== 2'b11)
+                 else $error("invalid enque on full fifo ", full_r, enq_i);
 
              assert ({full_r,empty_r} !== 2'b11)
                else $error ("fifo full and empty at same time ", full_r, empty_r);
           end // if (~reset_i)
      end // always_ff @
-   
+
    always_ff @(posedge clk_i)
      if (verbose_p)
        begin
           if (v_i)
-	    $display("### %m enq %x onto fifo",data_i);
-	  
-	  if (deq_i)
-	    $display("### %m deq %x from fifo",data_o);
+            $display("### %m enq %x onto fifo",data_i);
+
+          if (deq_i)
+            $display("### %m deq %x from fifo",data_o);
        end
-   
+
    // for debugging
    wire [31:0] num_elements_debug = full_r + (empty_r==0);
-   
+
    // synopsys translate_on
 
 endmodule
