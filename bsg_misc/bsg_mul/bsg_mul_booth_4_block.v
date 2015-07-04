b3c7403508c5f35149beef74321b82b08651eac3
Michael Taylor <prof.taylor@gmail.com>
2015-07-04 16:42:22 -0700

    factor out sub modules so they are easy to replace with hardened versions

---
+++ bsg_misc/bsg_mul/bsg_mul_booth_4_block.v
@@ -0,0 +1,40 @@
+
+// PP_i[210] = { S D N }
+module bsg_mul_booth_4_block #(
+                        S_above_vec_p  = 4'b0000
+                       , dot_bar_vec_p = 4'b0000
+                       , B_vec_p       = 4'b0000
+                       , one_vec_p     = 4'b0000)
+   ( input [4:0][2:0] SDN_i // SDN_i[0] is the SDN before this row. used for S_above_vec_p.
+     , input cr_i
+     , input [3:0][1:0] y_i
+     , output cl_o
+     , output c_o
+     , output s_o
+     );
+
+   wire [3:0] dot_vals;
+
+   genvar     i;
+
+   for (i = 0; i < 4; i=i+1)
+     begin: rof
+          if (S_above_vec_p[i])
+            assign dot_vals[i] = SDN_i[i][0];
+          else
+          if (dot_bar_vec_p[i])
+            assign dot_vals[i] = ~bsg_mul_booth_dot(SDN_i[i+1],y_i[i][1],y_i[i][0]);
+          else
+            if (B_vec_p[i])
+              assign dot_vals[i] = 1'b0;
+            else
+              if (one_vec_p[i])
+                assign dot_vals[i] = 1'b1;
+              else
+                assign dot_vals[i] = bsg_mul_booth_dot(SDN_i[i+1],y_i[i][1],y_i[i][0]);
+
+     end // block: rof
+
+   bsg_mul_comp42 c (.i(dot_vals), .cr_i(cr_i), .cl_o(cl_o), .c_o(c_o), .s_o(s_o));
+
+endmodule // bsg_mul_booth_4_block
---
+++ bsg_misc/bsg_mul/bsg_mul_booth_4_block_rep.v
@@ -0,0 +1,41 @@
+module bsg_mul_booth_4_block_rep #(parameter [31:0] blocks_p=1
+                                   ,parameter S_above_vec_p=0
+                                   ,parameter dot_bar_vec_p=0
+                                   ,parameter B_vec_p=0
+                                   ,parameter one_vec_p=0
+                                   )
+   ( input [4:0][2:0] SDN_i
+     , input                 cr_i
+     , input [blocks_p-1:0][3:0][1:0]  y_vec_i
+     , output                cl_o
+     , output [blocks_p-1:0] c_o
+     , output [blocks_p-1:0] s_o
+     );
+
+   wire [blocks_p:0] ci_local;
+
+   genvar i;
+
+        for (i = 0; i < blocks_p; i=i+1)
+          begin: rof
+             localparam S_above_vec_tmp   = (S_above_vec_p >> (i << 2)) & 4'hf;
+             localparam S_dot_bar_vec_tmp = (dot_bar_vec_p >> (i << 2)) & 4'hf;
+             localparam B_vec_tmp         = (B_vec_p       >> (i << 2)) & 4'hf;
+             localparam one_vec_tmp       = (one_vec_p     >> (i << 2)) & 4'hf;
+
+             bsg_mul_booth_4_block #(
+                             .S_above_vec_p(S_above_vec_tmp)
+                             ,.dot_bar_vec_p(S_dot_bar_vec_tmp)
+                             ,.B_vec_p(B_vec_tmp)
+                             ,.one_vec_p(one_vec_tmp)
+                             )
+             b4b (.SDN_i(SDN_i), .y_i (y_vec_i[i])
+                  , .cr_i(ci_local[i]), .cl_o(ci_local[i+1]), .c_o (c_o[i]), .s_o (s_o[i]));
+          end // block: rof
+
+
+   assign ci_local[0] = cr_i;
+   assign cl_o = ci_local[blocks_p];
+
+endmodule // bsg_mul_booth_4_block_rep
+
