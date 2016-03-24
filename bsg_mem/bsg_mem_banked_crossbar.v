c5655a412287a70750f099dbb62c5263ef208b61
Bandhav Veluri <bandhav.veluri00@gmail.com>
2016-03-24 12:19:25 +0530

    added bsg_crossbar_o_by_i

--- bsg_mem/bsg_mem_banked_crossbar.v
+++ bsg_mem/bsg_mem_banked_crossbar.v
@@ -1,33 +1,3 @@
-/******************************
-* bsg_crossbar_o_by_i
-*******************************/
-
-module bsg_crossbar_o_by_i #( parameter i_els_p = -1
-                             ,parameter o_els_p = -1
-                             ,parameter width_p = -1
-                            )
-  ( input  [i_els_p-1:0][width_p-1:0] i
-   ,input  [o_els_p-1:0][i_els_p-1:0] sel_oi_one_hot_i
-   ,output [o_els_p-1:0][width_p-1:0] o
-  );
-
-  genvar lineout;
-
-  for(lineout=0; lineout<o_els_p; lineout++)
-  begin
-    bsg_mux_one_hot #( .width_p(width_p)
-                      ,.els_p  (i_els_p)
-                     ) mux_one_hot
-                     ( .data_i        (i)
-                      ,.sel_one_hot_i (sel_oi_one_hot_i[lineout])
-                      ,.data_o        (o[lineout])
-                     );
-  end
-
-endmodule // bsg_crossbar_o_by_i
-
-
-
 /*********************************
 * bsg_crossbar_control_rr_o_by_i
 **********************************/
---
+++ bsg_misc/bsg_crossbar_o_by_i.v
@@ -0,0 +1,23 @@
+module bsg_crossbar_o_by_i #( parameter i_els_p = -1
+                             ,parameter o_els_p = -1
+                             ,parameter width_p = -1
+                            )
+  ( input  [i_els_p-1:0][width_p-1:0] i
+   ,input  [o_els_p-1:0][i_els_p-1:0] sel_oi_one_hot_i
+   ,output [o_els_p-1:0][width_p-1:0] o
+  );
+
+  genvar lineout;
+
+  for(lineout=0; lineout<o_els_p; lineout++)
+  begin
+    bsg_mux_one_hot #( .width_p(width_p)
+                      ,.els_p  (i_els_p)
+                     ) mux_one_hot
+                     ( .data_i        (i)
+                      ,.sel_one_hot_i (sel_oi_one_hot_i[lineout])
+                      ,.data_o        (o[lineout])
+                     );
+  end
+
+endmodule // bsg_crossbar_o_by_i
