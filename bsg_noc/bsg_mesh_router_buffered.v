c6d425c7c6d6720484999b632786764e418c8134
proftaylor <prof.taylor@gmail.com>
2016-08-02 09:48:31 -0700

    update for refactor

--- bsg_noc/bsg_mesh_router_buffered.v
+++ bsg_noc/bsg_mesh_router_buffered.v
@@ -1,36 +1,46 @@
+`include "bsg_noc_links.vh"
+
 module bsg_mesh_router_buffered #(width_p        = -1
                                   ,x_cord_width_p = -1
                                   ,y_cord_width_p = -1
                                   ,debug_p       = 0
                                   ,dirs_lp       = 5
                                   ,stub_p        = { dirs_lp {1'b0}}  // SNEWP
-				  ,allow_S_to_EW_p = 0
+                                  ,allow_S_to_EW_p = 0
+                                  ,bsg_ready_and_link_sif_width_lp=`bsg_ready_and_link_sif_width(width_p)
                                   )
    (
     input clk_i
     , input reset_i
 
-    , input [dirs_lp-1:0]              v_i
-    , input [dirs_lp-1:0][width_p-1:0] data_i
-    , output logic [dirs_lp-1:0]       ready_o
-
-    , output [dirs_lp-1:0]              v_o
-    , output [dirs_lp-1:0][width_p-1:0] data_o
-    , input  [dirs_lp-1:0]              ready_i
+    , input  [dirs_lp-1:0][bsg_ready_and_link_sif_width_lp-1:0] link_i
+    , output [dirs_lp-1:0][bsg_ready_and_link_sif_width_lp-1:0] link_o
 
     , input [x_cord_width_p-1:0] my_x_i
     , input [y_cord_width_p-1:0] my_y_i
     );
 
+   `declare_bsg_ready_and_link_sif_s(width_p,bsg_ready_and_link_sif_s);
+
+   bsg_ready_and_link_sif_s [dirs_lp-1:0] link_i_cast, link_o_cast;
+
+   assign link_i_cast =link_i;
+   assign link_o = link_o_cast;
+
    logic [dirs_lp-1:0]              fifo_valid;
    logic [dirs_lp-1:0][width_p-1:0] fifo_data;
    logic [dirs_lp-1:0]              fifo_yumi;
 
-   if (debug_p)
-     always_ff @(negedge clk_i)
-       $display("%m SNEWP v_i=%b ready_o=%b v_o=%b ready_i=%b",v_i,ready_o,v_o,ready_i);
-
    genvar                           i;
+
+   if (debug_p)
+     for (i = 0; i < dirs_lp;i=i+1)
+       begin
+          always_ff @(negedge clk_i)
+            $display("%m x=%d y=%d SNEWP[%d] v_i=%b ready_o=%b v_o=%b ready_i=%b %b"
+                     ,my_x_i,my_y_i,i,link_i_cast[i].v,link_o_cast[i].ready_and_rev,
+                     link_o_cast[i].v,link_i_cast[i].ready_and_rev,link_i[i]);
+       end
 
    for (i = 0; i < dirs_lp; i=i+1)
      begin: rof
@@ -40,12 +50,12 @@ module bsg_mesh_router_buffered #(width_p        = -1
              assign fifo_valid  [i] = 1'b0;
 
              // accept no data from outside of stubbed port
-             assign ready_o     [i] = 1'b0;
+             assign link_o_cast[i].ready_and_rev = 1'b0;
 
              // synopsys translate off
              always @(negedge clk_i)
-               if (v_o[i])
-                 $display("## warning %m: stubbed port %x consumed word %x",i,data_i[i]);
+               if (link_o_cast[i].v)
+                 $display("## warning %m: stubbed port %x received word %x",i,link_i_cast[i].data);
              // synopsys translate on
           end
         else
@@ -55,9 +65,9 @@ module bsg_mesh_router_buffered #(width_p        = -1
                (.clk_i
                 ,.reset_i
 
-                ,.v_i     (v_i [i])
-                ,.data_i  (data_i  [i])
-                ,.ready_o (ready_o [i])
+                ,.v_i     (link_i_cast[i].v            )
+                ,.data_i  (link_i_cast[i].data         )
+                ,.ready_o (link_o_cast[i].ready_and_rev)
 
                 ,.v_o     (fifo_valid[i])
                 ,.data_o  (fifo_data [i])
@@ -66,12 +76,29 @@ module bsg_mesh_router_buffered #(width_p        = -1
           end
      end
 
+   // router does not have symmetric interfaces;
+   // so we do not use bsg_ready_and_link_sif
+   // and manually convert. support for arrays
+   // of interfaces in systemverilog would
+   // fix this.
+
+   logic [dirs_lp-1:0]              valid_lo;
+   logic [dirs_lp-1:0][width_p-1:0] data_lo;
+   logic [dirs_lp-1:0]              ready_li;
+
+   for (i = 0; i < dirs_lp; i=i+1)
+     begin: rof2
+        assign link_o_cast[i].v    = valid_lo[i];
+        assign link_o_cast[i].data = data_lo [i];
+        assign ready_li[i] = link_i_cast[i].ready_and_rev;
+     end
+
    bsg_mesh_router #( .width_p      (width_p      )
                      ,.x_cord_width_p(x_cord_width_p)
                      ,.y_cord_width_p(y_cord_width_p)
                      ,.debug_p      (debug_p      )
                      ,.stub_p       (stub_p       )
-		     ,.allow_S_to_EW_p(allow_S_to_EW_p)
+                     ,.allow_S_to_EW_p(allow_S_to_EW_p)
                      ) bmr
    (.clk_i
     ,.reset_i
@@ -79,13 +106,13 @@ module bsg_mesh_router_buffered #(width_p        = -1
     ,.data_i (fifo_data )
     ,.yumi_o (fifo_yumi )
 
-    ,.v_o
-    ,.data_o
+    ,.v_o   (valid_lo)
+    ,.data_o(data_lo)
 
     // this will be hardwired to 1 by inside of this module
     // if port is stubbed
 
-    ,.ready_i
+    ,.ready_i(ready_li)
 
     ,.my_x_i
     ,.my_y_i
