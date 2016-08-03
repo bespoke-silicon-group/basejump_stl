2eb804d6ff702c9845ec8dd5e4c744d299e105fe
proftaylor <prof.taylor@gmail.com>
2016-08-02 20:43:36 -0700

    reorder fields in bsg_fsb

--- bsg_fsb/bsg_fsb_pkg.v
+++ bsg_fsb/bsg_fsb_pkg.v
@@ -7,11 +7,17 @@ package bsg_fsb_pkg;
                            ,RNRESET_ENABLE_CMD  = 'd5
                            ,RNRESET_DISABLE_CMD = 'd6} bsg_fsb_opcode_s;
 
+// the top 5 bits are not re-purposable.
+//
+
   typedef struct packed {
-    logic [3:0]      srcid;  // 4 bits
-    logic [3:0]      destid; // 4 bis
+    // these bits are reserved and needed for this network
+    logic [3:0]      destid; // 4 bits
     logic [0:0]      cmd;    // 1 bits (1 for switch, 0 for node)
-    bsg_fsb_opcode_s opcode; // 7 bits
+
+    // for cmd=0, these 75 bits are free for general use; they can be repurposed
+    bsg_fsb_opcode_s opcode; // 7 bits - only looked at by switch
+    logic [3:0]      srcid;  // 4 bits
     logic [63:0]     data;   // 64 bits
   } bsg_fsb_pkt_s;
 
