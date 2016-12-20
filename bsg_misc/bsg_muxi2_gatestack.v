822b52465acd09247ce69f9aa77a989fc053feff
Michael Taylor <prof.taylor@gmail.com>
2016-12-19 20:10:45 -0800

    increase delays

---
+++ bsg_misc/bsg_muxi2_gatestack.v
@@ -0,0 +1,15 @@
+module bsg_muxi2_gatestack #(width_p="inv", harden=1)
+   (input [width_p-1:0] i0
+    , input [width_p-1:0] i1
+    , input [width_p-1:0] i2
+    , output [width_p-1:0] o
+    );
+
+   genvar j;
+
+   for (j = 0; j < width_p; j=j+1)
+     begin
+	assign o[j] = ~(i2[j] ? i1[j] : i0[j]);
+     end
+
+endmodule
