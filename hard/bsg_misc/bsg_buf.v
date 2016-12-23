abd966f6af39cbc779c45f6119edfddc887e176b
Michael Taylor <prof.taylor@gmail.com>
2016-12-23 12:52:56 -0800

    add feature to add output repeaters

--- hard/bsg_misc/bsg_buf.v
+++ hard/bsg_misc/bsg_buf.v
@@ -1,16 +1,77 @@
-`define bsg_buf_macro(bits)                                 \
-if (harden_p && (width_p==bits))                            \
-  begin: macro                                              \
-     bsg_rp_tsmc_250_BUFX8_b``bits buf_gate (.i0(i),.o);    \
-  end
+`define bsg_buf_macro(bits)                                   \
+if (harden_p && (width_p==bits) && vertical_p)                \
+  begin: macro                                                \
+     bsg_rp_tsmc_250_BUFX8_b``bits buf_gate (.i0(i),.o);      \
+  end                                                         \
+else                                                          \
+if (harden_p && (width_p==bits) && ~vertical_p)               \
+  begin: macro                                                \
+     bsg_rp_tsmc_250_BUFX8_horiz_b``bits buf_gate (.i0(i),.o);\
+   end
 
 module bsg_buf #(parameter width_p="inv"
                  , parameter harden_p=1
+		 , parameter vertical_p=1
                  )
    (input    [width_p-1:0] i
     , output [width_p-1:0] o
     );
 
+   `bsg_buf_macro(89) else
+   `bsg_buf_macro(88) else
+   `bsg_buf_macro(87) else
+   `bsg_buf_macro(86) else
+   `bsg_buf_macro(85) else
+   `bsg_buf_macro(84) else
+   `bsg_buf_macro(83) else
+   `bsg_buf_macro(82) else
+   `bsg_buf_macro(81) else
+   `bsg_buf_macro(80) else
+   `bsg_buf_macro(79) else
+   `bsg_buf_macro(78) else
+   `bsg_buf_macro(77) else
+   `bsg_buf_macro(76) else
+   `bsg_buf_macro(75) else
+   `bsg_buf_macro(74) else
+   `bsg_buf_macro(73) else
+   `bsg_buf_macro(72) else
+   `bsg_buf_macro(71) else
+   `bsg_buf_macro(70) else
+   `bsg_buf_macro(69) else
+   `bsg_buf_macro(68) else
+   `bsg_buf_macro(67) else
+   `bsg_buf_macro(66) else
+   `bsg_buf_macro(65) else
+   `bsg_buf_macro(64) else
+   `bsg_buf_macro(63) else
+   `bsg_buf_macro(62) else
+   `bsg_buf_macro(61) else
+   `bsg_buf_macro(60) else
+   `bsg_buf_macro(59) else
+   `bsg_buf_macro(58) else
+   `bsg_buf_macro(57) else
+   `bsg_buf_macro(56) else
+   `bsg_buf_macro(55) else
+   `bsg_buf_macro(54) else
+   `bsg_buf_macro(53) else
+   `bsg_buf_macro(52) else
+   `bsg_buf_macro(51) else
+   `bsg_buf_macro(50) else
+   `bsg_buf_macro(49) else
+   `bsg_buf_macro(48) else
+   `bsg_buf_macro(47) else
+   `bsg_buf_macro(46) else
+   `bsg_buf_macro(45) else
+   `bsg_buf_macro(44) else
+   `bsg_buf_macro(43) else
+   `bsg_buf_macro(42) else
+   `bsg_buf_macro(41) else
+   `bsg_buf_macro(40) else
+   `bsg_buf_macro(39) else
+   `bsg_buf_macro(38) else
+   `bsg_buf_macro(37) else
+   `bsg_buf_macro(36) else
+   `bsg_buf_macro(35) else
    `bsg_buf_macro(34) else
    `bsg_buf_macro(33) else
    `bsg_buf_macro(32) else
