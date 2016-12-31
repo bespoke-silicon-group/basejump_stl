d812cf0747d4dab7f990b5f135c3e3b4b6f2920b
Michael Taylor <prof.taylor@gmail.com>
2016-12-30 18:48:56 -0800

    alpaca ram substitutions; bug fixes

--- hard/bsg_mem/bsg_mem_1r1w_sync_mask_write_bit.v
+++ hard/bsg_mem/bsg_mem_1r1w_sync_mask_write_bit.v
@@ -4,11 +4,31 @@
 //
 // reads are synchronous
 
+
+`define bsg_mem_1r1w_sync_macro_rf(words,bits,lgEls,mux)        \
+if (els_p == words && width_p == bits)                          \
+  begin: macro                                                  \
+          tsmc180_2rf_lg``lgEls``_w``bits``_m``mux``_bit mem    \
+            (                                                   \
+             .CLKA (clk_i   )                                   \
+             ,.AA  (r_addr_i)                                   \
+             ,.CENA(~r_v_i  )                                   \
+             ,.QA  (r_data_o)                                   \
+                                                                \
+             ,.CLKB(clk_i )	                                \
+             ,.CENB(~w_v_i)                                     \
+             ,.WENB(w_mask_i)                                   \
+             ,.AB  (w_addr_i)                                   \
+             ,.DB  (w_data_i)                                   \
+             );                                                 \
+  end
+
+
 module bsg_mem_1r1w_sync_mask_write_bit #(parameter width_p=-1
                                         , parameter els_p=-1
                                         , parameter read_write_same_addr_p=0
                                         , parameter addr_width_lp=`BSG_SAFE_CLOG2(els_p)
-                                        , parameter harden_p=0
+                                        , parameter harden_p=1
                                         )
    (input   clk_i
     , input reset_i
@@ -25,7 +45,8 @@ module bsg_mem_1r1w_sync_mask_write_bit #(parameter width_p=-1
     , output logic [width_p-1:0] r_data_o
     );
 
-
+`bsg_mem_1r1w_sync_macro_rf(256,128,8,1) else
+`bsg_mem_1r1w_sync_macro_rf(64,88,6,1) else
    bsg_mem_1r1w_sync_mask_write_bit_synth
      #(.width_p(width_p)
        ,.els_p (els_p  )
--- hard/bsg_mem/bsg_mem_1rw_sync_mask_write_bit.v
+++ hard/bsg_mem/bsg_mem_1rw_sync_mask_write_bit.v
@@ -3,6 +3,27 @@
 // Synchronous 1-port ram.
 // Only one read or one write may be done per cycle.
 
+
+
+`define bsg_mem_1rw_sync_macro_2rf(words,bits,lgEls,mux)        \
+if (els_p == words && width_p == bits)                          \
+  begin: macro                                                  \
+          tsmc180_2rf_lg``lgEls``_w``bits``_m``mux``_bit mem    \
+            (                                                   \
+             .CLKA (clk_i   )                                   \
+             ,.AA  (addr_i)                                     \
+             ,.CENA(~(~w_i&v_i))                                \
+             ,.QA  (data_o)                                     \
+                                                                \
+             ,.CLKB(clk_i )	                                \
+             ,.CENB(~(w_i&v_i))                                 \
+             ,.WENB(w_mask_i)                                   \
+             ,.AB  (addr_i)                                     \
+             ,.DB  (data_i)                                     \
+             );                                                 \
+  end
+
+
 module bsg_mem_1rw_sync_mask_write_bit #(parameter width_p=-1
 			               , parameter els_p=-1
 			               , parameter addr_width_lp=`BSG_SAFE_CLOG2(els_p))
@@ -16,36 +37,22 @@ module bsg_mem_1rw_sync_mask_write_bit #(parameter width_p=-1
     , output [width_p-1:0]  data_o
     );
 
-   wire unused = reset_i;
+   // we use a 2 port RF because the 1 port RF
+   // does not support bit-level masking for 80-bit width
+   // alternatively we could instantiate 2 40-bit 1rw RF's 								
+   `bsg_mem_1rw_sync_macro_2rf(64,80,6,1) else
+   bsg_mem_1rw_sync_mask_write_bit_synth
+     #(.width_p(width_p)
+       ,.els_p(els_p)
+       ) synth
+       (.*);
 
-   logic [addr_width_lp-1:0] addr_r;
-   logic [els_p-1:0][width_p-1:0] mem;
-
-   int i;
+   // synopsys translate_off
 
    always_ff @(posedge clk_i)
      if (v_i)
-       begin
-          // synopsys translate_off
-          assert (addr_i < els_p)
-            else $error("Invalid address %x to %m of size %x\n", addr_i, els_p);
-          // synopsys translate_on
-
-          if (w_i)
-            begin
-              for (i = 0; i < els_p; i=i+1)
-                if (w_mask_i[i])
-                  mem[addr_i][i] <= data_i[i];
-            end
-          else
-            begin
-              addr_r <= addr_i;
-            end
-       end
-
-   assign data_o = mem[addr_r];
-
-  // synopsys translate_off
+       assert (addr_i < els_p)
+         else $error("Invalid address %x to %m of size %x\n", addr_i, els_p);
 
    initial
      begin
