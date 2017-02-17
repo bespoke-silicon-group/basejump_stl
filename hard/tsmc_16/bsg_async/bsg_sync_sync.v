42a89d4c39d75a80470155db42be9f34c16efb55
Scott Davidson <scottdavidson1992@gmail.com>
2017-02-16 21:29:10 -0500

    adding 16nm hard files

---
+++ hard/tsmc_16/bsg_async/bsg_sync_sync.v
@@ -0,0 +1,121 @@
+// MBT 7/24/2014
+//
+// bsg_sync_sync
+//
+// This is just two synchronization flops.
+//
+// We use the rp placement flop from synopsys.
+// Fixme: move this into asic-specific components.
+//
+
+`ifndef rp_group
+ `define rp_group(x)
+ `define rp_place(x)
+ `define rp_endgroup(x)
+ `define rp_fill(x)
+ `define rp_array_dir(up)
+`endif
+
+`define bsg_sync_sync_unit(width_p)                             \
+                                                                \
+module bsg_sync_sync_``width_p``_unit                           \
+  (                                                             \
+   input oclk_i                                                 \
+   , input  [width_p-1:0] iclk_data_i                           \
+   , output [width_p-1:0] oclk_data_o // after sync flops       \
+   );                                                           \
+                                                                \
+                                                                \
+   `rp_group (bss_bank)                                         \
+   `rp_place (hier bss_1 0 0)                                   \
+   `rp_place (hier bss_2 1 0)                                   \
+   `rp_endgroup (bss_bank)                                      \
+                                                                \
+   logic [width_p-1:0] bsg_SYNC_1_r;                            \
+   logic [width_p-1:0] bsg_SYNC_2_r;                            \
+                                                                \
+   assign oclk_data_o = bsg_SYNC_2_r;                           \
+                                                                           \
+   genvar BITS_GEN;                                                        \
+   generate for ( BITS_GEN=0; BITS_GEN < width_p; BITS_GEN = BITS_GEN + 1) \
+     begin: labelhere                                                      \
+	SDFFYQ2D_X2N_A7P5PP96PTS_C16 sff_inst_BITS_GEN(                    \
+			   .D(iclk_data_i[BITS_GEN]),                      \
+			   .CK(oclk_i),                                    \
+			   .SI(1'b0),                                      \
+			   .SE(1'b0),                                      \
+			   .Q(bsg_SYNC_2_r[BITS_GEN]));                    \
+       end                                                                 \
+   endgenerate                                                             \
+endmodule                                                               
+
+//  Code from BSG without the explicit instantiation of the SFF
+//   always_ff @(posedge oclk_i)                                  \
+//     begin                                                      \
+//        `rp_group(bss_1)                                        \
+//        `rp_fill(0 0 UX)                                        \
+//        `rp_array_dir(up)                                       \
+//        `rp_endgroup(bss_1)                                     \
+//        bsg_SYNC_1_r <= iclk_data_i;                            \
+//     end                                                        \
+//                                                                \
+//   always_ff @(posedge oclk_i)                                  \
+//     begin                                                      \
+//        `rp_group(bss_2)                                        \
+//        `rp_fill(0 0 UX)                                        \
+//        `rp_array_dir(up)                                       \
+//        `rp_endgroup(bss_2)                                     \
+//        bsg_SYNC_2_r <= bsg_SYNC_1_r;                           \
+//     end                                                        \
+//                                                                \
+//endmodule
+
+`bsg_sync_sync_unit(1)
+`bsg_sync_sync_unit(2)
+`bsg_sync_sync_unit(3)
+`bsg_sync_sync_unit(4)
+`bsg_sync_sync_unit(5)
+`bsg_sync_sync_unit(6)
+`bsg_sync_sync_unit(7)
+`bsg_sync_sync_unit(8)
+
+// warning: if you make this != 8, you need to modify other
+// parts of this code.
+
+`define bss_max_block 8
+
+`define bss_if_clause(num) if ((width_p % `bss_max_block) == num) begin: z\
+                            bsg_sync_sync_``num``_unit bss                \
+                              (.oclk_i                                    \
+                               ,.iclk_data_i(iclk_data_i[width_p-1-:num]) \
+                               ,.oclk_data_o(oclk_data_o[width_p-1-:num]) \
+                               ); end
+
+
+module bsg_sync_sync #(parameter width_p = "inv")
+   (
+      input oclk_i
+    , input  [width_p-1:0] iclk_data_i
+    , output [width_p-1:0] oclk_data_o // after sync flops
+    );
+
+   genvar   i;
+
+   for (i = 0; i < (width_p/`bss_max_block)*`bss_max_block; i = i + 1)
+     begin : maxb
+        bsg_sync_sync_8_unit bss8
+            (.oclk_i
+             ,.iclk_data_i(iclk_data_i[i*`bss_max_block+:width_p])
+             ,.oclk_data_o(oclk_data_o[i*`bss_max_block+:width_p])
+             );
+     end
+
+   `bss_if_clause(1) else
+     `bss_if_clause(2) else
+       `bss_if_clause(3) else
+         `bss_if_clause(4) else
+           `bss_if_clause(5) else
+             `bss_if_clause(6) else
+               `bss_if_clause(7)
+
+endmodule
---
+++ hard/tsmc_16/bsg_mem/bsg_mem_1rw_sync.v
@@ -0,0 +1,105 @@
+// MBT 11/9/2014
+//
+// Synchronous 1-port ram.
+// Only one read or one write may be done per cycle.
+//
+
+`define bsg_mem_1rw_sync_macro(words,bits,lgEls,newBits)                       \
+if (els_p == words && width_p == bits)                                         \
+  begin: macro                                                                 \
+     tsmc16_1rw_lg``lgEls``_w``newBits``_all mem                               \
+       (.CLK   (clk_i )                                                        \
+       ,.Q     (data_o) // out                                                 \
+       ,.CEN   (~v_i  ) // lo true                                             \
+       ,.GWEN  (~w_i  ) // lo true                                             \
+       ,.A     (addr_i) // in                                                  \
+       ,.D     (data_i) // in                                                  \
+       ,.STOV  (1'd0  ) // Self-timing Override - disabled                     \
+       ,.EMA   (3'd3  ) // Extra Margin Adjustment - default value             \
+       ,.EMAW  (2'd0  ) // Extra Margin Adjustment Write - default value       \
+       ,.EMAS  (1'd0  ) // Extra Margin Adjustment Sense Amp. - default value  \
+       ,.RET1N (1'b1  ) // Retention Mode (active low) - disabled              \
+       );                                                                      \
+  end // block: macro
+
+`define bsg_mem_1rw_sync_macro_rf(words,bits,lgEls,newBits)                   \
+if (els_p == words && width_p == bits)                                        \
+  begin: macro                                                                \
+     tsmc16_1rf_lg``lgEls``_w``newBits``_all mem                              \
+       (.Q     (data_o)                                                       \
+       ,.CLK   (clk_i )                                                       \
+       ,.CEN   (~v_i  )                                                       \
+       ,.GWEN  (~w_i  )                                                       \
+       ,.A     (addr_i)                                                       \
+       ,.D     (data_i)                                                       \
+       ,.STOV  (1'd0  ) // Self-timing Override - disabled                    \
+       ,.EMA   (3'd3  ) // Extra Margin Adjustment - default value            \
+       ,.EMAW  (2'd0  ) // Extra Margin Adjustment Write - default value      \
+       ,.EMAS  (1'd0  ) // Extra Margin Adjustment Sense Amp. - default value \
+       ,.RET1N (1'b1  ) // Retention Mode (active low) - disabled             \
+       );                                                                     \
+  end // block: macro
+
+module bsg_mem_1rw_sync #(parameter width_p=-1
+                         ,parameter els_p=-1
+                         ,parameter addr_width_lp=$clog2(els_p)
+                         // whether to substitute a 1r1w
+                         ,parameter substitute_1r1w_p=1)
+  (input                      clk_i
+  ,input                      reset_i
+  ,input [width_p-1:0]        data_i
+  ,input [addr_width_lp-1:0]  addr_i
+  ,input                      v_i
+  ,input                      w_i
+  ,output logic [width_p-1:0] data_o
+  );
+
+  `bsg_mem_1rw_sync_macro    (1024, 32, 10, 32) else
+  `bsg_mem_1rw_sync_macro_rf (128,  66,  7, 66) else
+  `bsg_mem_1rw_sync_macro_rf (128,  62,  7, 62) else
+
+  begin: z
+    // we substitute a 1r1w macro
+    // fixme: theoretically there may be
+    // a more efficient way to generate a 1rw synthesized ram
+    if (substitute_1r1w_p)
+      begin: s1r1w
+
+        logic [width_p-1:0] data_lo;
+
+        bsg_mem_1r1w #(.width_p(width_p)
+                      ,.els_p(els_p)
+                      ,.read_write_same_addr_p(0)
+                      ) mem
+          (.w_clk_i   (clk_i     )
+          ,.w_reset_i (reset_i   )
+          ,.w_v_i     (v_i & w_i )
+          ,.w_addr_i  (addr_i    )
+          ,.w_data_i  (data_i    )
+          ,.r_addr_i  (addr_i    )
+          ,.r_v_i     (v_i & ~w_i)
+          ,.r_data_o  (data_lo   )
+          );
+
+        // register output data to convert sync to async
+        always_ff @(posedge clk_i)
+          data_o <= data_lo;
+
+      end // block: s1r1w
+    else
+      begin: notmacro
+
+        // Instantiate a synthesizable 1rw sync ram
+        bsg_mem_1rw_sync_synth #(.width_p(width_p), .els_p(els_p)) synth (.*);
+
+      end // block: notmacro
+  end // block: z
+
+  // synopsys translate_off
+  initial
+    begin
+      $display("## %L: instantiating width_p=%d, els_p=%d, substitute_1r1w_p=%d (%m)",width_p,els_p,substitute_1r1w_p);
+    end
+  // synopsys translate_on
+
+endmodule
