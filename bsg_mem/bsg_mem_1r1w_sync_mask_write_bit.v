0f051dfe666a2d0ed106754ffbe3ccd282c46014
Luis Vega <vegaluisjose@gmail.com>
2017-03-06 17:42:15 -0800

    [bsg_mem_1r1w_sync_mask_write] add parameter for disable collision

--- bsg_mem/bsg_mem_1r1w_sync_mask_write_bit.v
+++ bsg_mem/bsg_mem_1r1w_sync_mask_write_bit.v
@@ -9,6 +9,7 @@ module bsg_mem_1r1w_sync_mask_write_bit #(parameter width_p=-1
                                         , parameter read_write_same_addr_p=0
                                         , parameter addr_width_lp=`BSG_SAFE_CLOG2(els_p)
                                         , parameter harden_p=0
+                                        , parameter disable_collision_warning_p=1
                                         )
    (input   clk_i
     , input reset_i
@@ -29,6 +30,7 @@ module bsg_mem_1r1w_sync_mask_write_bit #(parameter width_p=-1
        ,.els_p (els_p  )
        ,.read_write_same_addr_p(read_write_same_addr_p)
        ,.harden_p(harden_p)
+       ,.disable_collision_warning_p(disable_collision_warning_p)
        ) synth
        (.*);
 
@@ -48,7 +50,7 @@ module bsg_mem_1r1w_sync_mask_write_bit #(parameter width_p=-1
           assert (w_addr_i < els_p)
             else $error("Invalid address %x to %m of size %x\n", w_addr_i, els_p);
 
-          assert (~(r_addr_i == w_addr_i && w_v_i && r_v_i && !read_write_same_addr_p))
+          assert (~(r_addr_i == w_addr_i && w_v_i && r_v_i && !read_write_same_addr_p && !disable_collision_warning_p))
             else
               begin
                  $error("%m: Attempt to read and write same address reset_i %b, %x <= %x (mask %x)",reset_i, w_addr_i,w_data_i,w_mask_i);
