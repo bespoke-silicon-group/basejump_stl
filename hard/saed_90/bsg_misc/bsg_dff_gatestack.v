970f291b381c38bdac0c84669f65b97aded5bcce
Scott Davidson <scottdavidson1992@gmail.com>
2018-01-22 14:41:38 -0800

    adding fake harded gatestack modules for saed_90 process as a temporary fix.

---
+++ hard/saed_90/bsg_misc/bsg_dff_gatestack.v
@@ -0,0 +1,17 @@
+module bsg_dff_gatestack #(width_p="inv", harden_p=1)
+   (input [width_p-1:0] i0
+    , input [width_p-1:0] i1
+    , output logic [width_p-1:0] o
+    );
+
+   initial $display("%m: warning module not actually hardened.");
+
+   genvar j;
+
+   for (j = 0; j < width_p; j=j+1)
+     begin
+        always_ff @(posedge i1[j])
+          o[j] <= i0[j];
+     end
+
+endmodule
