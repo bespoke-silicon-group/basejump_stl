768c7f5d1eecd51b67eeafb4dfa0522978a2a7a1
Scott Davidson <scottdavidson1992@gmail.com>
2018-01-22 14:32:45 -0800

    Adding saed_90 hardened ram file for bsg_1rw_sync.v

---
+++ hard/saed_90/bsg_mem/bsg_mem_1rw_sync.v
@@ -0,0 +1,82 @@
+// MBT 11/9/2014
+//
+// Synchronous 1-port ram.
+// Only one read or one write may be done per cycle.
+//
+
+`define bsg_mem_1rw_sync_macro(bits,words)  \
+  if (els_p == words && width_p == bits)    \
+    begin: macro                            \
+       saed90_``bits``x``words``_1P mem     \
+         (.CE1  (clk_i)                     \
+         ,.WEB1 (~w_i)                      \
+         ,.OEB1 (1'b0)                      \
+         ,.CSB1 (~v_i)                      \
+         ,.A1   (addr_i)                    \
+         ,.I1   (data_i)                    \
+         ,.O1   (data_o)                    \
+         );                                 \
+    end
+
+module bsg_mem_1rw_sync #(parameter width_p=-1
+                         ,parameter els_p=-1
+                         ,parameter addr_width_lp=$clog2(els_p)
+                         // whether to substitute a 1r1w
+                         ,parameter substitute_1r1w_p=1)
+  (input                      clk_i
+  ,input                      reset_i
+  ,input [width_p-1:0]        data_i
+  ,input [addr_width_lp-1:0]  addr_i
+  ,input                      v_i
+  ,input                      w_i
+  ,output logic [width_p-1:0] data_o
+  );
+
+  // TODO: ADD ANY NEW RAM CONFIGURATIONS HERE
+  //`bsg_mem_1rw_sync_macro    (128, 64) else
+
+  begin: z
+    // we substitute a 1r1w macro
+    // fixme: theoretically there may be
+    // a more efficient way to generate a 1rw synthesized ram
+    if (substitute_1r1w_p)
+      begin: s1r1w
+
+        logic [width_p-1:0] data_lo;
+
+        bsg_mem_1r1w #(.width_p(width_p)
+                      ,.els_p(els_p)
+                      ,.read_write_same_addr_p(0)
+                      ) mem
+          (.w_clk_i   (clk_i     )
+          ,.w_reset_i (reset_i   )
+          ,.w_v_i     (v_i & w_i )
+          ,.w_addr_i  (addr_i    )
+          ,.w_data_i  (data_i    )
+          ,.r_addr_i  (addr_i    )
+          ,.r_v_i     (v_i & ~w_i)
+          ,.r_data_o  (data_lo   )
+          );
+
+        // register output data to convert sync to async
+        always_ff @(posedge clk_i)
+          data_o <= data_lo;
+
+      end // block: s1r1w
+    else
+      begin: notmacro
+
+        // Instantiate a synthesizable 1rw sync ram
+        bsg_mem_1rw_sync_synth #(.width_p(width_p), .els_p(els_p)) synth (.*);
+
+      end // block: notmacro
+  end // block: z
+
+  // synopsys translate_off
+  initial
+    begin
+      $display("## %L: instantiating width_p=%d, els_p=%d, substitute_1r1w_p=%d (%m)",width_p,els_p,substitute_1r1w_p);
+    end
+  // synopsys translate_on
+
+endmodule
