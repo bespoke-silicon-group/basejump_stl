64caa26156720aba1f89382fb70ed4980b71fc05
akankshabaranwal <akankshabar@gmail.com>
2018-07-09 23:39:13 +0530

    fixes for modules of bsg_mem

--- bsg_mem/bsg_mem_1rw_sync_synth.v
+++ bsg_mem/bsg_mem_1rw_sync_synth.v
@@ -8,10 +8,10 @@ module bsg_mem_1rw_sync_synth #(parameter width_p=-1
 				, parameter els_p=-1
 				, parameter addr_width_lp=`BSG_SAFE_CLOG2(els_p))
    (input   clk_i
-    , input reset_i
+	 	, input v_i
+		, input reset_i
     , input [width_p-1:0] data_i
     , input [addr_width_lp-1:0] addr_i
-    , input v_i
     , input w_i
     , output logic [width_p-1:0]  data_o
     );
