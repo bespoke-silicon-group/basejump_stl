f7275e2a06cf59fcec9245e3c6b7a11afe3ab823
zhaochun <zhaochun07@gmail.com>
2018-07-17 11:24:53 -0700

    change the aspect ratio of srams

--- hard/tsmc_40/bsg_mem/bsg_mem_1rw_sync.v
+++ hard/tsmc_40/bsg_mem/bsg_mem_1rw_sync.v
@@ -57,7 +57,7 @@ module bsg_mem_1rw_sync #(parameter width_p=-1
 
    `bsg_mem_1rw_sync_macro(4096,48,12,48,8) else
    `bsg_mem_1rw_sync_macro(2048,32,11,32,8) else
-   `bsg_mem_1rw_sync_macro(1024,32,10,32,4) else
+   `bsg_mem_1rw_sync_macro(1024,32,10,32,8) else
    `bsg_mem_1rw_sync_macro(256,128,8,128,4) else
      `bsg_mem_1rw_sync_macro_rf(128,74,7,74,2) else
      `bsg_mem_1rw_sync_macro_rf(128,73,7,74,2) else
