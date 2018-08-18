cb8875e4e7a2047c186e7165a07b2344789886f3
shaolin <shawnless.xie@gmail.com>
2018-08-18 03:26:47 -0700

    reverted the Tommy's commit to bsg_decode_with_v.v

--- bsg_misc/bsg_decode_with_v.v
+++ bsg_misc/bsg_decode_with_v.v
@@ -1,23 +1,19 @@
-/**
- *  bsg_decode_with_v.v
- */
+module bsg_decode_with_v #(num_out_p=-1)
+   (
 
-module bsg_decode_with_v #(parameter num_out_p="inv")
-(
-  input [`BSG_SAFE_CLOG2(num_out_p)-1:0] i
-  ,input v_i
-  ,output logic [num_out_p-1:0] o
-);
+    input [`BSG_SAFE_CLOG2(num_out_p)-1:0] i
+    ,input v
+    ,output [num_out_p-1:0] o
+    );
 
-  logic [num_out_p-1:0] lo;
+   wire [num_out_p-1:0]                    lo;
 
-  bsg_decode #(
-    .num_out_p(num_out_p)
-  ) decoder (
-    .i(i)
-    ,.o(lo)
-  );
+   bsg_decode #(.num_out_p(num_out_p)
+                ) bd
+     (.i
+      ,.o(lo)
+      );
 
-  assign o = {(num_out_p){v_i}} & lo;
+   assign o = { (num_out_p) { v } } & lo;
 
 endmodule
