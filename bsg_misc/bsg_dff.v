7dbea00e14fd7de45df1f35d35d6a73dde209288
shaolin <shawnless.xie@gmail.com>
2018-08-18 03:09:29 -0700

    reverted the bsg_dff.v changes

--- bsg_misc/bsg_dff.v
+++ bsg_misc/bsg_dff.v
@@ -1,23 +1,18 @@
-/**
- *  bsg_dff.v
- */
 
-module bsg_dff #(parameter width_p="inv"
-		            ,parameter harden_p=0
-		            ,parameter strength_p=1   // set drive strength
-)
-(
-  input clock_i
-  ,input [width_p-1:0] data_i
-  ,output [width_p-1:0] data_o
-);
+module bsg_dff #(width_p=-1
+		 ,harden_p=0
+		 ,strength_p=1   // set drive strength
+		 )
+   (input   clk_i
+    ,input  [width_p-1:0] data_i
+    ,output [width_p-1:0] data_o
+    );
 
-  logic [width_p-1:0] data_r;
+   reg [width_p-1:0] data_r;
 
-  assign data_o = data_r;
+   assign data_o = data_r;
 
-  always_ff @ (posedge clock_i) begin
-    data_r <= data_i;
-  end
+   always @(posedge clk_i)
+     data_r <= data_i;
 
 endmodule
