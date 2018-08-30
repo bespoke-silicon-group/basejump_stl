7ff351308fec31ec996dba0ca0314a59a3b78a1b
Tommy Jung <gotomstergo@gmail.com>
2018-08-29 18:19:57 -0700

    asd

--- testing/bsg_cache/mesh_top_cache.v
+++ testing/bsg_cache/mesh_top_cache.v
@@ -17,7 +17,7 @@ module mesh_top_cache
     ,parameter packet_width_lp=`bsg_manycore_packet_width(link_addr_width_lp,data_width_p,x_cord_width_p,y_cord_width_p)
     ,parameter return_packet_width_lp=`bsg_manycore_return_packet_width(x_cord_width_p,y_cord_width_p,data_width_p)
     ,parameter link_sif_width_lp=`bsg_manycore_link_sif_width(link_addr_width_lp,data_width_p,x_cord_width_p,y_cord_width_p)
-    ,parameter cache_addr_width_lp=`BSG_SAFE_CLOG2(2-1)+link_addr_width_lp+`BSG_SAFE_CLOG2(data_width_p>>3)
+    ,parameter cache_addr_width_lp=link_addr_width_lp+`BSG_SAFE_CLOG2(data_width_p>>3)
 )
 (
   input clock_i
@@ -131,6 +131,18 @@ module mesh_top_cache
     ,.data_width_p(data_width_p)
     ,.x_cord_width_p(x_cord_width_p)
     ,.y_cord_width_p(y_cord_width_p)
+  ) node11_s_tieoff (
+    .clk_i(clock_i)
+    ,.reset_i(reset_i)
+    ,.link_sif_i(router_link_sif_lo[1][bsg_noc_pkg::S])
+    ,.link_sif_o(router_link_sif_li[1][bsg_noc_pkg::S])
+  );
+
+  bsg_manycore_link_sif_tieoff #(
+    .addr_width_p(link_addr_width_lp)
+    ,.data_width_p(data_width_p)
+    ,.x_cord_width_p(x_cord_width_p)
+    ,.y_cord_width_p(y_cord_width_p)
   ) node01_p_tieoff (
     .clk_i(clock_i)
     ,.reset_i(reset_i)
@@ -142,38 +154,36 @@ module mesh_top_cache
   //
   `declare_bsg_cache_pkt_s(cache_addr_width_lp, data_width_p);
   bsg_cache_pkt_s cache_packet;
-  logic links_to_cache_v_lo;
-  logic links_to_cache_yumi_lo;
+  logic link_to_cache_v_lo;
+  logic link_to_cache_yumi_lo;
   logic cache_ready_lo;
   logic [data_width_p-1:0] cache_data_lo;
   logic cache_v_lo;
   logic cache_v_v_we_lo;
 
-
-
-  // links_to_cache
+  // link_to_cache
   //
-  bsg_manycore_links_to_cache #(
+  bsg_manycore_link_to_cache #(
     .addr_width_p(link_addr_width_lp)
     ,.data_width_p(data_width_p)
     ,.x_cord_width_p(x_cord_width_p)
     ,.y_cord_width_p(y_cord_width_p)
-    ,.num_links_p(nodes_lp)
-  ) links_to_cache (
+    ,.tag_mem_boundary_p(2**25)
+  ) link_to_cache (
     .clock_i(clock_i)
     ,.reset_i(reset_i)
-    ,.my_x_i({(x_cord_width_p)'(1), (x_cord_width_p)'(0)})
-    ,.my_y_i({(y_cord_width_p)'(1), (y_cord_width_p)'(1)})
-    ,.links_sif_i({router_link_sif_lo[1][bsg_noc_pkg::S], router_link_sif_lo[0][bsg_noc_pkg::S]})
-    ,.links_sif_o({router_link_sif_li[1][bsg_noc_pkg::S], router_link_sif_li[0][bsg_noc_pkg::S]})
+    ,.my_x_i((x_cord_width_p)'(0))
+    ,.my_y_i((y_cord_width_p)'(1))
+    ,.link_sif_i(router_link_sif_lo[0][bsg_noc_pkg::S])
+    ,.link_sif_o(router_link_sif_li[0][bsg_noc_pkg::S])
 
     ,.packet_o(cache_packet)
-    ,.v_o(links_to_cache_v_lo)
+    ,.v_o(link_to_cache_v_lo)
     ,.ready_i(cache_ready_lo)
 
     ,.data_i(cache_data_lo)
     ,.v_i(cache_v_lo)
-    ,.yumi_o(links_to_cache_yumi_lo)
+    ,.yumi_o(link_to_cache_yumi_lo)
 
     ,.v_v_we_i(cache_v_v_we_lo)
   ); 
@@ -203,12 +213,12 @@ module mesh_top_cache
     ,.reset_i(reset_i)
 
     ,.packet_i(cache_packet)
-    ,.v_i(links_to_cache_v_lo)
+    ,.v_i(link_to_cache_v_lo)
     ,.ready_o(cache_ready_lo)
 
     ,.data_o(cache_data_lo)
     ,.v_o(cache_v_lo)
-    ,.yumi_i(links_to_cache_yumi_lo)
+    ,.yumi_i(link_to_cache_yumi_lo)
 
     ,.v_v_we_o(cache_v_v_we_lo)
 
