2f76e4e07980b382caa2562c0444f6385bbec1c1
zhaochun <zhaochun07@gmail.com>
2018-10-15 21:55:02 -0700

    add asynchronous reset for synchronizer flip-flops

--- hard/tsmc_40/bsg_clk_gen/bsg_rp_clk_gen_atomic_delay_tuner.v
+++ hard/tsmc_40/bsg_clk_gen/bsg_rp_clk_gen_atomic_delay_tuner.v
@@ -72,6 +72,7 @@ module bsg_rp_clk_gen_atomic_delay_tuner
    TIELBWP   ZB (.ZN(zero_bit));
 
    DFCND4BWP sel_r_reg_0 (.D(mux_lo[0]), .CP(o)      ,.CDN(async_reset_neg_i), .Q(sel_r[0]), .QN());
+   //LHCND4BWP sel_r_latch_0 (.D(mux_lo[0]), .E(o)      ,.CDN(async_reset_neg_i), .Q(sel_r[0]), .QN());
 
    // 40nm: non-inverting mux 32.5ps + load S->Z
    // 40nm: inverting     mux 43ps + load  S->ZN
@@ -83,8 +84,10 @@ module bsg_rp_clk_gen_atomic_delay_tuner
    // nand 10ps versus 22ps
    ND2D1BWP bsg_we_nand   (.A1(we_i_sync_sync), .A2(we_inited_i), .ZN(we_i_sync_sync_nand));
    // synchronizer flops; negative edge triggered
-   DFND1BWP bsg_SYNC_2_r  (.D(we_i_sync), .CPN(o), .Q(we_i_sync_sync), .QN());
-   DFND1BWP bsg_SYNC_1_r  (.D(we_async_i),     .CPN(o), .Q(we_i_sync),      .QN());
+   //DFND1BWP bsg_SYNC_2_r  (.D(we_i_sync), .CPN(o), .Q(we_i_sync_sync), .QN());
+   //DFND1BWP bsg_SYNC_1_r  (.D(we_async_i),     .CPN(o), .Q(we_i_sync),      .QN());
+   DFNCND1BWP bsg_SYNC_2_r  (.D(we_i_sync),  .CPN(o), .CDN(async_reset_neg_i), .Q(we_i_sync_sync), .QN());
+   DFNCND1BWP bsg_SYNC_1_r  (.D(we_async_i), .CPN(o), .CDN(async_reset_neg_i), .Q(we_i_sync),      .QN());
    // drive we signal to next CDT; minimize capacitive load on critical we_i path
    INVD0BWP we_o_pre      (.I(we_i_sync_sync_nand), .ZN(we_o_pre_buf));
    BUFFD4BWP we_o_buf     (.I(we_o_pre_buf),. Z(we_o));
