bb2f2a032490b68213bfad6b0afab142f0981da5
Scott Davidson <scottdavidson1992@gmail.com>
2018-10-23 00:57:15 -0700

    Bug fixes and refactoring

--- bsg_fsb/bsg_front_side_bus_hop_in_no_fc.v
+++ bsg_fsb/bsg_front_side_bus_hop_in_no_fc.v
@@ -7,45 +7,48 @@
 // all incoming packets.
 //
 
-module bsg_front_side_hop_in
+module bsg_front_side_bus_hop_in_no_fc #(parameter width_p="inv")
+  ( input   clk_i
+  , input   reset_i
+  
+  , input [width_p-1:0] data_i
+  , input               v_i
+  
+  // 0 is to the next switch
+  // 1 is to the local switch
+  
+  , output [1:0][width_p-1:0] data_o
+  , output [1:0]              v_o
+  , input                     local_accept_i
+  );
 
-  #(parameter width_p="inv")
+  logic [width_p-1:0] data_r;
+  logic               v_r;
 
-   (input clk_i
-    , input reset_i
+  // fixme: trade logic/speed for power
+  // and avoid fake transitions
 
-    , input                   v_i
-    , input [width_p-1:0]  data_i
+  assign data_o[0] = data_r;
+  assign    v_o[0] = v_r;
 
-    // 0 is to the next switch
-    // 1 is to the local switch
+  // to local node
+  assign data_o[1] = data_r;
+  assign    v_o[1] = v_r & local_accept_i;
 
-    , output [1:0]               v_o
-    , output [1:0] [width_p-1:0] data_o
+  bsg_dff_reset #( .width_p($bits(v_r)) )
+    v_reg
+      ( .clk_i  ( clk_i )
+      , .reset_i( reset_i )
+      , .data_i ( v_i )
+      , .data_o ( v_r )
+      );
 
-    // from next switch
-    , input  ready_i
-    );
-
-   logic               v_r;
-   logic [width_p-1:0] data_r;
-
-   // fixme: trade logic/speed for power
-   // and avoid fake transitions
-
-   assign v_o   [0] = v_r;
-   assign data_o[0] = data_r;
-
-   // to local node
-   assign    v_o[1] = v_r; // & is_me
-   assign data_o[1] = data_r;
-
-   always @(posedge clk_i)
-     begin
-        // nullify valid bit on reset
-        v_r    <= reset_i ? v_i: 1'b0;
-        data_r <= data_o;
-     end
+  bsg_dff #( .width_p($bits(data_r)) )
+    data_reg
+      ( .clk_i ( clk_i )
+      , .data_i( data_i )
+      , .data_o( data_r )
+      );
 
 endmodule
 
