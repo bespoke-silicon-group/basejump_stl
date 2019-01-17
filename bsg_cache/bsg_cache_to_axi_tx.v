8c60714e1427a89fd2b89dec0fafc7d2bfc30c45
tommy <dcjung@uw.edu>
2019-01-16 16:22:26 -0800

    bsg_cache_to_axi

---
+++ bsg_cache/bsg_cache_to_axi_tx.v
@@ -0,0 +1,216 @@
+/**
+ *  bsg_cache_to_axi_tx.v
+ *
+ *  @author tommy
+ */
+
+
+module bsg_cache_to_axi_tx
+  #(parameter num_cache_p="inv"
+    ,parameter data_width_p="inv"
+    ,parameter block_size_in_words_p="inv"
+    
+    ,parameter axi_id_width_p="inv"
+    ,parameter axi_addr_width_p="inv"
+    ,parameter axi_data_width_p="inv"
+    ,parameter axi_burst_len_p="inv"
+
+    ,parameter lg_num_cache_lp=`BSG_SAFE_CLOG2(num_cache_p)
+
+    ,parameter axi_strb_width_lp=(axi_data_width_p>>3)
+    ,parameter data_width_ratio_lp=(axi_data_width_p/data_width_p)
+  )
+  (
+    input clk_i
+    ,input reset_i
+
+    ,input v_i
+    ,output logic yumi_o
+    ,input [lg_num_cache_lp-1:0] tag_i
+    ,input [axi_addr_width_p-1:0] axi_addr_i
+
+    // cache dma write channel
+    ,input  [num_cache_p-1:0][data_width_p-1:0] dma_data_i
+    ,input [num_cache_p-1:0] dma_data_v_i
+    ,output logic [num_cache_p-1:0] dma_data_yumi_o
+
+    // axi write address channel
+    ,output logic [axi_id_width_p-1:0] axi_awid_o
+    ,output logic [axi_addr_width_p-1:0] axi_awaddr_o
+    ,output logic [7:0] axi_awlen_o
+    ,output logic [2:0] axi_awsize_o
+    ,output logic [1:0] axi_awburst_o
+    ,output logic [3:0] axi_awcache_o
+    ,output logic [2:0] axi_awprot_o
+    ,output logic [1:0] axi_awlock_o
+    ,output logic axi_awvalid_o
+    ,input axi_awready_i
+
+    // axi write data channel
+    ,output logic [axi_data_width_p-1:0] axi_wdata_o
+    ,output logic [axi_strb_width_lp-1:0] axi_wstrb_o
+    ,output logic axi_wlast_o
+    ,output logic axi_wvalid_o
+    ,input axi_wready_i
+
+    // axi write response channel
+    ,input [axi_id_width_p-1:0] axi_bid_i
+    ,input [1:0] axi_bresp_i
+    ,input axi_bvalid_i
+    ,output logic axi_bready_o
+  );
+
+  // tag fifo
+  //
+  logic tag_fifo_ready_lo;
+  logic tag_fifo_v_li;
+  logic tag_fifo_v_lo;
+  logic tag_fifo_yumi_li;
+  logic [num_cache_p-1:0] tag_lo;
+
+  bsg_fifo_1r1w_small #(
+    .width_p(lg_num_cache_lp)
+    ,.els_p(num_cache_p)
+  ) tag_fifo (
+    .clk_i(clk_i)
+    ,.reset_i(reset_i)
+
+    ,.v_i(yumi_o)
+    ,.ready_o(tag_fifo_ready_lo)
+    ,.data_i(tag_i)
+
+    ,.v_o(tag_fifo_v_lo)
+    ,.data_o(tag_lo)
+    ,.yumi_i(tag_fifo_yumi_li)
+  );
+
+  assign yumi_o = v_i & tag_fifo_ready_lo & axi_awready_i;
+
+  // axi write address channel
+  //
+  assign axi_awid_o = {axi_id_width_p{1'b0}};
+  assign axi_awaddr_o = axi_addr_i;
+  assign axi_awlen_o = axi_burst_len_p - 1; // burst len
+  assign axi_awsize_o = `BSG_SAFE_CLOG2(axi_data_width_p>>3);
+  assign axi_awburst_o = 2'b01;   // incr
+  assign axi_awcache_o = 4'b0000; // non-bufferable
+  assign axi_awprot_o = 2'b00;    // unprivileged
+  assign axi_awlock_o = 2'b00;    // normal access
+  assign axi_awvalid_o = yumi_o;
+
+  // axi write data channel
+  //
+  logic sipo_v_li;
+  logic sipo_ready_lo;
+  logic [data_width_p-1:0] sipo_data_li;
+  logic [$clog2(data_width_ratio_lp+1)-1:0] sipo_yumi_cnt_li;
+  logic [data_width_ratio_lp-1:0] sipo_v_lo;
+  logic [num_cache_p-1:0] cache_sel;
+  
+  bsg_decode_with_v #(
+    .num_out_p(num_cache_p)
+  ) demux (
+    .i(tag_lo)
+    ,.v_i(tag_fifo_v_lo)
+    ,.o(cache_sel)
+  );
+
+  assign sipo_data_li = dma_data_i[tag_lo];
+  assign dma_data_yumi_o = cache_sel & dma_data_v_i & {num_cache_p{sipo_ready_lo}};
+ 
+  bsg_serial_in_parallel_out #(
+    .width_p(data_width_p)
+    ,.els_p(data_width_ratio_lp)
+  ) sipo (
+    .clk_i(clk_i)
+    ,.reset_i(reset_i)
+
+    ,.valid_i(sipo_v_li)
+    ,.data_i(sipo_data_li)
+    ,.ready_o(sipo_ready_lo)
+
+    ,.valid_o(sipo_v_lo)
+    ,.data_o(axi_wdata_o)
+    ,.yumi_cnt_i(sipo_yumi_cnt_li)
+  );
+
+  assign axi_wvalid_o = &sipo_v_lo;
+  assign sipo_v_li = tag_fifo_v_lo & dma_data_v_i[tag_lo];
+  assign sipo_yumi_cnt_li = axi_wvalid_o & axi_wready_i
+    ? ($clog2(data_width_ratio_lp+1))'(data_width_ratio_lp)
+    : '0;
+ 
+  assign axi_wstrb_o = {axi_strb_width_lp{1'b1}};
+
+  // word counter
+  //
+  logic [`BSG_SAFE_CLOG2(block_size_in_words_p)-1:0] word_count_lo;
+  logic word_up_li;
+  logic word_clear_li;
+
+  bsg_counter_clear_up #(
+    .max_val_p(block_size_in_words_p-1)
+    ,.init_val_p(0)
+  ) word_counter (
+    .clk_i(clk_i)
+    ,.reset_i(reset_i)
+    ,.clear_i(word_clear_li)
+    ,.up_i(word_up_li)
+    ,.count_o(word_count_lo)
+  );
+  
+
+
+  logic pop_word;
+  assign pop_word = dma_data_v_i[tag_lo] & dma_data_yumi_o[tag_lo] & tag_fifo_v_lo;
+
+  always_comb begin
+    if (word_count_lo == block_size_in_words_p-1) begin
+      word_clear_li = pop_word;
+      word_up_li = 1'b0;
+      tag_fifo_yumi_li = pop_word;
+    end
+    else begin
+      word_clear_li = 1'b0;
+      word_up_li = pop_word;
+      tag_fifo_yumi_li = 1'b0;
+    end
+  end
+
+
+  // burst counter
+  //
+  logic [`BSG_SAFE_CLOG2(axi_burst_len_p)-1:0] burst_count_lo;
+  logic burst_up_li;
+  logic burst_clear_li;
+
+  bsg_counter_clear_up #(
+    .max_val_p(axi_burst_len_p-1)
+    ,.init_val_p(0)
+  ) burst_counter (
+    .clk_i(clk_i)
+    ,.reset_i(reset_i)
+    ,.clear_i(burst_clear_li)
+    ,.up_i(burst_up_li)
+    ,.count_o(burst_count_lo)
+  );
+
+  always_comb begin
+    if (burst_count_lo == axi_burst_len_p-1) begin
+      burst_clear_li = axi_wvalid_o & axi_wready_i;
+      burst_up_li = 1'b0;
+      axi_wlast_o = axi_wvalid_o;
+    end
+    else begin
+      burst_clear_li = 1'b0;
+      burst_up_li = axi_wvalid_o & axi_wready_i;
+      axi_wlast_o = 1'b0;
+    end
+  end
+
+  // axi write response channel
+  //
+  assign axi_bready_o = 1'b1; // don't really care about the resp.
+  
+
+endmodule
---
+++ testing/bsg_cache/bsg_cache_to_axi/bsg_test_node_master.v
@@ -0,0 +1,156 @@
+/**
+ *  bsg_test_node_master.v
+ */
+
+`include "bsg_cache_pkt.vh"
+
+module bsg_test_node_master
+  import bsg_cache_pkg::*;
+  #(parameter id_p="inv"
+    ,parameter sets_p="inv"
+    ,parameter addr_width_p="inv"
+    ,parameter data_width_p="inv"
+
+    ,parameter bsg_cache_pkt_width_lp=`bsg_cache_pkt_width(addr_width_p,data_width_p)
+    ,parameter data_mask_width_lp=(data_width_p>>3)
+    ,parameter lg_data_mask_width_lp=`BSG_SAFE_CLOG2(data_mask_width_lp)
+    ,parameter lg_sets_lp=`BSG_SAFE_CLOG2(sets_p)
+  )
+  (
+    input clk_i
+    ,input reset_i
+
+    ,output logic [bsg_cache_pkt_width_lp-1:0] cache_pkt_o
+    ,output logic v_o
+    ,input ready_i   
+
+    ,input [data_width_p-1:0] data_i
+    ,input v_i
+    ,output logic yumi_o
+    
+    ,output logic done_o
+  );
+
+  // casting packet struct
+  //
+  `declare_bsg_cache_pkt_s(addr_width_p, data_width_p);
+  bsg_cache_pkt_s cache_pkt;
+  assign cache_pkt_o = cache_pkt;
+
+  typedef enum logic [2:0] {
+    SEND_TAGST
+    ,RECV_TAGST
+    ,SEND_STORE
+    ,RECV_STORE
+    ,SEND_LOAD
+    ,RECV_LOAD
+    ,DONE
+  } state_e;
+
+  state_e state_r, state_n;
+  logic [`BSG_SAFE_CLOG2(sets_p*2)-1:0] tagst_count_r, tagst_count_n;
+
+  // fifo
+  //
+  logic fifo_ready_lo;
+  logic fifo_v_lo;
+  logic [data_width_p-1:0] fifo_data_lo;
+  logic fifo_yumi_li;
+
+  assign yumi_o = v_i & fifo_ready_lo;
+
+  bsg_fifo_1r1w_large #(
+    .width_p(data_width_p)
+    ,.els_p(1024)
+  ) output_fifo (
+    .clk_i(clk_i)
+    ,.reset_i(reset_i)
+
+    ,.data_i(data_i)
+    ,.v_i(v_i)
+    ,.ready_o(fifo_ready_lo)
+  
+    ,.v_o(fifo_v_lo)
+    ,.data_o(fifo_data_lo)
+    ,.yumi_i(fifo_yumi_li)
+  );
+
+  always_comb begin
+    v_o = 1'b0;
+    fifo_yumi_li = 1'b0;
+    cache_pkt.sigext = 1'b0;
+    cache_pkt.mask = '0;
+    cache_pkt.opcode = TAGST;
+    cache_pkt.addr = '0;
+    cache_pkt.data = '0;
+    tagst_count_n = tagst_count_r;
+    state_n = state_r;
+    done_o = 1'b0;
+      
+    case (state_r)
+
+      SEND_TAGST: begin
+        v_o = 1;
+        cache_pkt.opcode = TAGST;
+        cache_pkt.addr = {
+          {(addr_width_p-lg_sets_lp*2-1-lg_data_mask_width_lp){1'b0}},
+          tagst_count_r,
+          {(lg_data_mask_width_lp+lg_sets_lp){1'b0}}
+        };
+        cache_pkt.data = '0;
+        tagst_count_n = ready_i
+          ? tagst_count_r + 1
+          : tagst_count_r;
+        state_n = (tagst_count_r == (sets_p*2-1)) & ready_i
+          ? RECV_TAGST
+          : SEND_TAGST;
+      end
+
+      RECV_TAGST: begin
+        if (~reset_i & fifo_v_lo) begin
+          $display("[%d] tagst %d", id_p, tagst_count_r);
+        end
+        tagst_count_n = fifo_v_lo
+          ? tagst_count_r + 1
+          : tagst_count_r;
+        state_n = (tagst_count_r == (sets_p*2-1)) & ready_i
+          ? DONE
+          : RECV_TAGST;
+      end
+
+      SEND_STORE: begin
+
+      end
+
+      RECV_STORE: begin
+
+      end
+
+      SEND_LOAD: begin
+
+      end
+
+      RECV_LOAD: begin
+
+      end
+
+      DONE: begin
+        state_n = DONE;
+        done_o = 1'b1;
+      end
+
+    endcase
+  end
+  
+  always_ff @ (posedge clk_i) begin
+    if (reset_i) begin
+      state_r <= SEND_TAGST;
+      tagst_count_r <= '0;
+    end
+    else begin
+      state_r <= state_n;
+      tagst_count_r <= tagst_count_n;
+    end
+  end
+
+endmodule
