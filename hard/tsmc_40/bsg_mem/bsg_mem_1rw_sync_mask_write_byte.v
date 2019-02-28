8eb5a68bde5c9adb81d408b9ea55aed63ece4069
zhaochun <zhaochun07@gmail.com>
2019-02-28 11:29:30 -0800

    added 2-banks support

--- hard/tsmc_40/bsg_mem/bsg_mem_1rw_sync_mask_write_byte.v
+++ hard/tsmc_40/bsg_mem/bsg_mem_1rw_sync_mask_write_byte.v
@@ -41,6 +41,38 @@ if (els_p == words && data_width_p == bits)               \
       ,.DELAY ( 2'b0   ));                                \
   end
 
+`define bsg_mem_1rf_sync_macro_byte_banks(words,bits,lgEls,mux) \
+if (els_p == 2*``words`` && data_width_p == bits)               \
+  begin: macro                                                  \
+    wire [data_width_p-1:0] wen;                                \
+    wire [data_width_p-1:0] bank_data [0:1];                    \
+    logic sel;                                                  \
+    always_ff @(posedge clk_i)                                  \
+      sel <= addr_i[0];                                         \
+    genvar i;                                                   \
+    for(i=0;i<write_mask_width_lp;i++)                          \
+      assign wen[8*i+:8] = {8{write_mask_i[i]}};                \
+    tsmc40_1rf_lg``lgEls``_w``bits``_m``mux mem0                \
+      (.A     ( addr_i[addr_width_lp-1:1] )                     \
+      ,.D     ( data_i                    )                     \
+      ,.BWEB  ( ~wen                      )                     \
+      ,.WEB   ( ~w_i | addr_i[0]          )                     \
+      ,.CEB   ( ~v_i | addr_i[0]          )                     \
+      ,.CLK   ( clk_i                     )                     \
+      ,.Q     ( bank_data[0]              )                     \
+      ,.DELAY ( 2'b0   ));                                      \
+    tsmc40_1rf_lg``lgEls``_w``bits``_m``mux mem1                \
+      (.A     ( addr_i[addr_width_lp-1:1] )                     \
+      ,.D     ( data_i                    )                     \
+      ,.BWEB  ( ~wen                      )                     \
+      ,.WEB   ( ~w_i | ~addr_i[0]         )                     \
+      ,.CEB   ( ~v_i | ~addr_i[0]         )                     \
+      ,.CLK   ( clk_i                     )                     \
+      ,.Q     ( bank_data[1]              )                     \
+      ,.DELAY ( 2'b0                      ));                   \
+    assign data_o = sel? bank_data[1]: bank_data[0];            \
+  end
+
 module bsg_mem_1rw_sync_mask_write_byte
 
  #(parameter els_p = -1
@@ -67,6 +99,9 @@ module bsg_mem_1rw_sync_mask_write_byte
 
   `bsg_mem_1rw_sync_macro_byte(4096,64,12,8) else
   `bsg_mem_1rw_sync_macro_byte(2048,64,11,4) else
+  `bsg_mem_1rw_sync_macro_byte(2048,64,11,4) else
+  `bsg_mem_1rf_sync_macro_byte_banks(512,32,9,4) else
+  `bsg_mem_1rf_sync_macro_byte(1024,32,10,8) else
   `bsg_mem_1rw_sync_macro_byte(1024,32,10,4) else
   // no hardened version found
     begin  : notmacro
