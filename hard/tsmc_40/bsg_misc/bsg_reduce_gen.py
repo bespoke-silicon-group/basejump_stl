851dbccf7cba811699e76e32f5508db83bad91a3
zhaochun <zhaochun07@gmail.com>
2019-03-05 11:55:41 -0800

    update to be compatible with 40nm

--- hard/tsmc_40/bsg_misc/bsg_reduce_gen.py
+++ hard/tsmc_40/bsg_misc/bsg_reduce_gen.py
@@ -9,11 +9,11 @@ wire [1:0] lo;
 // synopsys rp_group    (andr_b4)
 // synopsys rp_fill(0 3 UX)
 // synopsys rp_fill(0 1 UX)
-NAND2X2 b01   (.A(i[0]),.B(i[1]),.Y(lo[0]));
+ND2D2BWP b01   (.A1(i[0]),.A2(i[1]),.ZN(lo[0]));
 // synopsys rp_fill(0 2 UX)
-NR2D4BWP b0123 (.A(lo[0]),.B(lo[1]),.Y(o));
+NR2D4BWP b0123 (.A1(lo[0]),.A2(lo[1]),.ZN(o));
 // synopsys rp_fill(0 3 UX)
-NAND2X2 b23   (.A(i[2]),.B(i[3]),.Y(lo[1]));
+ND2D2BWP b23   (.A1(i[2]),.A2(i[3]),.ZN(lo[1]));
 // synopsys rp_endgroup (andr_b4)
 endmodule
 
@@ -22,11 +22,11 @@ wire [1:0] lo;
 // synopsys rp_group    (andr_b6 )
 // synopsys rp_fill(0 4 UX)
 // synopsys rp_fill(0 2 UX)
-NAND3X2 b012   (.A(i[0]),.B(i[1]),.C(i[2]),.Y(lo[0]));
+ND3D2BWP b012   (.A1(i[0]),.A2(i[1]),.A3(i[2]),.ZN(lo[0]));
 // synopsys rp_fill(0 3 UX)
-NR2D4BWP b012345 (.A(lo[0]),.B(lo[1]),.Y(o));
+NR2D4BWP b012345 (.A1(lo[0]),.A2(lo[1]),.ZN(o));
 // synopsys rp_fill(0 4 UX)
-NAND3X2 b345   (.A(i[3]),.B(i[4]),.C(i[5]),.Y(lo[1]));
+ND3D2BWP b345   (.A1(i[3]),.A2(i[4]),.A3(i[5]),.ZN(lo[1]));
 // synopsys rp_endgroup (andr_b6)
 endmodule
 
@@ -48,13 +48,13 @@ wire [2:0] lo;
 // synopsys rp_group    (andr_b9 )
 // synopsys rp_fill(0 9 UX)
 // synopsys rp_fill(0 2 UX)
-NAND3X2 b012   (.A(i[0]),.B(i[1]),.C(i[2]),.Y(lo[0]));
+ND3D2BWP b012   (.A1(i[0]),.A2(i[1]),.A3(i[2]),.ZN(lo[0]));
 // synopsys rp_fill(0 3 UX)
-NOR3X4 b012345678 (.A(lo[0]),.B(lo[1]),.C(lo[2]),.Y(o));
+NR3D4BWP b012345678 (.A1(lo[0]),.A2(lo[1]),.A3(lo[2]),.ZN(o));
 // synopsys rp_fill(0 4 UX)
-NAND3X2 b345   (.A(i[3]),.B(i[4]),.C(i[5]),.Y(lo[1]));
+ND3D2BWP b345   (.A1(i[3]),.A2(i[4]),.A3(i[5]),.ZN(lo[1]));
 // synopsys rp_fill(0 7 UX)
-NAND3X2 b678   (.A(i[6]),.B(i[7]),.C(i[8]),.Y(lo[2]));
+ND3D2BWP b678   (.A1(i[6]),.A2(i[7]),.A3(i[8]),.ZN(lo[2]));
 // synopsys rp_endgroup (andr_b9)
 endmodule
 
@@ -63,13 +63,13 @@ wire [2:0] lo;
 // synopsys rp_group    (andr_b12 )
 // synopsys rp_fill(0 12 UX)
 // synopsys rp_fill(0 2  UX)
-ND4D2BWP b0123   (.A(i[0]),.B(i[1]),.C(i[2]),.D(i[3]),.Y(lo[0]));
+ND4D2BWP b0123   (.A1(i[0]),.A2(i[1]),.A3(i[2]),.A4(i[3]),.ZN(lo[0]));
 // synopsys rp_fill(0 3  UX)
-NOR3X4 b012345678 (.A(lo[0]),.B(lo[1]),.C(lo[2]),.Y(o));
+NR3D4BWP b012345678 (.A1(lo[0]),.A2(lo[1]),.A3(lo[2]),.ZN(o));
 // synopsys rp_fill(0 4  UX)
-ND4D2BWP b4567   (.A(i[4]),.B(i[5]),.C(i[6]),.D(i[7]),.Y(lo[1]));
+ND4D2BWP b4567   (.A1(i[4]),.A2(i[5]),.A3(i[6]),.A4(i[7]),.ZN(lo[1]));
 // synopsys rp_fill(0 7  UX)
-ND4D2BWP b89AB   (.A(i[8]),.B(i[9]),.C(i[10]),.D(i[11]),.Y(lo[2]));
+ND4D2BWP b89AB   (.A1(i[8]),.A2(i[9]),.A3(i[10]),.A4(i[11]),.ZN(lo[2]));
 // synopsys rp_endgroup (andr_b12)
 endmodule
 
@@ -78,11 +78,11 @@ wire [3:0] lo;
 // synopsys rp_group    (andr_b16 )
 // synopsys rp_fill(0 16 UX)
 // synopsys rp_fill(0 2  UX)
-ND4D2BWP b0123   (.A(i[0]),.B(i[1]),.C(i[2]),.D(i[3]),.Y(lo[0]));
+ND4D2BWP b0123   (.A1(i[0]),.A2(i[1]),.A3(i[2]),.A4(i[3]),.ZN(lo[0]));
 // synopsys rp_fill(0 6  UX)
-ND4D2BWP b4567   (.A(i[4]),.B(i[5]),.C(i[6]),.D(i[7]),.Y(lo[1]));
+ND4D2BWP b4567   (.A1(i[4]),.A2(i[5]),.A3(i[6]),.A4(i[7]),.ZN(lo[1]));
 // synopsys rp_fill(0 7  UX)
-NOR4X4 b012345678 (.A(lo[0]),.B(lo[1]),.C(lo[2]),.D(lo[3]),.Y(o));
+NR4D4BWP b012345678 (.A1(lo[0]),.A2(lo[1]),.A3(lo[2]),.A4(lo[3]),.ZN(o));
 // synopsys rp_fill(0 9  UX)
 ND4D2BWP b89AB   (.A1(i[8]),.A2(i[9]),.A3(i[10]),.A4(i[11]),.ZN(lo[2]));
 // synopsys rp_fill(0 13  UX)
