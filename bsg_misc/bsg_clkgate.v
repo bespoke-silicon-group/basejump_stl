97519cca78a64207f7ac0125cda0d8f6427a8617
Scott Davidson <scottdavidson1992@gmail.com>
2019-03-06 11:33:25 -0800

    Refactored bsg_clkgate to use new bsg_dlatch

--- bsg_misc/bsg_clkgate.v
+++ bsg_misc/bsg_clkgate.v
@@ -2,48 +2,23 @@
 // This logic may be susceptible bug if en_i changes multiple times within a clk cyle
 
 module bsg_clkgate  (input  clk_i
-                    ,input  reset_i
                     ,input  en_i
                     ,output gated_clock_o
                     );
-  wire en_latch;                  
-  assign en_latch = (reset_i) ?  1'b0 :
-                      (~clk_i)  ?  en_i : en_latch;
+  //wire en_latch;                  
+  //assign en_latch = (reset_i) ?  1'b0 :
+  //                    (~clk_i)  ?  en_i : en_latch;
+
+  wire latched_en_lo;
+
+  bsg_dlatch #(.width_p(1) )
+    en_latch
+      ( .en_i   ( ~clk_i )
+      , .data_i ( en_i )
+      , .data_o ( latched_en_lo  )
+      );
   
-  assign gated_clock_o = en_latch & clk_i;
+  assign gated_clock_o = latched_en_lo & clk_i;
 
 endmodule
 
-//implementation two: 
-/* module bsg_clkgate  (input  clk_i
-                    ,input  reset_i
-                    ,input  en_i
-                    ,output gated_clock_o
-                    );
-  wire en_latch                  
-  always @ (reset_i | clk_i | en_i)
-    begin
-      if (reset_i)
-        en_latch = 1'b0;
-      else if (~clk_i)
-        en_latch = en_i;
-    end
-  assign gated_clock_o = gated_en_o & en_latch;
-endmodule
-
-//implementation three:
-module bsg_clkgate  (input en_i
-                    ,input clk_i
-                    ,input reset_i
-                    ,output gated_clk_o
-                    );
-					
-	wire x1, x2, Q, Q_bar;
-	assign x1 = ~en_i & ~clk_i;
-	assign x2 = en_i & ~clk_i;
-	assign Q = ~(x1 | Q_bar);
-	assign Q_bar = ~(x2 | Q);
-	
-	assign gated_clk_o = (reset_i) ? 1'b0 : (Q & clk_i);
-
-endmodule*/
