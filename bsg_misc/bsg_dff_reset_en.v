5a07a4b34a37fdb0fbf93acbc03ae8eb837b910d
tommy <dcjung@uw.edu>
2019-03-07 12:52:53 -0800

    added reset val param

--- bsg_misc/bsg_dff_reset_en.v
+++ bsg_misc/bsg_dff_reset_en.v
@@ -1,24 +1,33 @@
-module bsg_dff_reset_en #(width_p=-1, harden_p=0)
-   (input   clk_i
-    ,input  reset_i
-    ,input  en_i
-    ,input  [width_p-1:0] data_i
-    ,output [width_p-1:0] data_o
-    );
+/**
+ *  bsg_dff_reset_en.v
+ */
 
-   reg [width_p-1:0] data_r;
+module bsg_dff_reset_en
+  #(parameter width_p="inv"
+    , parameter reset_val_p=0
+    , parameter harden_p=0
+  )
+  (
+    input clk_i
+    , input reset_i
+    , input en_i
+    , input [width_p-1:0] data_i
+    , output logic [width_p-1:0] data_o
+  );
 
-   assign data_o = data_r;
+  logic  [width_p-1:0] data_r;
 
-   always @(posedge clk_i)
-     begin
-        if (reset_i)
-          data_r <= width_p'(0);
-        else
-          if (en_i)
-            data_r <= data_i;
-     end
+  assign data_o = data_r;
 
-
+  always_ff @ (posedge clk_i) begin
+    if (reset_i) begin
+      data_r <= width_p'(reset_val_p);
+    end
+    else begin
+      if (en_i) begin
+        data_r <= data_i;
+      end
+    end
+  end
 
 endmodule
