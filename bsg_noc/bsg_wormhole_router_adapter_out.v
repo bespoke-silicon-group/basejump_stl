9c6ed0ba2be56b0df68209010a5838ae35071255
Tommy Jung <dcjung@uw.edu>
2019-03-20 09:59:56 -0700

    wormhole_router adapter in/out

--- bsg_noc/bsg_wormhole_router_adapter_out.v
+++ bsg_noc/bsg_wormhole_router_adapter_out.v
@@ -1,13 +1,13 @@
 /**
- *  bsg_wormhole_router_adapter_in.v
+ *  bsg_wormhole_router_adapter_out.v
  *
  *  packet = {payload, length, y_cord, x_cord}
  */
 
-module bsg_wormhole_router_adapter_in
+module bsg_wormhole_router_adapter_out
   #(parameter max_num_flit_p="inv"
     , parameter max_payload_width_p="inv"
-    , parameter x_cord_width_p="inv"      
+    , parameter x_cord_width_p="inv"
     , parameter y_cord_width_p="inv"
 
     , localparam len_width_lp=`BSG_SAFE_CLOG2(max_num_flit_p)
@@ -25,77 +25,87 @@ module bsg_wormhole_router_adapter_in
     input clk_i
     , input reset_i
 
-    , input [max_packet_width_lp-1:0] data_i
+    , input [width_lp-1:0] data_i
     , input v_i
     , output logic ready_o
 
-    , output logic [width_lp-1:0] data_o
+    , output logic [max_packet_width_lp-1:0] data_o
     , output logic v_o
     , input ready_i
   );
 
-  typedef enum logic {
-    WAIT,
-    SEND
+  typedef enum logic [1:0] {
+    WAIT_HEADER,
+    WAIT_BODY,
+    VALID_OUT
   } state_e;
 
   state_e state_r, state_n;
-  logic [max_packet_width_lp-1:0] data_r, data_n;
   logic [len_width_lp-1:0] count_r, count_n;
+  logic [max_num_flit_p-1:0][width_lp-1:0] data_r;
+  logic we;
 
-
-  logic [padded_packet_width_lp-1:0] padded_data;
-  logic [len_width_lp-1:0] length;
-  
-  assign padded_data = {{padding_width_lp{1'b0}}, data_r};
-  assign length = padded_data[len_offset_lp+:len_width_lp];
-
-  bsg_mux #(
-    .width_p(width_lp)
-    ,.els_p(max_num_flit_p)
-  ) mux (
-    .data_i(padded_data)
-    ,.sel_i(count_r)
-    ,.data_o(data_o)
-  );
+  logic [padded_packet_width_lp-1:0] data_1d;
+  assign data_1d = data_r;
+  assign data_o = data_1d[0+:max_packet_width_lp];
 
   always_comb begin
     state_n = state_r;
-    data_n = data_r;
     count_n = count_r;
     ready_o = 1'b0;
     v_o = 1'b0;
+    we = 1'b0;
 
     case (state_r) 
-      WAIT: begin
+
+      WAIT_HEADER: begin
         ready_o = 1'b1;
         if (v_i) begin
-          state_n = SEND;
-          data_n = data_i; 
+          we = 1'b1;
+          count_n = count_r + 1;
+          state_n = (data_i[len_offset_lp+:len_width_lp] == 0)
+            ? VALID_OUT
+            : WAIT_BODY;
+        end
+      end
+
+      WAIT_BODY: begin
+        ready_o = 1'b1;
+        if (v_i) begin
+          we = 1'b1;
+          count_n = count_r + 1;
+          state_n = (data_r[len_offset_lp+:len_width_lp] == count_r)
+            ? VALID_OUT
+            : WAIT_BODY;
+        end
+      end
+
+      VALID_OUT: begin
+        v_o = 1'b1;
+        if (ready_i) begin
+          state_n = WAIT_HEADER;
           count_n = '0;
         end
       end
-      SEND: begin
-        v_o = 1'b1;
-        count_n = ready_i
-          ? count_r + 1
-          : count_r;
-        state_n = ready_i & (count_r == length)
-          ? WAIT
-          : SEND;
+
+      default: begin
+        state_n = WAIT_HEADER;
       end
+
     endcase
   end
 
   always_ff @ (posedge clk_i) begin
     if (reset_i) begin
-      state_r <= WAIT;
+      state_r <= WAIT_HEADER;
       count_r <= '0;
     end
     else begin
       state_r <= state_n;
       count_r <= count_n;
-      data_r <= data_n;
+      if (we) begin
+        data_r[count_r] <= data_i;
+      end
     end
   end
 
@@ -107,9 +117,10 @@ module bsg_wormhole_router_adapter_in
   end
 
   always_ff @ (negedge clk_i) begin
-    if ((state_r == SEND)) begin
-      assert(length < max_num_flit_p)
-        else $error("Received a packet with length [%0d] that exceeds max_num_flit_p [%0d].", length, max_num_flit_p);
+    if ((state_r == WAIT_BODY)) begin
+      assert(data_r[len_offset_lp+:len_width_lp] < max_num_flit_p)
+        else $error("Received a packet with length [%0d] that exceeds max_num_flit_p [%0d].",
+                    data_r[len_offset_lp+:len_width_lp], max_num_flit_p);
     end
   end
   // synopsys translate_on
