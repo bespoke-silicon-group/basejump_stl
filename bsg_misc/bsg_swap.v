be62bb78acd40fd986bac32b008e202c36be4e61
Tommy Jung <dcjung@uw.edu>
2019-03-28 15:35:55 -0700

    bsg_mux_interleave

---
+++ bsg_misc/bsg_swap.v
@@ -0,0 +1,19 @@
+/**
+ *  bsg_swap.v
+ *
+ *  @author tommy
+ */
+
+module bsg_swap
+  #(parameter width_p="inv")
+  (
+    input [1:0][width_p-1:0] data_i
+    , input swap_i
+    , output logic [1:0][width_p-1:0] data_o
+  );
+
+  assign data_o = swap_i
+    ? {data_i[0], data_i[1]}
+    : {data_i[1], data_i[0]};
+  
+endmodule
