d6fcb7263a2a28433ae2f28d3f511c3764e46472
taylor-bsg <taylor-bsg@users.noreply.github.com>
2019-04-12 16:06:53 -0700

    Update bsg_reduce_segmented.v

--- bsg_misc/bsg_reduce_segmented.v
+++ bsg_misc/bsg_reduce_segmented.v
@@ -3,7 +3,8 @@ module bsg_reduce_segmented #(parameter segments_p = "inv"
 
                   , parameter xor_p = 0
                   , parameter and_p = 0
-                  , parameter or_p = 0                         
+                  , parameter or_p = 0
+                  , parameter nor_p = 0                        
                   )
   (input    [segments_p*segment_width_p-1:0] i
    , output [segments_p-1:0] o
@@ -11,8 +12,8 @@ module bsg_reduce_segmented #(parameter segments_p = "inv"
 
    // synopsys translate_off
    initial
-      assert( $countones({xor_p & 1'b1, and_p & 1'b1, or_p & 1'b1}) == 1)
-        else $error("%m: exactly one function may be selected\n");
+     assert( $countones({xor_p & 1'b1, and_p & 1'b1, or_p & 1'b1, nor_p & 1'b1}) == 1)
+       else $error("%m: exactly one function may be selected\n");
 
   // synopsys translate_on
 
@@ -27,6 +28,8 @@ module bsg_reduce_segmented #(parameter segments_p = "inv"
      assign o[j] = &i[(j+1)*segment_width_p:j*segment_width_p];
    else if (or_p)
      assign o[j] = |i[(j+1)*segment_width_p:j*segment_width_p];
+    else if (nor_p)
+      assign o[j] = ~|i[(j+1)*segment_width_p:j*segment_width_p];
   end
     
 endmodule
