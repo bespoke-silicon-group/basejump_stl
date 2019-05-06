34cbc90f0ba9bd1d78247bb0153c967a2307a22c
Tommy Jung <dcjung@uw.edu>
2019-05-06 10:14:00 -0700

    remove unused enum

--- bsg_fpu/bsg_fpu_pkg.v
+++ bsg_fpu/bsg_fpu_pkg.v
@@ -7,41 +7,6 @@
 
 package bsg_fpu_pkg;
 
-  // opcode for bsg_fpu_top
-  //
-  typedef enum logic [3:0] {
-
-    // arithmetic
-    BSG_FPU_ADD,
-    BSG_FPU_SUB,
-    BSG_FPU_MUL,
-
-    // comparison
-    BSG_FPU_EQ,
-    BSG_FPU_LT,
-    BSG_FPU_LE,
-
-    // min-max
-    BSG_FPU_MIN,
-    BSG_FPU_MAX,
-
-    // conversion
-    BSG_FPU_UI2F,
-    BSG_FPU_I2F,
-    BSG_FPU_F2I,
-    BSG_FPU_F2UI,
-
-    // sign inject
-    BSG_FPU_SGNJ,
-    BSG_FPU_SGNJN,
-    BSG_FPU_SGNJX,
-
-    // classify
-    BSG_FPU_CLASS
-
-  } bsg_fpu_opcode_e;
-
-
   // Special numbers
   //
   `define BSG_FPU_QUIETNAN(e,m) {1'b0, {e{1'b1}}, 1'b1, {(m-1){1'b0}}}  // aka canonical NaN
