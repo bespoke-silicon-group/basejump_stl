9430387c8730b8c1e3d634ccaacedcc51079779a
Tommy Jung <dcjung@uw.edu>
2019-05-11 15:51:04 -0700

    refactor derived params, bsg_cache_pkt_decode

--- bsg_cache/bsg_cache_dma.v
+++ bsg_cache/bsg_cache_dma.v
@@ -11,9 +11,11 @@ module bsg_cache_dma
   #(parameter addr_width_p="inv"
     ,parameter data_width_p="inv"
     ,parameter block_size_in_words_p="inv"
-    ,parameter lg_block_size_in_words_lp="inv"
-    ,parameter lg_sets_lp="inv"
-    ,parameter bsg_cache_dma_pkt_width_lp=`bsg_cache_dma_pkt_width(addr_width_p)
+    ,parameter sets_p="int"
+
+    ,localparam lg_block_size_in_words_lp=`BSG_SAFE_CLOG2(block_size_in_words_p)
+    ,localparam lg_sets_lp=`BSG_SAFE_CLOG2(sets_p)
+    ,localparam bsg_cache_dma_pkt_width_lp=`bsg_cache_dma_pkt_width(addr_width_p)
   
     ,parameter debug_p=0
   )
