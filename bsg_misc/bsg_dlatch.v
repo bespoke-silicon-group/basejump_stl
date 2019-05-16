428f38175e0054adce87cb66b9e47b39422f502e
Scott Davidson <scottdavidson1992@gmail.com>
2019-05-16 11:12:16 -0700

    Changed always_ff to always_latch
    Changed en_i to clk_i
    
    Adding i_know_this_is_a_bad_idea_p parameter

--- bsg_misc/bsg_dlatch.v
+++ bsg_misc/bsg_dlatch.v
@@ -1,12 +1,21 @@
-module bsg_dlatch #(parameter width_p="inv")
-  (input               en_i
+module bsg_dlatch #(parameter width_p="inv"
+                   ,parameter i_know_this_is_a_bad_idea_p=0
+                   )
+  (input               clk_i
   ,input [width_p-1:0] data_i
   ,output logic [width_p-1:0] data_o
   );
 
-  always_ff @ (en_i or data_i)
+  // Do not surround with translate off tags so that a warning is issued in DC
+  // if the user has not admitted that this is a bad idea.
+  if (i_know_this_is_a_bad_idea_p == 0)
     begin
-      if (en_i)
+      initial $warning("# WARNING (%m) using bsg_dlatch without admitting that this is a bad idea!");
+    end
+
+  always_latch
+    begin
+      if (clk_i)
         data_o <= data_i;
     end
 
