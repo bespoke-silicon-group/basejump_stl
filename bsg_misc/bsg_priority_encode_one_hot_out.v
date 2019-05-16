0a9b3a9ee26c233e126da93d3235db7ce8d598c9
taylor-bsg <taylor-bsg@users.noreply.github.com>
2019-05-15 22:34:47 -0700

    fix N=1 case for bsg_priority_encode_one_hot_out.v; update bsg_mem_banked_crossbar test for mask=1 meaning write

--- bsg_misc/bsg_priority_encode_one_hot_out.v
+++ bsg_misc/bsg_priority_encode_one_hot_out.v
@@ -17,17 +17,21 @@ module bsg_priority_encode_one_hot_out #(parameter width_p      = "inv"
 
    logic [width_p-1:0] scan_lo;
 
-   bsg_scan #(.width_p(width_p)
-              ,.or_p      (1)
-              ,.lo_to_hi_p(lo_to_hi_p)
-              ) scan (.i (i)
-                      ,.o(scan_lo)
-                      );
-
-   // edge detect
-   if (lo_to_hi_p)
-     assign o = scan_lo & { (~scan_lo[width_p-2:0]), 1'b1 };
+   if (width_p == 1)
+     assign o = i;
    else
-     assign o = scan_lo & { 1'b1, (~scan_lo[width_p-1:1]) };
+     begin
+	bsg_scan #(.width_p(width_p)
+		   ,.or_p      (1)
+		   ,.lo_to_hi_p(lo_to_hi_p)
+		   ) scan (.i (i)
+			   ,.o(scan_lo)
+			   );
 
+	// edge detect
+	if (lo_to_hi_p)
+	  assign o = scan_lo & { (~scan_lo[width_p-2:0]), 1'b1 };
+	else
+	  assign o = scan_lo & { 1'b1, (~scan_lo[width_p-1:1]) };
+     end
 endmodule
