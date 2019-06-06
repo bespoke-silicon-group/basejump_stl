707a56dab5ee80f53b23c7fe3d59c742777b4048
Paul Gao <gaozihou@uw.edu>
2019-06-05 18:09:02 -0700

    add reset_i_r flop. update description.

--- bsg_link/bsg_link_oddr_phy.v
+++ bsg_link/bsg_link_oddr_phy.v
@@ -9,6 +9,42 @@
 // has 90-degree phase delay to posedge of clk_i.
 // Need output delay constraint(s) to ensure clock and data delay are same
 //
+// Waveform of all signals when going out of reset:
+/******************************************************************************************
+
+          +---+   +---+   +---+   +---+   +---+   +---+   +---+   +---+   +---+   +---+
+clk_i         |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+              +---+   +---+   +---+   +---+   +---+   +---+   +---+   +---+   +---+   +---+
+          +--------+
+reset_i            |
+                   +----------------------------------------------------------------------+
+          +----------------+
+reset_i_r                  |
+                           +--------------------------------------------------------------+
+                               +-------+       +-------+       +-------+       +-------+
+clk_r                          |       |       |       |       |       |       |       |
+          +--------------------+       +-------+       +-------+       +-------+       +---
+                           +-------+       +-------+       +-------+       +-------+
+odd_r                      |       |       |       |       |       |       |       |
+          +----------------+       +-------+       +-------+       +-------+       +-------
+          +----------------+       +-------+       +-------+       +-------+       +-------
+ready_o                    |       |       |       |       |       |       |       |
+                           +-------+       +-------+       +-------+       +-------+
+          +--------+-----------------------------------------------------------------------
+data_i    |        | d1d0  |       | d1d0  |       | d1d0  |       | d1d0  |       | d1d0
+          +--------------------------------------------------------------------------------
+          +--------------------------------------------------------------------------------
+data_i_r  |                |     d1d0      |     d1d0      |     d1d0      |     d1d0
+          +----------------+---------------------------------------------------------------
+                                       +-------+       +-------+       +-------+       +---
+clk_o                                  |       |       |       |       |       |       |
+          +----------------------------+       +-------+       +-------+       +-------+
+          +--------------------------------+---------------+---------------+---------------
+data_o    |                        |  d0   |  d1   |  d0   |  d1   |  d0   |  d1   |  d0
+          +------------------------+-------+-------+-------+-------+-------+-------+-------
+
+******************************************************************************************/
+//
 // Schematic and more information: (Google Doc) 
 // https://docs.google.com/document/d/1lmkOxvlAvxrk_MM5W8xv3ho2DS26xbOMTCqUIyS6di8/edit?ts=5cf76063#heading=h.o6ptt6mn49us
 //
@@ -29,7 +65,7 @@ module bsg_link_oddr_phy
   ,output logic               clk_r_o
   );
   
-  logic odd_r, clk_r;  
+  logic odd_r, clk_r, reset_i_r;  
   logic [1:0][width_p-1:0] data_i_r;
   
   // ready to accept new data every two cycles
@@ -37,22 +73,26 @@ module bsg_link_oddr_phy
   
   // register 2x-wide input data in flops
   always_ff @(posedge clk_i)
-    if (reset_i | ~odd_r)
+    if (~odd_r)
         data_i_r <= data_i;
-    else
-        data_i_r <= data_i_r;
         
   // odd_r signal (mux select bit)
   always_ff @(posedge clk_i)
-    if (reset_i) 
-        odd_r <= 1'b1;
+    if (reset_i)
+        odd_r <= 1'b0;
     else 
         odd_r <= ~odd_r;
+  
+  // reset_i is sync to posedge of clk_i, while clk_r is sync to negedge.
+  // This will potentially become critical path (only 1/2 period max delay).
+  // Add an extra flop for clk_r reset.
+  always_ff @(posedge clk_i)
+    reset_i_r <= reset_i;
   
   // clock output
   always_ff @(negedge clk_i)
   begin
-    if (reset_i)
+    if (reset_i_r)
         clk_r <= 1'b0;
     else 
         clk_r <= ~clk_r;
