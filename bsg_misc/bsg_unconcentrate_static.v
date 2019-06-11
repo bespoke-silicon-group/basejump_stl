3dd70fc619de1e23c833f2a26dff7c25a5464097
taylor-bsg <taylor-bsg@users.noreply.github.com>
2019-06-10 23:55:08 -0700

    bsg_unconcentrate_static created

---
+++ bsg_misc/bsg_unconcentrate_static.v
@@ -0,0 +1,22 @@
+// takes an input vector and spreads the elements according to a bit pattern
+// example width_p=3, pattern_els_p=5'b10101
+module bsg_unconcentrate_static #(parameter width_p="inv", pattern_els_p="inv")
+(input [width_p-1:0] i
+ ,output [$bits(pattern_els_p)-1:0] o
+ );
+   genvar j;
+
+   if (pattern_els_p[0])
+     assign o[0] = i[0];
+   else
+     assign o[0] = 'z;
+   
+   for (j = 1; j < $bits(pattern_els_p); j=j+1)
+     begin: rof
+	     if (pattern_els_p[j])
+	       assign o[j] = i[`BSG_COUNTONES_SYNTH(pattern_els_p[j-1:0])];
+	     else
+	       assign o[j] = 'z;
+     end
+      
+endmodule
