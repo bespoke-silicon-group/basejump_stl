5f085aeeab70cc55eb1f3c3897af626833616f68
Michael Taylor <prof.taylor@gmail.com>
2019-06-13 15:26:54 -0700

    missing include; remove whitespace chars

--- bsg_misc/bsg_unconcentrate_static.v
+++ bsg_misc/bsg_unconcentrate_static.v
@@ -1,8 +1,8 @@
 // takes an input vector and spreads the elements according to a bit pattern
 // example width_p=3, pattern_els_p=5'b10101
 module bsg_unconcentrate_static #(pattern_els_p="inv"
-				  , width_lp=`BSG_COUNTONES_SYNTH(pattern_els_p)
-				 )
+                                  , width_lp=`BSG_COUNTONES_SYNTH(pattern_els_p)
+                                 )
 (input [width_lp-1:0] i
  ,output [$bits(pattern_els_p)-1:0] o
  );
@@ -12,13 +12,13 @@ module bsg_unconcentrate_static #(pattern_els_p="inv"
      assign o[0] = i[0];
    else
      assign o[0] = 'z;
-   
+
    for (j = 1; j < $bits(pattern_els_p); j=j+1)
      begin: rof
-	     if (pattern_els_p[j])
-	       assign o[j] = i[`BSG_COUNTONES_SYNTH(pattern_els_p[j-1:0])];
-	     else
-	       assign o[j] = 'z;
+             if (pattern_els_p[j])
+               assign o[j] = i[`BSG_COUNTONES_SYNTH(pattern_els_p[j-1:0])];
+             else
+               assign o[j] = 'z;
      end
-      
+
 endmodule
