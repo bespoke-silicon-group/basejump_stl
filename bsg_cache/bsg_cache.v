692dae66fb2ec4a73a11ea53098f1473245f584c
taylor-bsg <taylor-bsg@users.noreply.github.com>
2019-06-19 22:38:45 -0700

    update with google doc link that will not expire

--- bsg_cache/bsg_cache.v
+++ bsg_cache/bsg_cache.v
@@ -8,7 +8,7 @@
  *
  *  @author tommy
  * 
- * See https://docs.google.com/document/d/15XJv3vH-08uvdUijjlAHJKznX4Wf42e19rZMyExlxGI/view for design doc
+ * See https://docs.google.com/document/d/1AIjhuwTbOYwyZHdu-Uc4dr9Fwxi6ZKscKSGTiUeQEYo/edit for design doc
  */
 
 `include "bsg_cache_pkt.vh"
