99f246f6161690e68a8879dd624adc9d97d12a0c
Dan Petrisko <petrisko@cs.washington.edu>
2019-08-14 12:40:36 -0700

    Separating out wormhole concentrator_in/out/duplex

--- bsg_noc/bsg_wormhole_concentrator.v
+++ bsg_noc/bsg_wormhole_concentrator.v
@@ -44,153 +44,67 @@ module bsg_wormhole_concentrator
   );
 
   `declare_bsg_ready_and_link_sif_s(flit_width_p,bsg_ready_and_link_sif_s);
-  `declare_bsg_wormhole_router_header_s(cord_markers_pos_p[dims_p], len_width_p, bsg_wormhole_router_header_s);
-  `declare_bsg_wormhole_concentrator_header_s(cord_markers_pos_p[dims_p], len_width_p,cid_width_p, bsg_wormhole_concentrator_header_s);
   
   bsg_ready_and_link_sif_s [num_in_p-1:0] links_i_cast, links_o_cast;
+  bsg_ready_and_link_sif_s [num_in_p-1:0] links_o_stubbed_v, links_o_stubbed_ready;
   bsg_ready_and_link_sif_s concentrated_link_i_cast, concentrated_link_o_cast;
+  bsg_ready_and_link_sif_s concentrated_link_o_stubbed_v, concentrated_link_o_stubbed_ready;
   
   assign links_i_cast = links_i;
   assign links_o = links_o_cast;
-  
+
   assign concentrated_link_i_cast = concentrated_link_i;
   assign concentrated_link_o = concentrated_link_o_cast;
   
-  genvar i,j;
-
-  /********** From unconcentrated side to concentrated side **********/
-  
-  wire [num_in_p-1:0][flit_width_p-1:0] fifo_data_lo;
-  wire [num_in_p-1:0]                   fifo_valid_lo;
-
-  // one for each input channel; it broadcasts that it is finished to all of the outputs
-  wire [num_in_p-1:0] releases;
-
-  // from each input to concentrated output
-  wire [num_in_p-1:0] reqs;
-
-  // from concentrated output to each input
-  wire [num_in_p-1:0] yumis;
-
-  for (i = 0; i < num_in_p; i=i+1)
-    begin: in_ch
-
-      bsg_two_fifo #(.width_p(flit_width_p)) twofer
-        (.clk_i
-        ,.reset_i
-        ,.ready_o(links_o_cast[i].ready_and_rev)
-        ,.data_i (links_i_cast[i].data)
-        ,.v_i    (links_i_cast[i].v)
-        ,.v_o    (fifo_valid_lo[i])
-        ,.data_o (fifo_data_lo [i])
-        ,.yumi_i (yumis[i])
-        );
-
-      bsg_wormhole_router_header_s hdr;
-      assign hdr = fifo_data_lo[i][$bits(bsg_wormhole_router_header_s)-1:0];
-
-      bsg_wormhole_router_input_control #(.output_dirs_p(1), .payload_len_bits_p($bits(hdr.len))) wic
-        (.clk_i
-        ,.reset_i
-        ,.fifo_v_i           (fifo_valid_lo[i])
-        ,.fifo_yumi_i        (yumis[i])
-        ,.fifo_decoded_dest_i(1'b1)
-        ,.fifo_payload_len_i (hdr.len)
-        ,.reqs_o             (reqs[i])
-        ,.release_o          (releases[i]) // broadcast to all
-        ,.detected_header_o  ()
-        );
-
+  for (genvar i = 0; i < num_in_p; i++)
+    begin : stub
+      assign links_o_cast[i].data          = links_o_stubbed_ready[i].data;
+      assign links_o_cast[i].v             = links_o_stubbed_ready[i].v;
+      assign links_o_cast[i].ready_and_rev = links_o_stubbed_v[i].ready_and_rev;
     end
+      assign concentrated_link_o_cast.data          = concentrated_link_o_stubbed_ready.data;
+      assign concentrated_link_o_cast.v             = concentrated_link_o_stubbed_ready.v;
+      assign concentrated_link_o_cast.ready_and_rev = concentrated_link_o_stubbed_v.ready_and_rev;
 
-  wire [num_in_p-1:0] data_sel_lo;
+  bsg_wormhole_concentrator_in
+   #(.flit_width_p(flit_width_p)
+     ,.len_width_p(len_width_p)
+     ,.cid_width_p(cid_width_p)
+     ,.num_in_p(num_in_p)
+     ,.dims_p(dims_p)
+     ,.cord_markers_pos_p(cord_markers_pos_p)
+     ,.debug_lp(debug_lp)
+     )
+   concentrator_in
+    (.clk_i(clk_i)
+     ,.reset_i(reset_i)
 
-  bsg_wormhole_router_output_control #(.input_dirs_p(num_in_p)) woc
-    (.clk_i
-    ,.reset_i
-    ,.reqs_i    (reqs         )
-    ,.release_i (releases     )
-    ,.valid_i   (fifo_valid_lo)
-    ,.yumi_o    (yumis        )
-    ,.ready_i   (concentrated_link_i_cast.ready_and_rev)
-    ,.valid_o   (concentrated_link_o_cast.v)
-    ,.data_sel_o(data_sel_lo)
-    );
-  
-  bsg_mux_one_hot #(.width_p(flit_width_p)
-                   ,.els_p  (num_in_p)
-                   ) data_mux
-    (.data_i       (fifo_data_lo)
-    ,.sel_one_hot_i(data_sel_lo)
-    ,.data_o       (concentrated_link_o_cast.data)
-    );
-    
-  /********** From concentrated side to unconcentrated side **********/
-  
-  wire [flit_width_p-1:0] concentrated_fifo_data_lo;
-  wire                    concentrated_fifo_valid_lo;
+     ,.links_i(links_i)
+     ,.links_o(links_o_stubbed_v)
 
-  // one for each input channel; it broadcasts that it is finished to all of the outputs
-  wire concentrated_releases;
-
-  // from concentrated input to each output
-  wire [num_in_p-1:0] concentrated_reqs;
-
-  // from each output to concentrated input
-  wire [num_in_p-1:0] concentrated_yumis;
-
-  wire concentrated_any_yumi = | concentrated_yumis;
-
-  bsg_two_fifo #(.width_p(flit_width_p)) concentrated_twofer
-    (.clk_i
-    ,.reset_i
-    ,.ready_o(concentrated_link_o_cast.ready_and_rev)
-    ,.data_i (concentrated_link_i_cast.data)
-    ,.v_i    (concentrated_link_i_cast.v)
-    ,.v_o    (concentrated_fifo_valid_lo)
-    ,.data_o (concentrated_fifo_data_lo )
-    ,.yumi_i (concentrated_any_yumi)
+     ,.concentrated_link_i(concentrated_link_i)
+     ,.concentrated_link_o(concentrated_link_o_stubbed_ready)
      );
 
-  bsg_wormhole_concentrator_header_s concentrated_hdr;
-  assign concentrated_hdr = concentrated_fifo_data_lo[$bits(bsg_wormhole_concentrator_header_s)-1:0];
-  
-  wire [num_in_p-1:0] concentrated_decoded_dest_lo;
-  bsg_decode #(.num_out_p(num_in_p)) concentrated_decoder
-    (.i(concentrated_hdr.cid[0+:`BSG_SAFE_CLOG2(num_in_p)])
-    ,.o(concentrated_decoded_dest_lo)
-    );
+  bsg_wormhole_concentrator_out
+   #(.flit_width_p(flit_width_p)
+     ,.len_width_p(len_width_p)
+     ,.cid_width_p(cid_width_p)
+     ,.num_in_p(num_in_p)
+     ,.dims_p(dims_p)
+     ,.cord_markers_pos_p(cord_markers_pos_p)
+     ,.debug_lp(debug_lp)
+     )
+   concentrator_out
+    (.clk_i(clk_i)
+     ,.reset_i(reset_i)
 
-  bsg_wormhole_router_input_control #(.output_dirs_p(num_in_p), .payload_len_bits_p($bits(concentrated_hdr.len))) concentrated_wic
-    (.clk_i
-    ,.reset_i
-    ,.fifo_v_i           (concentrated_fifo_valid_lo)
-    ,.fifo_yumi_i        (concentrated_any_yumi)
-    ,.fifo_decoded_dest_i(concentrated_decoded_dest_lo)
-    ,.fifo_payload_len_i (concentrated_hdr.len)
-    ,.reqs_o             (concentrated_reqs)
-    ,.release_o          (concentrated_releases) // broadcast to all
-    ,.detected_header_o  ()
-    );
+     ,.links_i(links_i)
+     ,.links_o(links_o_stubbed_ready)
 
-  // iterate through each output channel
-  for (i = 0; i < num_in_p; i=i+1)
-    begin: out_ch
-
-      bsg_wormhole_router_output_control #(.input_dirs_p(1)) concentrated_woc
-        (.clk_i
-        ,.reset_i
-        ,.reqs_i    (concentrated_reqs[i] )
-        ,.release_i (concentrated_releases)
-        ,.valid_i   (concentrated_fifo_valid_lo)
-        ,.yumi_o    (concentrated_yumis[i])
-        ,.ready_i   (links_i_cast[i].ready_and_rev)
-        ,.valid_o   (links_o_cast[i].v)
-        ,.data_sel_o()
-        );
-      
-      assign links_o_cast[i].data = concentrated_fifo_data_lo;
-      
-    end
+     ,.concentrated_link_i(concentrated_link_i)
+     ,.concentrated_link_o(concentrated_link_o_stubbed_v)
+     );
 
 endmodule
+
