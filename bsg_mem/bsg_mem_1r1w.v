2bf6a7687dbda740b8eca4ed6b607a89e83bbda2
Dan Petrisko <petrisko@cs.washington.edu>
2019-09-05 14:32:23 -0700

    Converting bsg_mem_xr1w.v assertions to pure logical expressions

--- bsg_mem/bsg_mem_1r1w.v
+++ bsg_mem/bsg_mem_1r1w.v
@@ -48,7 +48,7 @@ module bsg_mem_1r1w #(parameter width_p=-1
           assert ((w_reset_i === 'X) || (w_reset_i === 1'b1) || (w_addr_i < els_p))
             else $error("Invalid address %x to %m of size %x (w_reset_i=%b, w_v_i=%b)\n", w_addr_i, els_p, w_reset_i, w_v_i);
 
-          assert ((w_reset_i === 'X) || (w_reset_i === 1'b1) || ~(r_addr_i == w_addr_i && w_v_i && r_v_i && !read_write_same_addr_p))
+          assert ((w_reset_i === 'X) || (w_reset_i === 1'b1) || !(r_addr_i == w_addr_i && w_v_i && r_v_i && !read_write_same_addr_p))
             else $error("%m: Attempt to read and write same address %x (w_v_i = %b, w_reset_i = %b)",w_addr_i,w_v_i,w_reset_i);
        end
 
