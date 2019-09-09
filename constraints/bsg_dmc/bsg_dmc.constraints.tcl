cb722c62ab181f5f7700cc06969633203f1ac7ae
zhaochun <zhaochun07@gmail.com>
2019-09-09 11:08:58 -0700

    make variable names clear

--- constraints/bsg_dmc/bsg_dmc.constraints.tcl
+++ constraints/bsg_dmc/bsg_dmc.constraints.tcl
@@ -16,10 +16,10 @@ proc bsg_dmc_data_timing_constraints { \
 } {
   # dfi_clk_1x and dfi_clk_2x are defined inside bsg_dram_clk_gen
   # we use their sources and cycle time to derive other dram-related clocks
-  set dfi_clk_period [expr [get_attribute [get_clocks $dfi_clk_2x_name] period] * 2.0]
-  set dfi_clk_source [get_attribute [get_clocks $dfi_clk_1x_name] sources]
+  set dfi_clk_1x_period [expr [get_attribute [get_clocks $dfi_clk_2x_name] period] * 2.0]
+  set dfi_clk_1x_source [get_attribute [get_clocks $dfi_clk_1x_name] sources]
   # create input dqs clock
-  create_clock -period $dfi_clk_period \
+  create_clock -period $dfi_clk_1x_period \
                -name dqs_${grp_idx}    \
                $dqs_i
 
@@ -43,7 +43,7 @@ proc bsg_dmc_data_timing_constraints { \
     foreach_in_collection to_obj [concat $dq_o $dm_o] {
       # ideally output clock is center aligned with output data, so the clock has (-25%, +25%) of clock cycle time as the sampling window
       # we deduct 20% from the margin and check if the clock is in the scope of (-5%, +5%) of the data center
-      set_data_check -from $from_obj -to $to_obj [expr $dfi_clk_period * 0.2]
+      set_data_check -from $from_obj -to $to_obj [expr $dfi_clk_1x_period * 0.2]
       # set_data_check has a default zero cycle checking behavior which need to overcome in this case
       # please take solvnet #024664 as a reference
       set_multicycle_path -end -setup 1 -to $to_obj
@@ -65,15 +65,15 @@ proc bsg_dmc_ctrl_timing_constraints { \
 } {
   # dfi_clk_1x and dfi_clk_2x are defined inside bsg_dram_clk_gen
   # we use their sources and cycle time to derive other dram-related clocks
-  set dfi_clk_period [expr [get_attribute [get_clocks $dfi_clk_2x_name] period] * 2.0]
-  set dfi_clk_source [get_attribute [get_clocks $dfi_clk_1x_name] sources]
+  set dfi_clk_1x_period [expr [get_attribute [get_clocks $dfi_clk_2x_name] period] * 2.0]
+  set dfi_clk_1x_source [get_attribute [get_clocks $dfi_clk_1x_name] sources]
   # create generated clocks to drive dram device, all the address and command signals are synchronous to these clocks
-  create_generated_clock -name ddr_ck_p -divide_by 1         -source $dfi_clk_source -master_clock [get_clocks $dfi_clk_1x_name] $ck_p
-  create_generated_clock -name ddr_ck_n -divide_by 1 -invert -source $dfi_clk_source -master_clock [get_clocks $dfi_clk_1x_name] $ck_n
+  create_generated_clock -name ddr_ck_p -divide_by 1         -source $dfi_clk_1x_source -master_clock [get_clocks $dfi_clk_1x_name] $ck_p
+  create_generated_clock -name ddr_ck_n -divide_by 1 -invert -source $dfi_clk_1x_source -master_clock [get_clocks $dfi_clk_1x_name] $ck_n
   # all the address and command signals are registered outputs which are aligned well with clock edges
   # we give it a 10% margin to account for the misalignment caused by PCB traces and bond wires
-  set_output_delay [expr  $dfi_clk_period * 0.1] $ctrl_signals -clock [get_clocks ddr_ck_p] -max
-  set_output_delay [expr -$dfi_clk_period * 0.1] $ctrl_signals -clock [get_clocks ddr_ck_p] -min
+  set_output_delay [expr  $dfi_clk_1x_period * 0.1] $ctrl_signals -clock [get_clocks ddr_ck_p] -max
+  set_output_delay [expr -$dfi_clk_1x_period * 0.1] $ctrl_signals -clock [get_clocks ddr_ck_p] -min
 }
 
 puts "Info: Completed script [info script]\n"
