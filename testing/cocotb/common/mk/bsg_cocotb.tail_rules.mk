7357f5e912f92c3c2f8201f48cd092c13f7e9219
Scott Davidson <scottdavidson1992@gmail.com>
2019-09-15 13:43:23 -0700

    Adding partial makefiles that make up the generic side of the bsg cocotb testing infrastructure.

---
+++ testing/cocotb/common/mk/bsg_cocotb.tail_rules.mk
@@ -0,0 +1,74 @@
+TESTING_COCOTB_COMMON_MK_DIR := $(realpath $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+include $(TESTING_COCOTB_COMMON_MK_DIR)/../cocotb_tools.mk
+
+#===============================================================================
+# bsg_cocotb.tail_rules.mk
+#
+# This makefile is to be included at the end of a cocotb test makefile. It
+# defines the main generalized makefile targets that will be used throughout
+# the testing infrastructure.
+#
+
+# Set the default goal to run all test parameterizations.
+.DEFAULT_GOAL := test.all
+
+# This is the makefile that is going to run the cocotb test. We copy this into
+# the sweep test run directory and then call make on this makefile.
+RUN_COCOTB_MAKEFILE := $(TESTING_COCOTB_COMMON_MK_DIR)/run_cocotb.mk
+
+#===============================================================================
+# Spawn Test Parameterization Targets
+#
+# Here we define a function to spawn a test and clean.test targets for a given
+# parameterization. Then we go through and actually spwan the targets for each
+# of the defined parameterizations.
+#
+
+# Collection of all test targets that get spawned
+ALL_TEST_TARGETS :=
+
+# Function to spawn a test.% and clean.test.% target for the given param value
+define add_test=
+
+TEST_NAME_$1 :=$$(word 1,$$(BSG_PARAM_SWEEP_PARAMS_$1))
+TEST_PVAL_$1 :=$$(wordlist 2,$$(words $$(BSG_PARAM_SWEEP_PARAMS_$1)),$$(BSG_PARAM_SWEEP_PARAMS_$1))
+
+ALL_TEST_TARGETS += test.$$(TEST_NAME_$1)
+
+test.$$(TEST_NAME_$1): test.%: build_tools
+	mkdir -p $$*
+	cp $$(RUN_COCOTB_MAKEFILE) $$*/Makefile
+	$$(eval export BSG_TOPLEVEL_PVALS=$$(TEST_PVAL_$1))
+	$$(eval export BSG_ADDITIONAL_PYTHONPATH=$$(CURDIR))
+	cd $$* && $(COCOTB_VENV_ACTIVATE) && make $$(TEST_PVAL_$1) sim 2>&1 | tee -i run.log
+
+clean.test.$$(TEST_NAME_$1): clean.test.%:
+	rm -rf $$*
+
+endef
+
+# Spawn all of the test cases based on the BSG_PARAM_SWEEP_* variables.
+$(foreach i, $(shell for i in {1..$(BSG_PARAM_SWEEP_COUNT)}; do echo $$i; done), $(eval $(call add_test,$i)))
+
+#===============================================================================
+# Main Targets
+#
+# All other standard target declarations. Makefiles that include this makefile
+# can extend the prereq list for these targets to do design specific actions
+# before these targets are executed.
+#
+
+# Alias run target for all test cases. Can run in parallel using the -j flag.
+test.all: $(ALL_TEST_TARGETS)
+
+# Alias clean target for all test cases. Can run in parallel using the -j flag.
+clean.test.all: $(addprefix clean.,$(ALL_TEST_TARGETS))
+
+# Clean target (doesn't clean test runs)
+clean:
+	rm -f *.pyc
+
+# Alias target to clean everything (prestine)
+deep_clean: clean.test.all clean
+
---
+++ testing/cocotb/common/mk/run_cocotb.mk
@@ -0,0 +1,65 @@
+#===============================================================================
+# run_cocotb.mk
+#
+# This makefile is a standard cocotb makefile with a BSG twist. In our testing
+# infrastructure, we define environment variables that need to be translated to
+# the variables and structure that cocotb is expecting. This makefile get's
+# copied into each testing parameterization's subfolder before being executed.
+#
+# The comments for what each of the cocotb variables does is taken directly
+# from the cocotb documentation.
+#
+
+# Define the main RTL language
+TOPLEVEL_LANG ?= verilog
+
+# Selects which simulator Makefile to use. 
+SIM ?= vcs
+
+# Bin directory for vcs (if not in PATH)
+VCS_BIN_DIR ?= $(VCS_HOME)/bin
+
+# A list of the Verilog source files to include.
+VERILOG_SOURCES ?= $(BSG_VERILOG_SOURCES)
+
+# Used to indicate the instance in the hierarchy to use as the DUT. If this
+# isn’t defined then the first root instance is used.
+TOPLEVEL ?= $(BSG_TOPLEVEL_MODULE)
+
+# The name of the module(s) to search for test functions. Multiple modules can
+# be specified using a comma-separated list.
+MODULE ?= $(BSG_PY_TEST_MODULES)
+
+# Any arguments or flags to pass to the compile stage of the simulation.
+COMPILE_ARGS ?= -timescale=1ns/1ns \
+                $(addprefix +incdir+,$(BSG_VERILOG_INCDIRS)) \
+                $(addprefix -pvalue ,$(BSG_TOPLEVEL_PVALS))
+
+# Any arguments or flags to pass to the execution of the compiled simulation.
+#SIM_ARGS ?=
+
+# Passed to both the compile and execute phases of simulators with two rules,
+# or passed to the single compile and run command for simulators which don’t
+# have a distinct compilation stage.
+#EXTRA_ARGS ?=
+
+# Use to add additional dependencies to the compilation target; useful for
+# defining additional rules to run pre-compilation or if the compilation phase
+# depends on files other than the RTL sources listed in VERILOG_SOURCES or
+# VHDL_SOURCES.
+#CUSTOM_COMPILE_DEPS ?=
+
+# Seed the Python random module to recreate a previous test stimulus.
+export RANDOM_SEED ?= 0
+
+# If defined, log lines displayed in terminal will be shorter. It will print
+# only time, message type (INFO, WARNING, ERROR) and log message.
+export COCOTB_REDUCED_LOG_FMT
+
+# Search path for python imports.
+PYTHONPATH := $(PYTHONPATH):$(BSG_ADDITIONAL_PYTHONPATH)
+
+# Include standard cocotb makefile infrastructure
+include $(shell cocotb-config --makefiles)/Makefile.inc
+include $(shell cocotb-config --makefiles)/Makefile.sim
+
