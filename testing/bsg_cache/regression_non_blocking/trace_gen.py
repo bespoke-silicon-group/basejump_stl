2aaf89845da333d280cf63fad297adad91f546ce
Tommy Jung <tommy.dc.jung@gmail.com>
2019-10-08 15:48:34 -0700

    block random test

--- testing/bsg_cache/regression_non_blocking/trace_gen.py
+++ testing/bsg_cache/regression_non_blocking/trace_gen.py
@@ -16,6 +16,7 @@ class BsgCacheNonBlockingRegression:
     self.curr_data = 1
     self.sets_p = 128
     self.ways_p = 8
+    self.block_size_in_words_p = 8
 
   def send(self, opcode, addr):
     if opcode == SW:
@@ -37,6 +38,13 @@ class BsgCacheNonBlockingRegression:
   def done(self):
     self.tg.done()
 
+  # max_delay: max delay in number of cycle
+  def delay_random(self, max_delay):
+    delay = random.randint(0,max_delay)
+    if delay != 0:
+      self.wait(delay)
+
+
   # stride: byte size
   # max_addr: exclusive upper bound for addr.
   def test_stride(self, stride, max_addr):
@@ -44,13 +52,16 @@ class BsgCacheNonBlockingRegression:
     while base < stride:
       i = base
       while i < max_addr:
+        self.delay_random(4)
         self.send(SW, i)
         i += stride
       i = base
       while i < max_addr:
+        self.delay_random(4)
         self.send(LW, i)
         i += stride
       base += 4
+
 
   # num: number of random accesses
   # max_addr: exclusive upper bound for addr.
@@ -58,10 +69,29 @@ class BsgCacheNonBlockingRegression:
     for i in range(num):
       addr = random.randint(0, (max_addr/4)-1)*4
       store_not_load = random.randint(0,1)
+      self.delay_random(16)
       if store_not_load:
         self.send(SW, addr)
       else:
         self.send(LW, addr)
+
+
+  # block_addr: byte addr (can be any offset)
+  def test_block_random(self, block_addr):
+    base_addr = block_addr - (block_addr % (self.block_size_in_words_p*4))
+    block_offset = list(range(0,4*self.block_size_in_words_p,4))
+    random.shuffle(block_offset)
+
+    for i in block_offset:
+      self.delay_random(13)
+      addr = base_addr + i
+      op = random.randint(0,2)
+      if op == 0:
+        self.send(LW, addr)
+      elif op == 1:
+        self.send(SW, addr)
+      
+      
 
 
 #   main()
@@ -77,15 +107,19 @@ if __name__ == "__main__":
   MAX_ADDR = 65536
 
   # test_stride
-  stride = 4
-  while stride < 256:
-    tg.test_stride(stride, MAX_ADDR)
-    stride += 4
+  strides = list(range(4,256,4))
+  random.shuffle(strides)
+  for i in strides:
+    tg.test_stride(i, MAX_ADDR)
 
   # test_random
-  N = 10000
+  N = 2500000
   tg.test_random(N, MAX_ADDR)
 
+  # test_block_random
+  for i in range(100000):
+    addr = random.randint(0,MAX_ADDR-1)
+    tg.test_block_random(addr)
 
   # done
   tg.done()
