216c3d3e12585f22365f507b7fbf001649f5af35
Dan Petrisko <petrisko@cs.washington.edu>
2019-11-13 16:02:26 -0800

    Adding drivers for demanding DUTs

--- testing/cocotb/common/bsg_cocotb_lib/bsg_cocotb_utils.py
+++ testing/cocotb/common/bsg_cocotb_lib/bsg_cocotb_utils.py
@@ -49,6 +49,50 @@ class ReadyValidBusProducer(Driver):
         self.valid_sig <= 0
 
 
+class ValidYumiBusProducer(Driver):
+    def __init__(self, entity, name, clock, data_sig, valid_sig, yumi_sig):
+        Driver.__init__(self)
+
+        self.entity = entity
+        self.name = name
+        self.clock = clock
+
+        self.log = self.entity._log
+
+        self.data_sig = data_sig
+        self.valid_sig = valid_sig
+        self.yumi_sig = yumi_sig
+
+        # Drive initial values to avoid x asserts
+        self.yumi_sig.setimmediatevalue(0)
+        self.log.info("{} created".format(self.name))
+
+    @cocotb.coroutine
+    def start(self, data_gen=None, idle_gen=None, max_txns=None):
+        if data_gen is not None:
+            for i in range(max_txns):
+                if idle_gen is not None:
+                    for i in range(next(idle_gen)):
+                        yield RisingEdge(self.clock)
+                yield self.send(next(data_gen))
+        else:
+            self.log.error("No data generator specified")
+
+    @cocotb.coroutine
+    def _driver_send(self, data, sync=True, **kwargs):
+        yield NextTimeStep()
+        yield ReadWrite()
+        self.valid_sig <= 1
+        self.data_sig <= data
+        while True:
+            yield ReadOnly()
+            if self.yumi_sig.value:
+                break
+            yield RisingEdge(self.clock)
+        yield RisingEdge(self.clock)
+        self.valid_sig <= 0
+
+
 class ValidYumiBusConsumer(Driver):
     def __init__(self, entity, name, clock, data_sig, valid_sig, yumi_sig):
         Driver.__init__(self)
@@ -69,8 +113,7 @@ class ValidYumiBusConsumer(Driver):
     def start(self, backpressure=None):
         while True:
             if backpressure is not None:
-                delay = next(backpressure)
-                for i in range(delay):
+                for i in range(next(backpressure)):
                     yield RisingEdge(self.clock)
             yield self.recv()
 
@@ -91,6 +134,50 @@ class ValidYumiBusConsumer(Driver):
         self.yumi_sig <= 1
         yield RisingEdge(self.clock)
         self.yumi_sig <= 0
+
+
+class ReadyValidBusConsumer(Driver):
+    def __init__(self, entity, name, clock, data_sig, ready_sig, valid_sig):
+        Driver.__init__(self)
+
+        self.entity = entity
+        self.name = name
+        self.clock = clock
+
+        self.data_sig = data_sig
+        self.ready_sig = ready_sig
+        self.valid_sig = valid_sig
+
+        # Drive initial values to avoid x asserts
+        self.valid_sig.setimmediatevalue(0)
+        self.log.info("{} created".format(self.name))
+
+    @cocotb.coroutine
+    def start(self, backpressure=None):
+        while True:
+            if backpressure is not None:
+                for i in range(next(backpressure)):
+                    yield RisingEdge(self.clock)
+            yield self.recv()
+
+    # We alias send to receive in busconsumer
+    @cocotb.coroutine
+    def recv(self):
+        yield self.send(None)
+
+    @cocotb.coroutine
+    def _driver_send(self, data, sync=True, **kwargs):
+        while True:
+            yield NextTimeStep()
+            yield ReadWrite()
+            self.ready_sig <= 1
+            while True:
+                yield ReadOnly()
+                if self.yumi_sig.value:
+                    break
+                yield RisingEdge(self.clock)
+            yield RisingEdge(self.clock)
+            self.ready_sig <= 0
 
 
 class ReadyValidBusMonitor(Monitor):
