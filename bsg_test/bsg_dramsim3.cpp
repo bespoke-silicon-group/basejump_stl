e32599b625e72ebb4150fbb398be1175adbf77cd
Max Ruttenberg <mrutt@cs.washington.edu>
2019-12-20 10:19:22 -0800

    updates dramsim3 backend

--- bsg_test/bsg_dramsim3.cpp
+++ bsg_test/bsg_dramsim3.cpp
@@ -86,6 +86,7 @@ extern "C" addr_t bsg_dramsim3_get_done_addr(int ch)
  * Initialize the memory system.
  * @param[in] num_channels_p RTL parameter for the expected number of channels
  * @param[in] data_width_p   RTL parameter for the expected data width of the memory system (BL * bus width)
+ * @param[in] size_p         RTL parameter for the exepected size of the memory system in bits
  * @param[in] config_p       The path to the configuration file for the memory system (.ini)
  * @return true if the system was succesfully initialized, otherwise false.
  */
@@ -93,6 +94,7 @@ extern "C" bool bsg_dramsim3_init(
     /* for sanity checking */
     int num_channels_p,
     int data_width_p,
+    long long size_p,
     char *config_p)
 {
     string config_file(config_p);
@@ -104,7 +106,7 @@ extern "C" bool bsg_dramsim3_init(
 
     /* called when read completes */
     auto read_done  = [](uint64_t addr) {
-        int ch = _memory_system->GetDRAMSystem()->GetChannel(addr);
+        int ch = _memory_system->GetConfig()->AddressMapping(addr).channel;
         _read_done[ch] = true;
         _read_done_addr[ch] = addr;
     };
@@ -116,6 +118,13 @@ extern "C" bool bsg_dramsim3_init(
 
     /* sanity check */
     Config *cfg = _memory_system->GetConfig();
+
+    /* calculate device size */
+    long long channels = cfg->channels;
+    long long channel_size = cfg->channel_size;
+    long long memory_size = channels * channel_size;
+    long long memory_size_bits = memory_size * 8;
+
     if (cfg->channels != num_channels_p) {
         pr_err("num_channels_p (%d) does not match channels (%d) found in %s\n",
                num_channels_p, cfg->channels, config_p);
@@ -125,6 +134,10 @@ extern "C" bool bsg_dramsim3_init(
         pr_err("data_width_p (%d) does not match product of burst length (%d) and bus width (%d) found in %s\n",
                data_width_p, cfg->BL, cfg->bus_width, config_p);
         bsg_dramsim3_exit();
+        return false; // do I exit?
+    } else if (memory_size_bits != size_p) {
+        pr_err("size_p (%d) does not match device size (%d) found in %s\n",
+               size_p, memory_size_bits, config_p);
         return false; // do I exit?
     }
     return true;
