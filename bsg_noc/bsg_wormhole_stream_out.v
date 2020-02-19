50d652038ef45adb34cdf76081ee95b0682a0d6a
Paul Gao <gaozihou@uw.edu>
2020-02-18 23:50:50 -0800

    add bsg_wormhole_stream_control to avoid code duplication
    Co-authored-by: Dan Petrisko <petrisko@cs.washington.edu>

--- bsg_noc/bsg_wormhole_stream_out.v
+++ bsg_noc/bsg_wormhole_stream_out.v
@@ -27,10 +27,8 @@ module bsg_wormhole_stream_out
    , input                     data_yumi_i
    );
 
-  enum logic [1:0] {e_hdr, e_data} state_n, state_r;
-  wire is_hdr = (state_r == e_hdr);
-  wire is_data = (state_r == e_data);
-
+  wire is_hdr, is_data;
+  
   localparam [len_width_p-1:0] hdr_len_lp = `BSG_CDIV(hdr_width_lp, flit_width_p);
 
   wire                link_accept = link_ready_o & link_v_i;
@@ -96,92 +94,20 @@ module bsg_wormhole_stream_out
          ,.yumi_i(data_yumi_i)
          );
     end
-    
-  wire [len_width_p-1:0] data_len_li = link_data_i[cord_width_p+:len_width_p] - hdr_len_lp + (len_width_p)'(1);
-
-  // count from num_flits to zero, count_r_o==1 means last flit
-  logic [len_width_p-1:0] hdr_flit_cnt, data_flit_cnt;
-
-  // Sending last hdr flit
-  wire hdr_flit_last  = (hdr_flit_cnt  == (len_width_p)'(1));
-  // Sending last data flit
-  wire data_flit_last = (data_flit_cnt == (len_width_p)'(1));
-  // All hdr flits are sent
-  wire hdr_flit_done  = (hdr_flit_cnt  == '0);
-  // All data flits are sent
-  wire data_flit_done = (data_flit_cnt == '0);
-
-  // Set counter value when new packet hdr arrives
-  // and all hdr flits are sent
-  // (set data_flit_counter in same cycle)
-  wire set_counter    = is_hdr & hdr_flit_done & link_accept;
-
-  bsg_counter_set_down
-   #(.width_p(len_width_p)
-     ,.init_val_p(0)
-     // allow set down same cycle to avoid bubble
-     ,.set_and_down_exclusive_p(0)
-     )
-   hdr_flit_counter
-    (.clk_i(clk_i)
-     ,.reset_i(reset_i)
-     ,.set_i(set_counter)
-     ,.val_i(hdr_len_lp)
-     ,.down_i(is_hdr & link_accept)
-     ,.count_r_o(hdr_flit_cnt)
-     );
-
-  bsg_counter_set_down
-   #(.width_p(len_width_p)
-     ,.init_val_p(0)
-     // allow set down same cycle to avoid bubble
-     ,.set_and_down_exclusive_p(0)
-     )
-   data_flit_counter
-    (.clk_i(clk_i)
-     ,.reset_i(reset_i)
-     ,.set_i(set_counter)
-     ,.val_i(data_len_li)
-     ,.down_i(is_data & link_accept)
-     ,.count_r_o(data_flit_cnt)
-     );
-
-  wire e_hdr_to_e_data;
-
-  // Single hdr flit
-  if (hdr_len_lp == 1)
-    // When wormhole link accept flit 
-    // and data flit non-zero
-    // and link_v_i (avoid possible X-pessimism in simulation)
-    //
-    // (data_flit_done signal takes one cycle to be registered, not useful
-    // in this case, extract data_len_li signal directly from hdr)
-    assign e_hdr_to_e_data = (link_accept & (link_v_i & data_len_li != '0));
-  // Multiple hdr flits
-  else
-    // When wormhole link accept flit 
-    // and sending last hdr flit
-    // and data flit non-zero
-    //
-    // (data_len_li signal only meaningful in first hdr flit, not useful
-    // in this case, use registered data_flit_done signal from data_flit_counter)
-    assign e_hdr_to_e_data = (link_accept & hdr_flit_last & ~data_flit_done);
   
-  // When wormhole link accept flit and sending last data flit
-  wire e_data_to_e_hdr = link_accept & data_flit_last;
+  bsg_wormhole_stream_control
+ #(.len_width_p  (len_width_p)
+  ,.hdr_len_p    (hdr_len_lp)
+  ) stream_control
+  (.clk_i        (clk_i)
+  ,.reset_i      (reset_i)
 
-  always_comb
-    case (state_r)
-      e_hdr  : state_n = (e_hdr_to_e_data)? e_data : e_hdr;
-      e_data : state_n = (e_data_to_e_hdr)? e_hdr : e_data;
-      default: state_n = e_hdr;
-    endcase
+  ,.len_i        (link_data_i[cord_width_p+:len_width_p])
+  ,.link_accept_i(link_accept)
 
-  always_ff @(posedge clk_i)
-    if (reset_i)
-      state_r <= e_hdr;
-    else
-      state_r <= state_n;
+  ,.is_hdr_o     (is_hdr)
+  ,.is_data_o    (is_data)
+  );
 
   assign link_ready_o = is_hdr ? hdr_ready_lo : data_ready_lo;
 
