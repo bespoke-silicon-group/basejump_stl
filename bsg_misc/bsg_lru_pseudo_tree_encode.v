974af0e2f7f7a1abfab4591d4974e4faa50eced5
taylor-bsg <taylor-bsg@users.noreply.github.com>
2020-03-11 23:48:19 -0700

    add comment that describes Tommy's picture

--- bsg_misc/bsg_lru_pseudo_tree_encode.v
+++ bsg_misc/bsg_lru_pseudo_tree_encode.v
@@ -40,11 +40,13 @@ module bsg_lru_pseudo_tree_encode
 
     if (i == 0) begin: z
 
+      // top way_id_o bit is just the lowest LRU bit
       assign way_id_o[lg_ways_lp-1] = lru_i[0];  // root
 
     end
     else begin: nz
-
+      // each output way_id_o bit uses *all* of the way_id_o bits above it in the way_id_o vector
+      // as the select to a mux which has as input an exponentially growing (2^i) collection of unique LRU bits
       bsg_mux #(
         .width_p(1)
         ,.els_p(2**i)
