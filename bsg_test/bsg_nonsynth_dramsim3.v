cd4a36817c0472c6589a31dde7af4ab79626c607
Max Ruttenberg <mrutt@cs.washington.edu>
2020-03-19 09:06:40 -0700

    removes unnecessary for-loop; adds a comment

--- bsg_test/bsg_nonsynth_dramsim3.v
+++ bsg_test/bsg_nonsynth_dramsim3.v
@@ -225,13 +225,15 @@ module bsg_nonsynth_dramsim3
   
   end
 
-  logic data_v_r[num_channels_p-1:0];
+  // this register is needed because we use blocking assignment 
+  // for read_done which drives read_v_li.
+  // without this register data_v_o mirrors read_v_li instead
+  // of being its value from the previous cycle.
+  logic [num_channels_p-1:0] data_v_r;
 
   always_ff @(posedge clk_i) begin
-    for (integer i = 0; i < num_channels_p; i++) begin
-      data_v_r[i] <= read_v_li[i];
-      data_v_o[i] <= data_v_r[i];
-    end
+    data_v_r <= read_v_li;
+    data_v_o <= data_v_r;
   end
 
   // debugging
