a4ec4792a09849af9eb576aca2814c96cb13643e
Tommy Jung <tommy.dc.jung@gmail.com>
2020-03-30 08:28:02 -0700

    vector_add

---
+++ testing/bsg_test/dramsim3_bandwidth2/vector_add.py
@@ -0,0 +1,21 @@
+import sys
+from trace_gen import *
+
+if __name__ == "__main__":
+  # common parameters
+  id_p = int(sys.argv[1])
+  num_cache_group_p = int(sys.argv[2])
+  num_subcache_p = int(sys.argv[3])
+  block_size_in_words_p = int(sys.argv[4])
+
+  tg = TraceGen(num_subcache_p, block_size_in_words_p)
+  tg.clear_tags()
+
+  words = (2**18)/num_cache_group_p # 1MB
+  #words = (2**9)/num_cache_group_p # 2KB
+  for i in range(words):
+    tg.send_read(i<<2)
+    tg.send_read((words+i)<<2)
+    tg.send_write(((2*words)+i)<<2)
+
+  tg.done()
