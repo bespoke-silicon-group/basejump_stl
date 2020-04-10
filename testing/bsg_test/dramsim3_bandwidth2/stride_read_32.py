8afc2a6684d89da73f70bf89f70d3a426f7be285
Tommy Jung <tommy.dc.jung@gmail.com>
2020-04-10 12:18:50 -0700

    stride_read

---
+++ testing/bsg_test/dramsim3_bandwidth2/stride_read_32.py
@@ -0,0 +1,18 @@
+import sys
+from trace_gen import *
+
+if __name__ == "__main__":
+  # common parameters
+  id_p = int(sys.argv[1])
+  num_cache_group_p = int(sys.argv[2])
+  num_subcache_p = int(sys.argv[3])
+  block_size_in_words_p = int(sys.argv[4])
+
+  tg = TraceGen(num_subcache_p, block_size_in_words_p)
+  tg.clear_tags()
+
+  words = (2**16)/num_cache_group_p
+  for i in range(words):
+    tg.send_read(i<<(2+5))
+
+  tg.done()
