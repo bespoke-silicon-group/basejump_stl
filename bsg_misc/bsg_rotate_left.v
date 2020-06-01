a172b05d6a932a9e30aee9b5e9503bd511b5d3c1
taylor-bsg <taylor-bsg@users.noreply.github.com>
2020-05-31 22:07:10 -0700

    Add bsg_rotate_left

---
+++ bsg_misc/bsg_rotate_left.v
@@ -0,0 +1,10 @@
+module bsg_rotate_left #(width_p=-1)
+   (input [width_p-1:0] data_i
+    , input [`BSG_SAFE_CLOG2(width_p)-1:0] rot_i
+    , output [width_p-1:0] o
+    );
+   
+  wire [width_p*3-1:0] temp = { 2 { data_i } } << rot_i;
+  assign o = temp[width_p*2-1:width_p];
+   
+endmodule
