aa0e42e006c38d01983ab09f939855e59987392a
Paul Gao <gaozihou@uw.edu>
2020-12-13 22:16:00 -0800

    Add comments

--- bsg_dataflow/bsg_parallel_in_serial_out_passthrough_dynamic.v
+++ bsg_dataflow/bsg_parallel_in_serial_out_passthrough_dynamic.v
@@ -1,22 +1,24 @@
-//
-// This data structure takes in a multi-word data and serializes
-// it to a single word output. This module is helpful on both sides.
-// Note:
-//   A transaction starts when ready_and_o & v_i. data_i must
-//     stay constant for the entirety of the transaction until
-//     ready_and_o is asserted.
-//   This may make the module incompatible with upstream modules that
-//     multiplex multiple inputs and can change which input they multiplex
-//     on the fly based on arrival of new inputs.
-//
-// len_i signal must be valid when ready_and_i and first_o signals are 
-// asserted, together with the first data word of each transaction.
-//
-// By definition of ready-and handshaking, ready_and_o must be earlier than
-// v_i. Since ready_and_o depends on value of len_i, so len_i must not depend
-// on v_i. For this reason, len_i signal is on the serial data side of the
-// module and depends on ready_and_i.
-//
+/**
+ * bsg_parallel_in_serial_out_passthrough_dynamic.v
+ *
+ * This data structure takes in a multi-word parallel input and serializes
+ * it to a single word output. This module is helpful on both sides, both v_o 
+ * and ready_and_o are early.
+ *
+ * Note:
+ *   A transaction starts when ready_and_o & v_i. data_i must
+ *     stay constant for the entirety of the transaction until
+ *     ready_and_o is asserted.
+ *   This may make the module incompatible with upstream modules that
+ *     multiplex multiple inputs and can change which input they multiplex
+ *     on the fly based on arrival of new inputs.
+ *
+ * By definition of ready-and handshaking, ready_and_o must be earlier than
+ * v_i. Since ready_and_o depends on value of len_i, so len_i must not depend
+ * on v_i. For this reason, len_i signal is on the serial data side of the
+ * module and depends on ready_and_i.
+ *
+ */
 
 `include "bsg_defines.v"
 
@@ -38,9 +40,21 @@ module bsg_parallel_in_serial_out_passthrough_dynamic
   // Data Output Channel
   ,output                              v_o
   ,output [width_p-1:0]                data_o
-  ,output                              first_o
-  ,input  [lg_max_els_lp-1:0]          len_i
   ,input                               ready_and_i
+  //
+  // When first_o is asserted, the upcoming data is the first word of a new 
+  // transaction. It can be asserted regardless of value of v_o.
+  ,output                              first_o
+  //
+  // len_i indicates the length (number of data words) of the upcoming new
+  // transaction. User need to guarantee that len_i is valid when first_o and
+  // ready_and_i are both asserted (when dequeueing the first data word of 
+  // the new transaction).
+  //
+  // Note that len_i is represented by (length - 1), in order to use minimum
+  // number of bits. For example, when a 4-word transaction is coming, len_i 
+  // should be set to 3. Single word transaction should have len_i == 0.
+  ,input  [lg_max_els_lp-1:0]          len_i
   );
 
   if (max_els_p == 1)
@@ -59,6 +73,8 @@ module bsg_parallel_in_serial_out_passthrough_dynamic
     logic is_zero_cnt, is_last_cnt, is_zero_len;
     logic en_li, clear_li, up_li;
 
+    // Register the length of upcoming transaction
+    // Add reset to prevent X-pessimism issue in simulation
     bsg_dff_reset_en 
    #(.width_p    (lg_max_els_lp)
     ,.reset_val_p(0            )
@@ -70,6 +86,7 @@ module bsg_parallel_in_serial_out_passthrough_dynamic
     ,.data_o     (len_r        )
     );
 
+    // Count how many words have been sent in each transaction
     bsg_counter_clear_up
    #(.max_val_p (max_els_p-1)
     ,.init_val_p(0          )
@@ -81,18 +98,29 @@ module bsg_parallel_in_serial_out_passthrough_dynamic
     ,.count_o   (count_r    )
     );
 
+    // Zero count means idle state, accepting new transaction
+    // Last count indicates sending last data word of transaction
+    // Zero length means the upcoming new transaction has single data word
     assign is_zero_cnt = (count_r == (lg_max_els_lp)'(0));
     assign is_last_cnt = ~is_zero_cnt & (count_r == len_r);
     assign is_zero_len = ready_and_i & (len_i == (lg_max_els_lp)'(0));
 
+    // Update length when sending the first data word of new transaction
+    // Count up when sending out data words
+    // Clear the counter at the end of each transaction
     assign en_li       = v_o & ready_and_i & is_zero_cnt;
     assign up_li       = v_o & ready_and_i & ~clear_li;
     assign clear_li    = v_i & ready_and_o;
 
+    // Output valid data when input data is valid, there is no registered data
+    // in this module.
     assign v_o         = v_i;
+    // Dequeue incoming parallel data at the end of each transaction.
+    // When incoming data is single-word, dequeue immediately.
     assign ready_and_o = (ready_and_i & is_last_cnt) | is_zero_len;
     assign first_o     = is_zero_cnt;
 
+    // Output data
     bsg_mux
    #(.width_p(width_p  )
     ,.els_p  (max_els_p)
--- bsg_dataflow/bsg_serial_in_parallel_out_passthrough_dynamic.v
+++ bsg_dataflow/bsg_serial_in_parallel_out_passthrough_dynamic.v
@@ -1,8 +1,14 @@
 /**
  * bsg_serial_in_parallel_out_passthrough_dynamic.v
  *
- * len_i signal must be valid when v_i and first_o signals are asserted, 
- * together with the first data word of each transaction.
+ * This data structure takes in single word serial input and deserializes it 
+ * to multi-word data output. This module is helpful on both sides, both v_o 
+ * and ready_and_o are early.
+ *
+ * By definition of ready-and handshaking, v_o must be earlier than 
+ * ready_and_i. Since v_o depends on value of len_i, so len_i must not depend
+ * on ready_and_i. For this reason, len_i signal is on the serial data side of 
+ * the module and depends on v_i.
  *
  */
 
@@ -20,9 +26,21 @@ module bsg_serial_in_parallel_out_passthrough_dynamic
 
   ,input                               v_i
   ,input  [width_p-1:0]                data_i
-  ,input  [lg_max_els_lp-1:0]          len_i
   ,output                              ready_and_o
+  //
+  // When first_o is asserted, the upcoming data is the first word of a new 
+  // transaction. It can be asserted regardless of value of ready_and_o.
   ,output                              first_o
+  //
+  // len_i indicates the length (number of data words) of the upcoming new
+  // transaction. User need to guarantee that len_i is valid when first_o and
+  // v_i are both asserted (when enqueueing the first data word of 
+  // the new transaction).
+  //
+  // Note that len_i is represented by (length - 1), in order to use minimum
+  // number of bits. For example, when a 4-word transaction is coming, len_i 
+  // should be set to 3. Single word transaction should have len_i == 0.
+  ,input  [lg_max_els_lp-1:0]          len_i
 
   ,output                              v_o
   ,output [max_els_p-1:0][width_p-1:0] data_o
@@ -44,9 +62,10 @@ module bsg_serial_in_parallel_out_passthrough_dynamic
     logic [lg_max_els_lp-1:0] count_r, len_r;
     logic is_zero_cnt, is_last_cnt, is_zero_len, is_waiting;
     logic en_li, clear_li, up_li;
-    logic [max_els_p-1:0][width_p-1:0] data_lo;
     logic [max_els_p-1:0] data_en_li;
 
+    // Register the length of upcoming transaction
+    // Add reset to prevent X-pessimism issue in simulation
     bsg_dff_reset_en
    #(.width_p    (lg_max_els_lp)
     ,.reset_val_p(0            )
@@ -58,6 +77,7 @@ module bsg_serial_in_parallel_out_passthrough_dynamic
     ,.data_o     (len_r        )
     );
 
+    // Count how many words have been received in each transaction
     bsg_counter_clear_up
    #(.max_val_p (max_els_p-1)
     ,.init_val_p(0          )
@@ -69,19 +89,42 @@ module bsg_serial_in_parallel_out_passthrough_dynamic
     ,.count_o   (count_r    )
     );
 
+    // Zero count means idle state, accepting new transaction
+    // Last count indicates receiving last data word of transaction
+    // Zero length means the upcoming new transaction has single data word
     assign is_zero_cnt = (count_r == (lg_max_els_lp)'(0));
     assign is_last_cnt = ~is_zero_cnt & (count_r == len_r);
-    assign is_waiting  = ~is_zero_cnt & (len_r == (lg_max_els_lp)'(0));
     assign is_zero_len = v_i & (len_i == (lg_max_els_lp)'(0));
+    //
+    // There is a special case for single word transaction, when v_i==1
+    // and ready_and_i==0, ready_and_o is asserted and data word is stored
+    // into dff_bypass. In next several cycles, the module will "wait" for 
+    // ready_and_i==1 and then proceed to next transaction.
+    //
+    // This does not happen when len_i>0, because ready_and_o will not be 
+    // asserted when ready_and_i==0 and count_r==len_r. len_r is available 
+    // from second word of each transaction. In idle state we cannot use 
+    // len_i directly, because ready_and_o must not depend on len_i, where 
+    // len_i depends on v_i.
+    assign is_waiting  = ~is_zero_cnt & (len_r == (lg_max_els_lp)'(0));
 
+    // Update length when receiving the first data word of new transaction
+    // Count up when receiving data words
+    // Clear the counter at the end of each transaction
     assign en_li       = v_i & ready_and_o & is_zero_cnt;
     assign up_li       = v_i & ready_and_o & ~clear_li;
     assign clear_li    = v_o & ready_and_i;
 
+    // Output valid data after receiving all data words of each transaction
+    // Also valid for special case of single word transation
     assign v_o         = (v_i & is_last_cnt) | is_zero_len | is_waiting;
+    // Dequeue incoming serial data and store in registers. Last data word 
+    // of each transaction is not registered to minimize hardware. Accept no 
+    // data word when waiting to send previous single word transaction. 
     assign ready_and_o = (ready_and_i | ~is_last_cnt) & ~is_waiting;
     assign first_o     = is_zero_cnt;
 
+    // Decide when to update data registers
     bsg_decode_with_v
    #(.num_out_p(max_els_p        )
     ) bdwv
@@ -90,6 +133,7 @@ module bsg_serial_in_parallel_out_passthrough_dynamic
     ,.o        (data_en_li       )
     );
 
+    // Registered data words
     for (genvar i = 0; i < max_els_p-1; i++)
       begin: rof
         bsg_dff_en_bypass
