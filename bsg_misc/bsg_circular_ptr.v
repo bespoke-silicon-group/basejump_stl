35690856e8756c4749b7e392f250d565c43fb9b2
Dustin Richmond <dustinar@uw.edu>
2021-01-08 18:05:35 -0800

    Switch to assert final for bsg_circular_ptr, to satisfy VCS 2020

--- bsg_misc/bsg_circular_ptr.v
+++ bsg_misc/bsg_circular_ptr.v
@@ -69,7 +69,7 @@ module bsg_circular_ptr #(parameter slots_p     = -1
 	  // synopsys translate_off
           always_comb
             begin
-              assert( (ptr_n < slots_p) || (|ptr_n === 'X) || reset_i || (add_i > slots_p))
+              assert final( (ptr_n < slots_p) || (|ptr_n === 'X) || reset_i || (add_i > slots_p))
                 else $error("bsg_circular_ptr counter overflow (ptr_r=%b/add_i=%b/ptr_wrap=%b/ptr_n=%b)",ptr_r,add_i,ptr_wrap,ptr_n, slots_p);
             end
 	  // synopsys translate_on
