23d611ef79a9f7df65a87d7566c03d25aaa7d7e0
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2021-01-21 18:09:11 -0800

    Addressing review comments

--- bsg_mem/bsg_mem_1r1w_from_1rw_sync.v
+++ bsg_mem/bsg_mem_1r1w_from_1rw_sync.v
@@ -5,13 +5,13 @@
 `include "bsg_defines.v"
 
 module bsg_mem_1r1w_from_1rw_sync #(parameter width_p=-1
-                                   ,parameter els_p=-1
-                                   ,parameter read_write_same_addr_p=0
-                                   ,parameter addr_width_lp=`BSG_SAFE_CLOG2(els_p)
-                                   ,parameter harden_p=0
-                                   ,parameter disable_collision_warning_p=1
-                                   ,parameter enable_clock_gating_p=0
-                                   )
+                                        ,parameter els_p=-1
+                                        ,parameter read_write_same_addr_p=0
+                                        ,parameter addr_width_lp=`BSG_SAFE_CLOG2(els_p)
+                                        ,parameter harden_p=0
+                                        ,parameter disable_collision_warning_p=1
+                                        ,parameter enable_clock_gating_p=0
+                                        )
   (input clk_i
   ,input reset_i
   ,input                                        w_v_i
@@ -23,29 +23,45 @@ module bsg_mem_1r1w_from_1rw_sync #(parameter width_p=-1
   );
 
   logic [els_p-1:0] v_r;
-  logic mem_r;
-  wire v_r_wr_ptr = ~r_v_i | ~v_r[r_addr_i];
+  logic [els_p_1:0] set_one_hot_li;
+  logic mem_select_r;
+
+  wire wr_ptr = ~r_v_i | ~v_r[r_addr_i];
+  always_comb
+    begin
+      set_one_hot_li = '0;
+      set_one_hot_li[0] = wr_ptr & w_v_i;
+      set_one_hot_li << w_addr_i;
+    end
   
-  always_ff @(posedge clk_i)
-    if (reset_i)
-      v_r <= '0;
-    else if (w_v_i)
-      v_r[w_addr_i] <= v_r_wr_ptr;
+  genvar i;
+  for(i = 0; i < els_p; i++)
+    begin: rof
+      bsg_dff_reset_set_clear
+        #(.width_p(els_p))
+        valid_reg
+        (.clk_i(clk_i)
+        ,.reset_i(reset_i)
+        ,.set_i(set_one_hot_li[i])
+        ,.clear_i('0)
+        ,.data_o(v_r[i])
+        );
+    end
 
   bsg_dff
     #(.width_p(1))
-    mem_reg
+    mem_select_reg
     (.clk_i(clk_i)
     ,.data_i(v_r[r_addr_i])
-    ,.data_o(mem_r)
+    ,.data_o(mem_select_r)
     );
 
-  wire [1:0] w_li    = {w_v_i & v_r_wr_ptr, w_v_i & ~v_r_wr_ptr};
+  wire [1:0] w_li    = {w_v_i & wr_ptr, w_v_i & ~wr_ptr};
   wire [1:0] v_li    = {(r_v_i & v_r[r_addr_i]) | w_li[1], (r_v_i & ~v_r[r_addr_i]) | w_li[0]};
   wire [1:0][addr_width_lp-1:0]
              addr_li = {w_li[1] ? w_addr_i : r_addr_i, w_li[0] ? w_addr_i : r_addr_i};
   logic [1:0][width_p-1:0] data_lo;
-  assign r_data_o    = mem_r ? data_lo[1] : data_lo[0];
+  assign r_data_o    = mem_select_r ? data_lo[1] : data_lo[0];
 
    bsg_mem_1rw_sync
     #(.width_p(width_p)
