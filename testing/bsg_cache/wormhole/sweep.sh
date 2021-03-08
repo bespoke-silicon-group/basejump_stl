bfa4a85f461039a6b067d4d00e100952169eee84
Dan Petrisko <petrisko@cs.washington.edu>
2021-03-08 01:09:57 -0800

    Adding sweep and removing unneccessary tests

--- testing/bsg_cache/wormhole/sweep.sh
+++ testing/bsg_cache/wormhole/sweep.sh
@@ -8,19 +8,25 @@ run_test() {
             DMA_READ_DELAY_P=$3  \
             DMA_WRITE_DELAY_P=$4 \
             DMA_REQ_DELAY_P=$5   \
-            DMA_DATA_DELAY_P=$6
+            DMA_DATA_DELAY_P=$6 \
+            NUM_DMA_P=$7 \
+            DMA_RATIO_P=$8 \
+
+
   echo "################" >> test_result.log
-  echo $1 $2 $3 $4 $5 $6 >> test_result.log
+  echo $1 $2 $3 $4 $5 $6 $7 $8 >> test_result.log
   make summary >> test_result.log
 }
 
-run_test 0 0 0 0 0 0
-run_test 0 2 0 0 0 0
-run_test 0 4 0 0 0 0
-
-run_test 0 0 16 16 4 4
-run_test 0 2 16 16 4 4
-run_test 0 4 16 16 4 4
+run_test 0 4 16 16 4 4 2 1
+run_test 0 4 16 16 4 4 2 2
+run_test 0 4 16 16 4 4 2 4
+run_test 0 4 16 16 4 4 3 1
+run_test 0 4 16 16 4 4 3 2
+run_test 0 4 16 16 4 4 3 4
+run_test 0 4 16 16 4 4 4 1
+run_test 0 4 16 16 4 4 4 2
+run_test 0 4 16 16 4 4 4 4
 
 if grep -H --color -e "BSG_FATAL" -e "Error" -e "BSG_ERROR" test_result.log; then
   echo test failed.
