6848f70d420c4686cc6b13dc5efa1bba4eaa9771
Dan Petrisko <petrisko@cs.washington.edu>
2021-03-14 01:24:21 -0800

    Adding bsg_dramsim3_lpddr_2Gb_x16_pkg
    This is the configuration we validated on FPGA DRAMSim3 for BP

--- bsg_test/bsg_dramsim3_pkg.v
+++ bsg_test/bsg_dramsim3_pkg.v
@@ -82,3 +82,26 @@ package bsg_dramsim3_lpddr3_8gb_x32_1600_pkg;
   } dram_ch_addr_s;
 
 endpackage // bsg_dramsim3_lpddr3_8gb_x32_1600_pkg
+
+package bsg_dramsim3_lpddr_2Gb_x16_pkg;
+  parameter int tck_ps=4800;
+  parameter int channel_addr_width_p=30;
+  parameter int data_width_p=512;
+  parameter int num_channels_p=1;
+  parameter int num_columns_p=256;
+  parameter int num_rows_p=16384;
+  parameter int num_ba_p=4;
+  parameter int num_bg_p=1;
+  parameter int num_ranks_p=1;
+  parameter longint size_in_bits_p=2**33; // 1GB (8Gb)
+  parameter string config_p="lpddr_2Gb_x16.ini";
+  parameter address_mapping_p=bsg_dramsim3_pkg::e_ro_ch_ra_ba_bg_co;
+  
+  typedef struct packed {
+    logic [$clog2(num_rows_p)-1:0] ro;
+    logic [$clog2(num_ba_p)-1:0] ba;
+    logic [$clog2(num_columns_p)-1:0] co;
+    logic [$clog2(data_width_p>>3)-1:0] byte_offset;
+  } dram_ch_addr_s;
+  
+endpackage // bsg_dramsim3_lpddr_2Gb_x16_pkg
