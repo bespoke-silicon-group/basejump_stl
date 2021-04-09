656357113137892530a7dd94e08284e4796de58d
Songchun <rafael.fc.lee@outlook.com>
2021-04-08 21:20:13 -0700

    Updating the test checking data zero-out with  AALLOCZ

--- testing/bsg_cache/regression_v2/test_aallocz1.py
+++ testing/bsg_cache/regression_v2/test_aallocz1.py
@@ -1,30 +1,27 @@
 import random
 from test_base import *
 
-class TestAallocZero(TestBase):
+class TestAallocz1(TestBase):
   # This test make sure the original data in the allocated cache line is zeroed out.
-  # The test result makes sense only when parameter alloc_zero_p == 1,
-  # and this test is not included in general all tests
 
   def generate(self):
     self.clear_tag()
  
-    # write some initial data into all cache line,
-    # which will be evicted later
-    for index_s in range(128):
+    # write 2 cache page of data
+    for index_s in range(self.sets_p*2):
       for tag_s in range(8):
           taddr = self.get_addr(tag_s, index_s)
           self.send_store_block(taddr)
 
-    # 0   <= index_a <=  63: cache allocate miss, with dirty data writeback and dirty data zero-out
-    for index_a in range(128):
-      for tag_a in range(8,16):
+    # Allocate the 1st page, allocate miss, with dirty data writeback and data zero-out
+    for index_a in range(self.sets_p):
+      for tag_a in range(8):
         taddr = self.get_addr(tag_a, index_a)
-        self.send_aalloc(taddr)
+        self.send_aallocz(taddr)
 
     # Read the cache line where there should only be zeros
-    for index_l in range(128):
-      for tag_l in range(8,16):
+    for index_l in range(self.sets_p):
+      for tag_l in range(8):
           taddr = self.get_addr(tag_l, index_l)
           self.send_load_block(taddr)
 
@@ -34,5 +31,5 @@ class TestAallocZero(TestBase):
 
 # main()
 if __name__ == "__main__":
-  t = TestAallocZero()
+  t = TestAallocz1()
   t.generate()
