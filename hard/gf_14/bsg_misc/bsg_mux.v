0a4c81ffb1c2e6b27ba888c34cb25fdcdab685c5
Scott Davidson <scottdavidson1992@gmail.com>
2021-05-03 14:13:09 -0700

    Changing the special DONT_TOUCH instance names to BSG_DONT_TOUCH to be more consistent with other special variables in our cad flows. This also makes it more clear that this is a BSG feature and not something that EDA tools support by default. (#431)

--- hard/gf_14/bsg_misc/bsg_mux.v
+++ hard/gf_14/bsg_misc/bsg_mux.v
@@ -19,7 +19,7 @@ module bsg_mux #(parameter width_p="inv"
         for (j = 0; j < width_p; j=j+1)
           begin: rof
              // fast, but not too extreme
-             SC7P5T_MUX4X4_SSC16L BSG_BAL41MUX_DONT_TOUCH (.D0(data_i[0][j]), .D1(data_i[1][j]), .D2(data_i[2][j]), .D3(data_i[3][j]), .S0(sel_i[0]), .S1(sel_i[1]), .Z(data_o[j]));
+             SC7P5T_MUX4X4_SSC16L BSG_BAL41MUX_BSG_DONT_TOUCH (.D0(data_i[0][j]), .D1(data_i[1][j]), .D2(data_i[2][j]), .D3(data_i[3][j]), .S0(sel_i[0]), .S1(sel_i[1]), .Z(data_o[j]));
           end
      end
    else
