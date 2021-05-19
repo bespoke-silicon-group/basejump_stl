e9a502b30da83457fcee0af7b2ce147b7899012f
Tommy Jung <46542701+tommydcjung@users.noreply.github.com>
2021-05-18 21:47:46 -0700

    x-pessimism issue in bsg_mesh_router_decoder_dor (#437)

--- bsg_noc/bsg_mesh_router.v
+++ bsg_noc/bsg_mesh_router.v
@@ -69,6 +69,8 @@ module bsg_mesh_router
   logic [dirs_lp-1:0][dirs_lp-1:0] req, req_t;
 
   for (genvar i = 0; i < dirs_lp; i++) begin: dor
+    logic [dirs_lp-1:0] temp_req;
+
     bsg_mesh_router_decoder_dor #(
       .x_cord_width_p(x_cord_width_p)
       ,.y_cord_width_p(y_cord_width_p)
@@ -81,14 +83,15 @@ module bsg_mesh_router
     ) dor_decoder (
       .clk_i(clk_i)
       ,.reset_i(reset_i)
-      ,.v_i(v_i[i])
       ,.x_dirs_i(x_dirs[i])
       ,.y_dirs_i(y_dirs[i])
       ,.my_x_i(my_x_i)
       ,.my_y_i(my_y_i)
 
-      ,.req_o(req[i])
+      ,.req_o(temp_req)
     );
+    
+    assign req[i] = {dirs_lp{v_i[i]}} & temp_req;
   end
 
   bsg_transpose #(
