f0f89d924dc996f8a246bce5d1c6f104b081ef4e
Dan Petrisko <petrisko@cs.washington.edu>
2021-06-17 01:01:41 -0700

    Adding pinmissing lint exception for BSG_ABSTRACT_MODULE

--- bsg_misc/bsg_defines.v
+++ bsg_misc/bsg_defines.v
@@ -18,7 +18,10 @@
 //    module name. Such an instance is called a top-level instance."
 //  
 
-`define BSG_ABSTRACT_MODULE(fn) module fn``__abstract(); if (0) fn not_used(); endmodule
+`define BSG_ABSTRACT_MODULE(fn) \
+    /*verilator lint_off PINMISSING*/ \
+    module fn``__abstract(); if (0) fn not_used(); endmodule \
+    /*verilator lint_on PINMISSING*/
 
 // macro for defining invalid parameter; with the abstract module declaration
 // it should be sufficient to omit the "inv" but we include this for tool portability
