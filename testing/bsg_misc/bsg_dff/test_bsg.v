4f0ea929791a70b0c93fa6ddf14ec7dd7b029573
adithyasunil26 <adithyasunil26@gmail.com>
2021-06-17 20:24:26 +0400

    parameter name fix

--- testing/bsg_misc/bsg_dff/test_bsg.v
+++ testing/bsg_misc/bsg_dff/test_bsg.v
@@ -26,10 +26,30 @@ module test_bsg
   wire clk;
   wire reset;
 
+<<<<<<< HEAD
   bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_p)
                           )  clock_gen
                           (  .o(clk)
                           );
+||||||| merged common ancestors
+  `ifdef VERILATOR
+    bsg_nonsynth_dpi_clock_gen
+  `else
+    bsg_nonsynth_clock_gen
+  `endif
+   #(.cycle_time_p(sim_clk_period))
+   clock_gen
+    (.o(clk));
+=======
+  `ifdef VERILATOR
+    bsg_nonsynth_dpi_clock_gen
+  `else
+    bsg_nonsynth_clock_gen
+  `endif
+   #(.cycle_time_p(sim_clk_period_p))
+   clock_gen
+    (.o(clk));
+>>>>>>> parameter name fix
 
   bsg_nonsynth_reset_gen #(  .num_clocks_p     (1)
                            , .reset_cycles_lo_p(reset_cycles_lo_p)
