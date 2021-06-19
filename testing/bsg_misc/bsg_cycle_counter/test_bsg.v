9200c2de75d172a6041f24260b806eb424d228aa
adithyasunil26 <adithyasunil26@gmail.com>
2021-06-19 23:56:50 +0400

    bsg_cycle_counter & bsg_dff_en_reset

--- testing/bsg_misc/bsg_cycle_counter/test_bsg.v
+++ testing/bsg_misc/bsg_cycle_counter/test_bsg.v
@@ -1,39 +1,41 @@
 `define WIDTH_P 2
 
 /**************************** TEST RATIONALE *******************************
-
 1. STATE SPACE
-
   An internal counter keeps track of the count which is compared with the 
   output of the DUT. This test continues for 2**WIDTH_P cycles and hence 
   tests the DUT exhaustively.
-
 2. PARAMETERIZATION
-
   A minimum set of tests might be WIDTH_P=1,2,..,8. Since the number of test
   cases grows exponentially, tests with large WIDTH_P takes very long to
   finish.
-
 ***************************************************************************/
 
-module test_bsg;
-  
-  localparam cycle_time_lp = 20;
-  localparam width_lp = `WIDTH_P;
+module test_bsg
+#(  
+  parameter cycle_time_p = 20,
+  parameter width_p = `WIDTH_P,
+  parameter reset_cycles_lo_p=1,
+  parameter reset_cycles_hi_p=5
+);
 
   wire clk;
   wire reset;
   
-  bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_lp)
-                          )  clock_gen
-                          (  .o(clk)
-                          );
+  `ifdef VERILATOR
+    bsg_nonsynth_dpi_clock_gen
+  `else
+    bsg_nonsynth_clock_gen
+  `endif
+   #(.cycle_time_p(cycle_time_p))
+   clock_gen
+    (.o(clk));
     
   bsg_nonsynth_reset_gen #(  .num_clocks_p     (1)
-                           , .reset_cycles_lo_p(1)
-                           , .reset_cycles_hi_p(5)
+                           , .reset_cycles_lo_p(reset_cycles_lo_p)
+                           , .reset_cycles_hi_p(reset_cycles_lo_p)
                           )  reset_gen
-                          (  .clk_i        (clk) 
+                          (  .clk_i        (clk)
                            , .async_reset_o(reset)
                           );
 
@@ -42,12 +44,12 @@ module test_bsg;
     $display("\n\n\n");
     $display(  "=============================================================\n"
              , "testing with ...\n"
-             , "WIDTH_P: %d\n", width_lp
+             , "WIDTH_P: %d\n", width_p
             );
   end
                                         
-  logic [width_lp-1:0] count, count_r;
-  wire  [width_lp-1:0] test_output;
+  logic [width_p-1:0] count, count_r;
+  wire  [width_p-1:0] test_output;
   
   always_ff @(posedge clk)
   begin
@@ -63,8 +65,8 @@ module test_bsg;
   always_ff @(posedge clk)
   begin
     if(!reset)
-      assert(count == test_output)
-        else $error("mismatch on clock cycle %x", count);
+      if(count != test_output)
+        $error("mismatch on clock cycle %x", count);
     
     if(!(|count) & (&count_r)) // finish when count value returns to 0
       begin
@@ -73,14 +75,14 @@ module test_bsg;
       end
   end
   
-  bsg_cycle_counter #(  .width_p(width_lp)
+  bsg_cycle_counter #(  .width_p(width_p)
                      )  DUT
                      (  .clk_i    (clk)
                       , .reset_i(reset)
                       , .ctr_r_o(test_output)
                      );
                              
-  /*bsg_nonsynth_ascii_writer #(  .width_p      (width_lp)
+  /*bsg_nonsynth_ascii_writer #(  .width_p      (width_p)
                               , .values_p     (2)
                               , .filename_p   ("output.log")
                               , .fopen_param_p("a+")
