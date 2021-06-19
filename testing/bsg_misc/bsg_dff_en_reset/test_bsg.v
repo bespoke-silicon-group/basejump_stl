31c559230acffdfccc14180ee5ce8a3748b65ebb
adithyasunil26 <adithyasunil26@gmail.com>
2021-06-19 23:56:50 +0400

    bsg_cycle_counter & bsg_dff_en_reset

--- testing/bsg_misc/bsg_dff_en_reset/test_bsg.v
+++ testing/bsg_misc/bsg_dff_en_reset/test_bsg.v
@@ -24,10 +24,26 @@ module test_bsg
   wire clk;
   wire reset;
   
+<<<<<<< HEAD
   bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_p)
                           )  clock_gen
                           (  .o(clk)
                           );
+||||||| merged common ancestors
+  bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_lp)
+                          )  clock_gen
+                          (  .o(clk)
+                          );
+=======
+  `ifdef VERILATOR
+    bsg_nonsynth_dpi_clock_gen
+  `else
+    bsg_nonsynth_clock_gen
+  `endif
+   #(.cycle_time_p(cycle_time_p))
+   clock_gen
+    (.o(clk));
+>>>>>>> bsg_cycle_counter & bsg_dff_en_reset
     
   bsg_nonsynth_reset_gen #(  .num_clocks_p     (1)
                            , .reset_cycles_lo_p(reset_cycles_lo_p)
