a8f00592435706486cf9baed214c45eb40b3467b
adithyasunil26 <adithyasunil26@gmail.com>
2021-06-20 13:37:12 +0400

    bsg_mux, bsg_mux_bitwise & bsg_mux_one_hot

--- testing/bsg_misc/bsg_mux_bitwise/test_bsg.v
+++ testing/bsg_misc/bsg_mux_bitwise/test_bsg.v
@@ -17,23 +17,29 @@
 
 ***************************************************************************/
 
-module test_bsg;
-  
-  // clock and reset generation
-  localparam cycle_time_lp = 20;
-  localparam width_lp = `WIDTH_P; // width of test input
-  
+module test_bsg
+#(
+  parameter cycle_time_p = 20,
+  parameter width_p = `WIDTH_P,
+  parameter reset_cycles_lo_p=0,
+  parameter reset_cycles_hi_p=5
+);
+
   wire clk;
   wire reset;
   
-  bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_lp)
-                          )  clock_gen
-                          (  .o(clk)
-                          );
+  `ifdef VERILATOR
+    bsg_nonsynth_dpi_clock_gen
+  `else
+    bsg_nonsynth_clock_gen
+  `endif
+   #(.cycle_time_p(cycle_time_p))
+   clock_gen
+    (.o(clk));
     
   bsg_nonsynth_reset_gen #(  .num_clocks_p     (1)
-                           , .reset_cycles_lo_p(1)
-                           , .reset_cycles_hi_p(5)
+                           , .reset_cycles_lo_p(reset_cycles_lo_p)
+                           , .reset_cycles_hi_p(reset_cycles_hi_p)
                           )  reset_gen
                           (  .clk_i        (clk) 
                            , .async_reset_o(reset)
@@ -43,10 +49,10 @@ module test_bsg;
     $display("\n\n\n");
     $display("===========================================================");
     $display("testing with ...");
-    $display("WIDTH_P: %d\n", width_lp);
+    $display("WIDTH_P: %d\n", width_p);
   end 
                                         
-  logic [width_lp-1:0] test_input_A, 
+  logic [width_p-1:0] test_input_A, 
                        test_input_B, 
                        test_input_sel, 
                        test_output, 
@@ -56,7 +62,7 @@ module test_bsg;
   begin
     if(reset)
       begin
-        test_input_A <= {width_lp{1'b1}};
+        test_input_A <= {width_p{1'b1}};
         test_input_B <= 0;
         test_input_sel <= 0;
       end
@@ -69,8 +75,8 @@ module test_bsg;
   always_ff @(posedge clk)
   begin
     if(!reset)  
-      assert (test_output==test_input_sel)
-        else $error("mismatch on input %x", test_input_sel);
+      if (test_output!=test_input_sel)
+        $error("mismatch on input %x", test_input_sel);
     
     /*$display("\ntest_input_A: %b test_input_B: %b test_input_sel: %b test_output: %b"
              , test_input_A, test_input_B, test_input_sel, test_output);*/
@@ -83,7 +89,7 @@ module test_bsg;
   end
   
   bsg_mux_bitwise #(
-    .width_p(width_lp)
+    .width_p(width_p)
   )  DUT (
     .data0_i(test_input_A)
     ,.data1_i(test_input_B)
