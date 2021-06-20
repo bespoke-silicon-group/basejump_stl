a99f0e15cef35265de81e5cf8359f70e28493692
adithyasunil26 <adithyasunil26@gmail.com>
2021-06-20 13:37:12 +0400

    bsg_mux, bsg_mux_bitwise & bsg_mux_one_hot

--- testing/bsg_misc/bsg_mux/test_bsg.v
+++ testing/bsg_misc/bsg_mux/test_bsg.v
@@ -34,10 +34,26 @@ module test_bsg
   wire clk;
   wire reset;
   
+<<<<<<< HEAD
   bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_p)
                           )  clock_gen
                           (  .o(clk)
                           );
+||||||| merged common ancestors
+  bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_lp)
+                          )  clock_gen
+                          (  .o(clk)
+                          );
+=======
+  `ifdef VERILATOR
+    bsg_nonsynth_dpi_clock_gen
+  `else
+    bsg_nonsynth_clock_gen
+  `endif
+   #(.cycle_time_p(cycle_time_p))
+   clock_gen
+    (.o(clk));
+>>>>>>> bsg_mux, bsg_mux_bitwise & bsg_mux_one_hot
     
   bsg_nonsynth_reset_gen #(  .num_clocks_p     (1)
                            , .reset_cycles_lo_p(reset_cycles_lo_p)
