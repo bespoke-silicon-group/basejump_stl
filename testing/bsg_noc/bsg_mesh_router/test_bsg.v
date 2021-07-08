471a4f09e9e295f6390f79c8b40f630a5be3ec3c
adithyasunil26 <adithyasunil26@gmail.com>
2021-07-08 16:34:05 +0400

    bsg_noc

--- testing/bsg_noc/bsg_mesh_router/test_bsg.v
+++ testing/bsg_noc/bsg_mesh_router/test_bsg.v
@@ -26,21 +26,29 @@ import bsg_noc_pkg::Dirs
        , bsg_noc_pkg::S; // south
 
 
-module test_bsg;
-  
+module test_bsg
+#(
+  parameter cycle_time_p = 20,
+  parameter reset_cycles_lo_p=1,
+  parameter reset_cycles_hi_p=5
+);
+
   // clock and reset generation
-  localparam cycle_time_lp = 20; 
   wire clk;
   wire reset;
 
-  bsg_nonsynth_clock_gen #(  .cycle_time_p(cycle_time_lp)
-                          )  clock_gen
-                          (  .o(clk)
-                          );
+  `ifdef VERILATOR
+    bsg_nonsynth_dpi_clock_gen
+  `else
+    bsg_nonsynth_clock_gen
+  `endif
+   #(.cycle_time_p(cycle_time_p))
+   clock_gen
+    (.o(clk));
     
   bsg_nonsynth_reset_gen #(  .num_clocks_p     (1)
-                           , .reset_cycles_lo_p(1)
-                           , .reset_cycles_hi_p(5)
+                           , .reset_cycles_lo_p(reset_cycles_lo_p)
+                           , .reset_cycles_hi_p(reset_cycles_hi_p)
                           )  reset_gen
                           (  .clk_i        (clk) 
                            , .async_reset_o(reset)
@@ -307,14 +315,14 @@ module test_bsg;
                     );
             test_output_data_r <= test_output_data[i][P];
             
-            assert(test_output_data[i][P][0+:(lg_node_x_lp+lg_node_y_lp)] == i)
-              else $error("received at tile: %0d, should go to tile: %0d"
-                          ,i
-                          ,test_output_data[i][P][0+:(lg_node_x_lp+lg_node_y_lp)]
-                         );
+            if(test_output_data[i][P][0+:(lg_node_x_lp+lg_node_y_lp)] != i)
+              $error("received at tile: %0d, should go to tile: %0d"
+                     ,i
+                     ,test_output_data[i][P][0+:(lg_node_x_lp+lg_node_y_lp)]
+                    );
 
-            assert((test_output_data_r!=test_output_data[i][P]) | output_count[i]==0)
-              else $error("packet received at tile %d is not unique", i);
+            if((test_output_data_r!=test_output_data[i][P]) | output_count[i]!=0)
+              $error("packet received at tile %d is not unique", i);
 
             output_count[i] <= output_count[i] + 1;
             
