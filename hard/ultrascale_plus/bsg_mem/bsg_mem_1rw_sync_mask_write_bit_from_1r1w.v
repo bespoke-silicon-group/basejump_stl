d6437b4290fd1f18dc7edcb2f46bc749b28289d1
mysoreanoop <mnanoop2014@gmail.com>
2021-08-06 11:28:27 +0530

    PR update N2

--- hard/ultrascale_plus/bsg_mem/bsg_mem_1rw_sync_mask_write_bit_from_1r1w.v
+++ hard/ultrascale_plus/bsg_mem/bsg_mem_1rw_sync_mask_write_bit_from_1r1w.v
@@ -55,8 +55,6 @@ module bsg_mem_1rw_sync_mask_write_bit_from_1r1w #(
     assert(int'(addr_i) < els_p) else $warning("%m Accessing uninitialized address!");
   // synopsys translate_on
 
-  logic [width_m1_lp:0]     bypass_data_r;
-
   wire                      v_and_w_r;
 
   bsg_dff
@@ -67,9 +65,8 @@ module bsg_mem_1rw_sync_mask_write_bit_from_1r1w #(
     ,.data_o (v_and_w_r)
     );
 
-  wire [addr_width_lp-1:0]  addr_r;
-  wire                      haz = (addr_r == addr_i);
-  wire                      bypass_n = haz & v_and_w_r;
+  wire [addr_width_lp-1:0]  w_addr_r;
+  wire                      bypass_n = v_and_w_r & (w_addr_r == addr_i);
   wire                      bypass_r;
 
   bsg_dff_en
@@ -78,21 +75,10 @@ module bsg_mem_1rw_sync_mask_write_bit_from_1r1w #(
     (.clk_i  (clk_i)
     ,.en_i   (v_i)
     ,.data_i ({addr_i, bypass_n})
-    ,.data_o ({addr_r, bypass_r})
+    ,.data_o ({w_addr_r, bypass_r})
     );
 
   wire [width_m1_lp:0]      w_mask_r;
-  wire                      w_r;
-
-  bsg_dff_reset_en
-    #(.width_p(1))
-    common_reset_regs
-    (.clk_i  (clk_i)
-    ,.reset_i(reset_i)
-    ,.en_i   (v_i)
-    ,.data_i (w_i)
-    ,.data_o (w_r)
-    );
 
   bsg_dff_en 
     #(.width_p(width_p))
@@ -105,16 +91,25 @@ module bsg_mem_1rw_sync_mask_write_bit_from_1r1w #(
 
   wire  [width_m1_lp:0]     mem_data_lo;
 
+  logic [width_m1_lp:0]     bypass_data_r;
+
   // If WAW, write bypass_data_r which incorporates both updates
   // else write updated mem_data_lo
-  wire  [width_m1_lp:0] w_data_li = (bypass_r & v_and_w_r)
+  wire  [width_m1_lp:0] w_data_li = bypass_r
           ? bypass_data_r : (mem_data_lo & ~w_mask_r) | (bypass_data_r & w_mask_r);
 
-  // Read if either RAR/WAR hazard or not a hazard
-  wire r_en_li = v_i & (haz & ~v_and_w_r | ~haz);
+  always_ff @(posedge clk_i)
+    if(~reset_i)
+      bypass_data_r <= (v_and_w_r & bypass_n)
+                 ? (w_i
+                   ? (w_data_li & ~w_mask_i) | (data_i & w_mask_i)
+                   : w_data_li)
+                 : data_i;
 
+  // Read if either RAR/WAR hazard or not a hazard
   // Write previous if not a write hazard currently
-  wire w_en_li = v_and_w_r & ~(v_i & w_i & haz);
+  wire r_en_li = v_i & ~bypass_n;
+  wire w_en_li = v_and_w_r;
 
   bsg_mem_1r1w_sync
     #(.width_p (width_p)
@@ -123,39 +118,40 @@ module bsg_mem_1rw_sync_mask_write_bit_from_1r1w #(
     (.clk_i    (clk_i)
     ,.reset_i  (reset_i)
     ,.w_v_i    (w_en_li)
-    ,.w_addr_i (addr_r)
+    ,.w_addr_i (w_addr_r)
     ,.w_data_i (w_data_li)
     ,.r_v_i    (r_en_li)
     ,.r_addr_i (addr_i)
     ,.r_data_o (mem_data_lo)
     );
 
-  always_ff @(posedge clk_i)
-    if(~reset_i)
-      bypass_data_r <= (w_r & haz) 
-                 ? (w_i
-                   ? (w_data_li & ~w_mask_i) | (data_i & w_mask_i)
-                   : w_data_li)
-                 : data_i;
-
   wire [width_m1_lp:0] data_o_latchable = bypass_r ? bypass_data_r : mem_data_lo;
   
   // synopsys translate_off
   always_ff @(posedge clk_i)
     if(verbose_p==1)
-      $display("w_en %b | r_en %b | w_data_li %b | mem_data_lo %b | addr_r %06h"
-              , w_en_li, r_en_li, w_data_li, mem_data_lo, addr_r
+      $display("w_en %b | r_en %b | w_data_li %b | mem_data_lo %b | w_addr_r %06h"
+              , w_en_li, r_en_li, w_data_li, mem_data_lo, w_addr_r
               , "| bypass_data_r %b | bypass_r %b|"
               , bypass_data_r, bypass_r);
   // synopsys translate_on
 
   if (latch_last_read_p)
     begin: llr
-      bsg_dff_en_bypass #(
-        .width_p (width_p)
-      ) dff_bypass (
-        .clk_i   (clk_i)
-        ,.en_i   (~w_r)
+      logic read_en_r;
+      bsg_dff
+        #(.width_p(1))
+        read_en_dff
+        (.clk_i  (clk_i)
+        ,.data_i (v_i & ~w_i)
+        ,.data_o (read_en_r)
+        );
+
+      bsg_dff_en_bypass 
+        #(.width_p (width_p)) 
+        dff_bypass 
+        (.clk_i   (clk_i)
+        ,.en_i   (read_en_r)
         ,.data_i (data_o_latchable)
         ,.data_o (data_o)
       );
@@ -167,4 +163,4 @@ module bsg_mem_1rw_sync_mask_write_bit_from_1r1w #(
 
 endmodule
 
-`BSG_ABSTRACT_MODULE(bsg_mem_1rw_sync_mask_write_bit_from_1r1w)
+//`BSG_ABSTRACT_MODULE(bsg_mem_1rw_sync_mask_write_bit_from_1r1w)
