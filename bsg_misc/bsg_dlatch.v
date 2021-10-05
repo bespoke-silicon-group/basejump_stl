728c6a7b8b3f2794b93bdf31fa16097a450f2882
Dan Petrisko <petrisko@cs.washington.edu>
2021-10-05 05:02:46 -0700

    Fixing latch for verilator

--- bsg_misc/bsg_dlatch.v
+++ bsg_misc/bsg_dlatch.v
@@ -11,11 +11,14 @@ module bsg_dlatch #(parameter `BSG_INV_PARAM(width_p)
   if (i_know_this_is_a_bad_idea_p == 0)
     $fatal( 1, "Error: you must admit this is a bad idea before you are allowed to use the bsg_dlatch module!" );
 
+  logic [width_p-1:0] data_r;
   always_latch
     begin
       if (clk_i)
-        data_o <= data_i;
+        data_r <= data_i;
     end
+
+  assign data_o = data_r;
 
 endmodule
 
