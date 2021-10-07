b37733366a04bebb10656762d5bba472d9ee4544
Dan Petrisko <petrisko@cs.washington.edu>
2021-10-07 15:46:20 -0700

    Fixing latch for verilator (#501)
    * Fixing latch for verilator
    
    * comment

--- bsg_misc/bsg_latch.v
+++ bsg_misc/bsg_latch.v
@@ -16,6 +16,8 @@ module bsg_latch
     if (clk_i)
       data_r <= data_i;
 
+  // Version 4.213 of Verilator doesn't detect latches
+  //   for data_o merged with data_r
   assign data_o = data_r;
   
 endmodule
