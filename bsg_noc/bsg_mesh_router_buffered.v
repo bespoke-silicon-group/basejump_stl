cd2b59a8431d0f0d17617d22bc63090bc06c64e8
Dan Petrisko <petrisko@cs.washington.edu>
2021-10-07 16:46:26 -0700

    Changing info in bsg_mesh_router_buffered to assertion (#500)
    * Changing to assertion
    
    * Fixing typo and adding reset assertion
    
    * Changing reset block

--- bsg_noc/bsg_mesh_router_buffered.v
+++ bsg_noc/bsg_mesh_router_buffered.v
@@ -72,8 +72,8 @@ module bsg_mesh_router_buffered
 
       // synopsys translate_off
       always @(negedge clk_i)
-        if (link_o_cast[i].v)
-          $display("## warning %m: stubbed port %x received word %x",i,link_i_cast[i].data);
+        assert (reset_i !== '0 || ~link_o_cast[i].v) else
+          $warning("## stubbed port %x received word %x",i,link_i_cast[i].data);
       // synopsys translate_on
     end
     else begin: fi
