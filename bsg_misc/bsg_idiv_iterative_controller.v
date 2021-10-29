8825b86558dfb4d4e876b5ac4f17c1adcc303e21
barnold3 <60902213+barnold3@users.noreply.github.com>
2021-10-28 23:13:05 -0700

    Update bsg_idiv_iterative_controller.v

--- bsg_misc/bsg_idiv_iterative_controller.v
+++ bsg_misc/bsg_idiv_iterative_controller.v
@@ -114,8 +114,8 @@ module bsg_idiv_iterative_controller #(parameter width_p=32)
           opA_sel_o    = 2'b10;
           opC_sel_o    = 3'b100;
        end
-       opA_sel_o    = 2'b10;
-       opC_sel_o    = 3'b100;
+       opA_sel_o    = 2'b00;
+       opC_sel_o    = 3'b000;
     end
     
     NEG0: begin
