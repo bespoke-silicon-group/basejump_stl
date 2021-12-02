1ea05ef48e8c5d879defcfb9de695ba1c407a0c8
Dan Petrisko <petrisko@cs.washington.edu>
2021-12-02 15:57:10 -0800

    Separating master/client

--- bsg_link/bsg_link_pkg.v
+++ bsg_link/bsg_link_pkg.v
@@ -5,13 +5,23 @@ package bsg_link_pkg;
 
   typedef struct packed
   {
+    bsg_tag_s uplink_reset;
+    bsg_tag_s downlink_reset;
+    bsg_tag_s downstream_reset;
+    bsg_tag_s token_reset;
+  }  bsg_link_sdr_client_tag_lines_s;
+  localparam bsg_link_sdr_client_tag_local_els_gp =
+	$bits(bsg_link_sdr_tag_lines_s) / $bits(bsg_tag_s);
+
+  typedef struct packed
+  {
     bsg_tag_s sdr_disable;
     bsg_tag_s uplink_reset;
     bsg_tag_s downlink_reset;
     bsg_tag_s downstream_reset;
     bsg_tag_s token_reset;
-  }  bsg_link_sdr_tag_lines_s;
-  localparam bsg_link_sdr_tag_local_els_gp =
+  }  bsg_link_sdr_master_tag_lines_s;
+  localparam bsg_link_sdr_master_tag_local_els_gp =
 	$bits(bsg_link_sdr_tag_lines_s) / $bits(bsg_tag_s);
 
 endpackage
