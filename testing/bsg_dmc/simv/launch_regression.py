afd7cefa65d33aabe577bc451fb935caaf1bbef2
Akash Suresh <akashs3@uw.edu>
2021-12-25 14:08:52 -0800

    add parsing support to regression script

--- testing/bsg_dmc/simv/launch_regression.py
+++ testing/bsg_dmc/simv/launch_regression.py
@@ -1,20 +1,26 @@
 # This script takes in a regression list in YAML format, parses it and launches regressions in the output dir passed as commandline arg
 # TODO: 
-# - Add code to check if a test has passed or not; 
 # - add regression statistics
 # - add support to compile and launch DVE as well
 # Point of contact: Akash S (akashs3@uw.edu)
 
-import yaml, getopt, sys, io, os, random
+import yaml, getopt, sys, io, os, random, re
 
 simv = ''
 seed_for_randomisation = 0;
 cov_opts = ''
+errors = ''
 
 def main(argv):
+	global errors
 	yaml, output_dir = get_cmdline_options(argv)
 	regression_dict = parse_yaml(yaml)
 	launch_regression(regression_dict, output_dir)
+
+	if errors != '':
+		f = open(output_dir + "/errors.log", "w")
+		f.write(errors)
+
 
 def get_cmdline_options(argv):
 	yaml = 'regression_list.yaml'
@@ -25,13 +31,13 @@ def get_cmdline_options(argv):
 		opts, args = getopt.getopt(argv,"hy:o:s:r:c",["yaml=","output_dir=", "simv=", "random_seed=", "cov_enable"])
 	except getopt.GetoptError:
 		print 'incorrect script call'
+		print_help();
 		sys.exit(2)
 
-	print(opts)
+	#print(opts)
 	for opt, arg in opts:
 		if opt == '-h':
-		   print 'test.py -i <inputfile> -o <outputfile>'
-		   sys.exit()
+			print_help()
 		elif opt in ("-y", "--yaml"):
 		   	yaml = arg
 		elif opt in ("-o", "--output_dir"):
@@ -42,8 +48,20 @@ def get_cmdline_options(argv):
 			seed_for_randomisation = arg			
 		elif opt in ("-c", "--cov_enable"):
 			cov_opts = ' -cm line+cond+fsm+tgl+path '
+	
 
 	return yaml, output_dir
+
+def print_help():
+	print ("""This script is used to launch a set of tests listed in a YAML script in the DMC testbench.
+			   		Usage:
+			   		-> -y / --yaml : absolute path to the YAML script
+					-> -o / --output_dir : absolute path to output directory
+					-> -s / --simv : absolute path to simv
+					-> -r / --random_seed : random seed based on which script randomises seeds to launch tests
+					-> -c / --cov_enable : use when running coverage regressions
+    """)
+	sys.exit()
 
 def parse_yaml(yaml_file):
 	print ("Parsing " + yaml_file)
@@ -79,5 +97,32 @@ def launch_individual_tests(test_properties, test_path):
 		print("Launching: " + simv + " " + test_properties["args"] + " +ntb_random_seed=" + str(rand_seed) + " -l " + seed_path + "/run.log +vpdfile+" + seed_path + "/waves.vpd" + cov_opts)
 		os.system(simv + " " + test_properties["args"] + "+ntb_random_seed=" + str(rand_seed) + " +vpdfile+" + seed_path + "/waves.vpd > " + seed_path + "/run.log" + cov_opts)
 
+		parse_log(seed_path)
+
+def parse_log(seed_path):
+	global errors
+
+	log_name = seed_path + '/run.log'
+	#os.system("cd " + seed_path)
+	file = open(log_name, 'r')
+	file_contents = file.readlines()
+
+	for line in file_contents:
+		match1 = re.search(r'^ERROR', line)
+		if match1:
+			errors += log_name + " : "
+			errors += line + "\n"
+
+		match2 = re.search(r'^UVM_FATAL.*', line)
+		if match2:
+			if(not re.search(r'^UVM_FATAL\s\:', line)):
+				errors += log_name + " : "
+				errors += line + "\n"
+
+		match3 = re.search(r'^UVM_ERROR.*', line)
+		if match3:
+			if(not re.search(r'^UVM_ERROR\s\:', line)):				
+				errors += log_name + " : "
+				errors += line + "\n"
 
 main(sys.argv[1:])
