ac56432c45a3021e1324330e20815ded0fb90377
barnold3 <60902213+barnold3@users.noreply.github.com>
2022-06-09 11:47:08 -0500

    Update bsg_idiv_iterative_controller.v

--- bsg_misc/bsg_idiv_iterative_controller.v
+++ bsg_misc/bsg_idiv_iterative_controller.v
@@ -42,10 +42,10 @@ module bsg_idiv_iterative_controller #(parameter width_p=32, parameter bits_per_
       ,input               yumi_i
       );
 
-   reg q_neg;
-   reg r_neg;
-   reg neg_ld;
-   reg add1_neg_last, add2_neg_last;
+   logic q_neg_r;
+   logic r_neg_r;
+   logic neg_ld;
+   logic add1_neg_last_r, add2_neg_last_r;
    
    typedef enum logic[5:0] 
            {WAIT, NEG0, NEG1, SHIFT,
@@ -55,19 +55,19 @@ module bsg_idiv_iterative_controller #(parameter width_p=32, parameter bits_per_
    idiv_ctrl_stat state, next_state;
 
    always @(posedge clk_i) begin
-      add1_neg_last <= adder1_result_is_neg_i;
-      add2_neg_last <= adder2_result_is_neg_i;
+      add1_neg_last_r <= adder1_result_is_neg_i;
+      add2_neg_last_r <= adder2_result_is_neg_i;
       
       if (neg_ld) begin
         // the quotient is negated if the signs of the operands differ
-        q_neg <= (opA_is_neg_i ^ opC_is_neg_i) & signed_div_r_i;
+        q_neg_r <= (opA_is_neg_i ^ opC_is_neg_i) & signed_div_r_i;
 
         // the remainder is negated if the dividend is negative
-        r_neg <= opC_is_neg_i & signed_div_r_i;
+        r_neg_r <= opC_is_neg_i & signed_div_r_i;
       end 
    end
 
-  logic [`BSG_SAFE_CLOG2(width_p/bits_per_iter_p+1)-1:0] calc_cnt;
+  logic [`BSG_WIDTH(width_p/bits_per_iter_p)-1:0] calc_cnt;
   wire calc_up_li = (state == CALC) && (calc_cnt < width_p/bits_per_iter_p);
   wire calc_done  = (calc_cnt == width_p/bits_per_iter_p);
   bsg_counter_clear_up#(.max_val_p(width_p/bits_per_iter_p)
@@ -94,12 +94,17 @@ module bsg_idiv_iterative_controller #(parameter width_p=32, parameter bits_per_
       opA_ld_o       = 1'b0;
 
       if (bits_per_iter_p == 2)
-        opA_inv_o    = !add2_neg_last;
+        opA_inv_o    = !add2_neg_last_r;
       else
-	opA_inv_o    = !add1_neg_last;
+	opA_inv_o    = !add1_neg_last_r;
       
       opA_clr_l_o    = 1'b1;
-      opB_sel_o      = 3'b001;
+	   
+      if (bits_per_iter_p == 2)
+        opB_sel_o      = 4'b0001;
+      else
+        opB_sel_o      = 3'b001;
+      
       opB_ld_o       = 1'b0;
       opB_inv_o      = 1'b0;
       opB_clr_l_o    = 1'b1;
@@ -107,9 +112,9 @@ module bsg_idiv_iterative_controller #(parameter width_p=32, parameter bits_per_
       opC_ld_o       = 1'b0;
       
       if (bits_per_iter_p == 2)
-        adder1_cin_o    = !add2_neg_last;
+        adder1_cin_o    = !add2_neg_last_r;
       else
-        adder1_cin_o    = !add1_neg_last;
+        adder1_cin_o    = !add1_neg_last_r;
      
       neg_ld         = 1'b0;
       latch_signed_div_o   = 1'b0;
@@ -138,7 +143,6 @@ module bsg_idiv_iterative_controller #(parameter width_p=32, parameter bits_per_
        opA_sel_o    = 2'b01;
        opA_inv_o    = 1'b1;
        opB_clr_l_o  = 1'b0;
-       opB_sel_o    = 3'b100;
        opB_ld_o     = 1'b1;
        opC_ld_o     = 1'b0;
        neg_ld       = 1'b1;
@@ -189,8 +193,8 @@ module bsg_idiv_iterative_controller #(parameter width_p=32, parameter bits_per_
          opB_sel_o  = calc_done ? 4'b0100 : 4'b0001;
 	 opC_sel_o  = calc_done ? 4'b0010 : 4'b0001;
          if (calc_cnt == 0) begin
-	   opA_inv_o    = !add1_neg_last;
-	   adder1_cin_o = !add1_neg_last;
+	   opA_inv_o    = !add1_neg_last_r;
+	   adder1_cin_o = !add1_neg_last_r;
          end
        end else
          opB_sel_o  = calc_done ? 3'b010 : 3'b001;
@@ -216,14 +220,14 @@ module bsg_idiv_iterative_controller #(parameter width_p=32, parameter bits_per_
     end
     
     REMAIN: begin
-       next_state = (zero_divisor_i | !q_neg) ? DONE: QUOT;
+	    next_state = (zero_divisor_i | !q_neg_r) ? DONE: QUOT;
        opA_sel_o    = 2'b01;
        opA_ld_o     = 1'b1;
        opA_clr_l_o  = 1'b0;
        opB_ld_o     = 1'b1;
        opC_ld_o     = 1'b0;
-       opB_inv_o    = r_neg;
-       adder1_cin_o    = r_neg;
+       opB_inv_o    = r_neg_r;
+       adder1_cin_o    = r_neg_r;
 
        if (bits_per_iter_p == 2)
          opB_sel_o = 4'b1000;
