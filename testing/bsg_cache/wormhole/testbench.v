13381168f3c63e839f2da997d6df00efdb79afc1
Farzam Gilani <gilanifarzam@gmail.com>
2022-07-30 16:46:49 -0700

    updating wormhole testbench

--- testing/bsg_cache/wormhole/testbench.v
+++ testing/bsg_cache/wormhole/testbench.v
@@ -36,10 +36,11 @@ module testbench();
   localparam block_size_in_words_p = 8;
   localparam sets_p = 128;
   localparam ways_p = 8;
+  localparam word_tracking_p = 1;
   localparam mem_size_p = block_size_in_words_p*sets_p*ways_p*4;
   localparam block_offset_width_p = `BSG_SAFE_CLOG2(data_width_p*block_size_in_words_p/8);
   localparam data_len_p=block_size_in_words_p*data_width_p/dma_data_width_p;
-  localparam wh_len_width_p=`BSG_WIDTH(data_len_p+1);
+  localparam wh_len_width_p=`BSG_WIDTH(data_len_p+2);
   localparam wh_cid_width_p=3;
   localparam wh_cord_width_p=3;
   localparam wh_flit_width_p=dma_data_width_p;
@@ -58,7 +59,7 @@ module testbench();
 
 
   `declare_bsg_cache_pkt_s(addr_width_p,data_width_p);
-  `declare_bsg_cache_dma_pkt_s(addr_width_p);
+  `declare_bsg_cache_dma_pkt_s(addr_width_p,block_size_in_words_p);
 
   bsg_cache_pkt_s [num_dma_p-1:0] cache_pkt;
   logic [num_dma_p-1:0] v_li;
@@ -93,6 +94,7 @@ module testbench();
         ,.block_size_in_words_p(block_size_in_words_p)
         ,.sets_p(sets_p)
         ,.ways_p(ways_p)
+        ,.word_tracking_p(word_tracking_p)
         ,.amo_support_p(amo_support_level_arithmetic_lp)
       ) cache (
         .clk_i(clk)
@@ -136,6 +138,7 @@ module testbench();
       bsg_cache_dma_to_wormhole #(
          .dma_addr_width_p(addr_width_p)
          ,.dma_burst_len_p(data_len_p)
+         ,.dma_mask_width_p(block_size_in_words_p)
          ,.wh_flit_width_p(wh_flit_width_p)
          ,.wh_cid_width_p(wh_cid_width_p)
          ,.wh_cord_width_p(wh_cord_width_p)
@@ -204,6 +207,7 @@ module testbench();
      .num_dma_p(num_dma_p)
      ,.dma_addr_width_p(addr_width_p)
      ,.dma_burst_len_p(data_len_p)
+     ,.dma_mask_width_p(block_size_in_words_p)
      ,.wh_flit_width_p(wh_flit_width_p)
      ,.wh_cid_width_p(wh_cid_width_p)
      ,.wh_cord_width_p(wh_cord_width_p)
@@ -236,6 +240,7 @@ module testbench();
       bsg_nonsynth_dma_model #(
         .addr_width_p(addr_width_p)
         ,.data_width_p(dma_data_width_p)
+        ,.mask_width_p(block_size_in_words_p)
         ,.block_size_in_words_p(data_len_p)
         ,.els_p(mem_size_p)
 
