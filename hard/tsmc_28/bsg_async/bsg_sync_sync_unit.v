95702bc11f9932fb1cfd9045f3ffdf51d41617ad
Dan Petrisko <petrisko@cs.washington.edu>
2022-08-30 15:19:05 -0700

    Adding comments

--- hard/tsmc_28/bsg_async/bsg_sync_sync_unit.v
+++ hard/tsmc_28/bsg_async/bsg_sync_sync_unit.v
@@ -1,4 +1,24 @@
 
+//
+// Context: The tsmc28 PDK we used did not have a sync cell
+//   to use explicitly, so we compose one ourselves and call
+//   it a synchronization 'unit'.
+//
+// bsg_SYNC_1_r and bsg_SYNC_2_r should be placed abutted if possible
+//   so that there is a small max path between them.
+//
+// In synopsys flows, rp_groups can be used and this file can be annotated.
+// In cadence flows, SDP groups serve the same purpose, but are contained within
+//   external files.
+// We have also seen good results with magnet placement of these two cells
+//
+// Users should
+//   set_false_path -to bsg_SYNC_1_r/D
+//   to avoid false CDC violations. With high clock uncertainty, we've also
+//   observed violating min paths between the flops. If the placement is
+//   correct, then this uncertainty is pessimistic and can be waived with
+//   set_false_path -hold -from bsg_SYNC_1_r/Q -to bsg_SYNC_1_r/D
+//
 module bsg_sync_sync_unit
  (input oclk_i
   , input iclk_data_i
