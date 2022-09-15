d17f01ecae1dd137341da0fa7326045d151ab74f
KinzaQamar <kinzahqamarzaman@gmail.com>
2022-09-15 19:59:11 +0500

    code cleanup

--- bsg_mem/bsg_mem_1rw_sync_subbanked.v
+++ bsg_mem/bsg_mem_1rw_sync_subbanked.v
@@ -1,5 +1,5 @@
 /*
- *  bsg_mem_1rw_sync_subbanked.v
+ *  bsg_mem_1rw_sync_mask_write_bit_subbanked.v
  *
  *  This module has the same interface/functionality as
     N bsg_mem_1rw_sync.
@@ -21,11 +21,6 @@ module bsg_mem_1rw_sync_mask_write_bit_subbanked
     , parameter latch_last_read_p=0
     , parameter num_subbank_p=1
 
-      // width = no mask, mask ignored
-      // We set this as a separate parameter so that we can substitute out the type of ram appropriately
-    , parameter mask_granularity_p=1
-    , parameter mask_width_lp = width_p/mask_granularity_p
-
     , localparam lg_els_lp = `BSG_SAFE_CLOG2(els_p)
 
       // Don't support depth subbanks due to conflicts
@@ -43,27 +38,6 @@ module bsg_mem_1rw_sync_mask_write_bit_subbanked
     	, output [num_subbank_p-1:0][subbank_width_lp-1:0] data_o
     );
 
-	if (num_subbank_p == 1) begin 
-
-    bsg_mem_1rw_sync_mask_write_bit #(
-      .width_p(subbank_width_lp)
-      ,.els_p(els_p)
-      ,.latch_last_read_p(latch_last_read_p)
-    ) sub_bank (
-      .clk_i(clk_i)
-      ,.reset_i(reset_i)
-      ,.v_i(v_i)
-      ,.w_i(w_i)
-      ,.addr_i(addr_i)
-      ,.data_i(data_i[0+:subbank_width_lp-1])
-      ,.w_mask_i(w_mask_i[0+:subbank_width_lp-1])
-      ,.data_o(data_o[0+:subbank_width_lp-1])
-    );
-		
-	end // if (num_subbank_p == 1)
-
-	else begin // !(num_subbank_p == 1)
-
     logic [subbank_width_lp-1:0] bank_data_lo [num_subbank_p-1:0];
 		wire [mask_width_lp-1] w_mask_expanded;
 
@@ -75,7 +49,7 @@ module bsg_mem_1rw_sync_mask_write_bit_subbanked
     	)
     	w_mask_expanded
     	(
-				.i(w_mask_i[j][j*subbank_width_lp+:subbank_width_lp-1])
+				.i(w_mask_i[j][j*subbank_width_lp+:subbank_width_lp])
     		,.o(w_mask_expanded)
       );
 
@@ -90,15 +64,11 @@ module bsg_mem_1rw_sync_mask_write_bit_subbanked
         ,.v_i(v_i)
         ,.w_i(w_i)
         ,.addr_i(addr_i)
-        ,.data_i(data_i[j][j*subbank_width_lp+:subbank_width_lp-1])
+        ,.data_i(data_i[j][j*subbank_width_lp+:subbank_width_lp])
         ,.w_mask_i(w_mask_expanded)
         ,.data_o(bank_data_lo[j])
       );
 
-		end // for (genvar j = 0; j < num_depth_bank_p; j++)
-
-		assign data_o [j][j*subbank_width_lp+:subbank_width_lp-1] = bank_data_lo[j];
-
-	end // !(num_subbank_p == 1)
+		assign data_o [j][j*subbank_width_lp+:subbank_width_lp] = bank_data_lo[j];
 
 endmodule
