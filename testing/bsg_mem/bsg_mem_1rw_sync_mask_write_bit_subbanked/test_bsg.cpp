76c4d4c0b4fcfa04d0287f39e174850d4bf0186d
KinzaQamar <kinzahqamarzaman@gmail.com>
2022-09-22 18:24:35 +0500

    Cleanups

--- testing/bsg_mem/bsg_mem_1rw_sync_mask_write_bit_subbanked/test_bsg.cpp
+++ testing/bsg_mem/bsg_mem_1rw_sync_mask_write_bit_subbanked/test_bsg.cpp
@@ -22,52 +22,42 @@ int main (int argc, char **argv) {
 
     Vtest_bsg* top = new Vtest_bsg;
 
-    // init trace dump
-    //VerilatedVcdC* tfp = NULL;
-
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
-    // if initialized above
-    //tfp = new VerilatedVcdC;
 
     top->trace (tfp, 99);
-    Verilated::mkdir("logs");
-    tfp->open("logs/sim.vcd");
+    Verilated::mkdir("waveform");
+    tfp->open("waveform/sim.vcd");
 
-    top -> clk = 1;
-    //top -> reset = 1;
-   
+    top -> clk = 0;
+    top -> v_i = 0;
+    top -> w_i = 0;
+
     while (main_time < 1000 && !Verilated::gotFinish()) 
     { 
-        //top->clk = top->clk ? 0 : 1;       // Toggle clock
+        main_time ++;
         top->eval();
-        top->clk = top->clk ? 0 : 1;
-        // if(main_time == 16) {
-            // top->reset = 0;
-        // } 
-        // if(main_time == 20) {
-            // top->reset = 1;
-        // }
 
-        // redundant
-        //top -> eval();
+        if(main_time == 16) {
+          top -> v_i = 1;
+          top -> w_i = 1;
+         } 
+
+        if(main_time == 200) {
+           top -> w_i = 0;
+        }
 
         if (tfp) tfp -> dump(main_time);
-        // or dump always
-        //tfp -> dump(main_time);
 
-        main_time ++;
+        top->clk = top->clk ? 0 : 1;
     }
 
     top -> final();
 
     if (tfp) tfp -> close();
-    // or close anyways
-    //tfp -> close();
 
     delete top;
 
     std::cout << "\nVerilatorTB: End of sim" << std::endl;
-    //exit(EXIT_SUCCESS);
     exit(0);
 }
