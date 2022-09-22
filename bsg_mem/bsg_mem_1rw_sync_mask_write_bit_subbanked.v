04836afad10ed53cae153278fbb88eeadfd8111d
KinzaQamar <kinzahqamarzaman@gmail.com>
2022-09-22 18:21:17 +0500

    Assertions added

--- bsg_mem/bsg_mem_1rw_sync_mask_write_bit_subbanked.v
+++ bsg_mem/bsg_mem_1rw_sync_mask_write_bit_subbanked.v
@@ -2,7 +2,7 @@
  *  bsg_mem_1rw_sync_mask_write_bit_subbanked.v
  *
  *  This module has the same interface/functionality as
-    N bsg_mem_1rw_sync.
+    bsg_mem_1rw_sync_mask_write_bit.
  *
  *  - width_p : width of the total memory
  *  - els_p   : depth of the total memory
@@ -18,7 +18,7 @@
 module bsg_mem_1rw_sync_mask_write_bit_subbanked
   #(parameter `BSG_INV_PARAM(width_p)
     , parameter `BSG_INV_PARAM(els_p)
-    , parameter latch_last_read_p=0
+    , parameter latch_last_read_p=1
     , parameter num_subbank_p=1
 
     , localparam els_lp = `BSG_SAFE_CLOG2(els_p)
@@ -38,7 +38,7 @@ module bsg_mem_1rw_sync_mask_write_bit_subbanked
     	, output logic [num_subbank_p-1:0][subbank_width_lp-1:0] data_o
     );
 
-    logic [width_p-1:0] bank_data_lo [els_lp-1:0];
+    logic [width_p-1:0] bank_data_lo [els_p-1:0];
 		wire [width_p-1:0] w_mask_expand;
     wire read_en;
 
@@ -50,7 +50,7 @@ module bsg_mem_1rw_sync_mask_write_bit_subbanked
     	)
     	w_mask_expanded
     	(
-				.i(w_mask_i)
+				.i(w_mask_i[0])
     		,.o(w_mask_expand)
       );
 
@@ -70,17 +70,11 @@ module bsg_mem_1rw_sync_mask_write_bit_subbanked
         ,.data_o(bank_data_lo[addr_i])
       );
 
-		  //assign data_o  = bank_data_lo[addr_i];
+    assign data_o = bank_data_lo[addr_i];
 
-
-
-    //end
-
-    assign read_en = v_i & ~w_i;
-
-    always@(posedge clk_i) begin
-      if (read_en)
-       data_o = bank_data_lo[addr_i];
+    always@(*) begin
+      assert (`BSG_IS_POW2(width_p) && `BSG_IS_POW2(els_p));
+      assert (width_p >= num_subbank_p);
     end
 
 endmodule
