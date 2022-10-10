f3a959e0e097be80c62b8c733521dafe1368ce9c
Kinza Qamar Zaman <66952091+KinzaQamar@users.noreply.github.com>
2022-10-10 08:29:48 +0500

    Code for bsg_mem_1rw_sync_subbanked SRAM (#585)
    * bsg_mem_1rw_sync_subbanked.v
    
    * Modified
    
    * code cleanup
    
    * Code corrections
    
    * Adding cpp testbench for verilator
    
    * Adding testbench for 1rw_sync_mask_write_bit_subbanked module
    
    * Adding verilator target
    
    * Further corrections
    
    * Delete bsg_mem_1rw_sync_subbanked.v
    
    * Assertions added
    
    * Cleanups
    
    * Cleanups
    
    * Cleanups
    
    * Added Reference model
    
    * Code Cleanups
    
    * Target Modifications
    
    * Added reference model
    
    * Further modifications
    
    * first draft with new interface in issue#400
    
    * reverting to previous code
    
    * Update interface
    
    * Cleanups
    
    * Modifications
    
    * Changed  to  for verilator
    
    * Modified write mask
    
    * Added simulation target
    
    * Cleanups
    
    * Modified reads through latch last read
    
    * latch_last_read_p is 0 for backing sram
    
    * Cleanups
    
    * Cleanups
    
    * Makefile for no mask sram
    
    * Verilator tb for no mask sram
    
    * TB for no mask sram
    
    * No mask sram
    
    * Added byte mask sram
    
    * TB byte mask sram
    
    * Verilator TB for byte mask sram
    
    * Makefile for byte mask sram
    
    * Modified checker logic
    
    * Addressed requested changes
    
    * Update checker logic
    
    * Updated checker logic
    
    * Addressed requested changes
    
    * TB modifications for no llr
    
    * Addressed requested changes
    
    * Modifications
    
    * Modifications
    
    * Modifications
    
    * Modifications and cleanups
    
    * Added a check for byte-mask SRAM
    
    * Modifications and cleanups
    
    * Additional check with cleanups
    
    * Modifications and cleanups
    
    * Added check for byte size subbank

---
+++ testing/bsg_mem/bsg_mem_1rw_sync_subbanked/test_bsg.cpp
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <iostream>
+#include <utility>
+#include <string>
+#include "Vtest_bsg.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+unsigned int main_time = 0;
+
+double sc_time_stamp () {
+    return main_time;
+}
+
+int main (int argc, char **argv) {
+
+    std::cout << "\nVerilatorTB: Start of sim\n" << std::endl; 
+
+    Verilated::commandArgs(argc, argv);
+
+    Vtest_bsg* top = new Vtest_bsg;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+
+    top->trace (tfp, 99);
+    Verilated::mkdir("waveform");
+    tfp->open("waveform/sim.vcd");
+
+    top -> clk = 0;
+    top -> v_i = 0;
+    top -> w_i = 0;
+
+    while (main_time < 1000 && !Verilated::gotFinish()) 
+    { 
+        main_time ++;
+        top->eval();
+
+        if(main_time == 16) {
+          top -> v_i = 0x1;
+          top -> w_i = 0x1;
+         } 
+
+        if(main_time >= 200) {
+           top -> w_i = 0;
+        }
+
+        if(main_time >= 500) {
+           top -> v_i = 0x1;
+           top -> w_i = 0x1;
+        }
+    
+        if(main_time >= 750) {
+           top -> w_i = 0x0;
+        }
+
+        if(main_time >= 950) {
+           top -> v_i = 0x1;
+           top -> w_i = 0x0;
+        }
+
+        if (tfp) tfp -> dump(main_time);
+
+        top->clk = top->clk ? 0 : 1;
+    }
+
+    top -> final();
+
+    if (tfp) tfp -> close();
+
+    delete top;
+
+    exit(0);
+}
