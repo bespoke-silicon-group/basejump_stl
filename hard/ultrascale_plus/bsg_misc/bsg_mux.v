484e29e44bcfa8ce815dcf782233ea8b91086c5b
mysoreanoop <mnanoop2014@gmail.com>
2022-12-09 08:19:43 -0800

    addressing review comments

--- hard/ultrascale_plus/bsg_misc/bsg_mux.v
+++ hard/ultrascale_plus/bsg_misc/bsg_mux.v
@@ -3,25 +3,14 @@
 
 `include "bsg_defines.v"
 
-module bsg_macro_clk_mux_2_1
-   (
-    input [1:0] data_i
-    ,input sel_i
-    ,output data_o
-    );
-
-  // BUFGMUX_CTRL: 2-to-1 General Clock MUX Buffer
-  //               Versal Prime series
-  // Xilinx HDL Language Template, version 2022.2
-  // https://docs.xilinx.com/r/en-US/ug1344-versal-architecture-libraries/BUFGMUX_CTRL
-
-  BUFGMUX_CTRL BUFGMUX_CTRL_inst (
-     .O(data_o),
-     .I0(data_i[0]),
-     .I1(data_i[1]),
-     .S(sel_i)
-  );
-endmodule
+// macro datasheet: https://docs.xilinx.com/r/en-US/ug1344-versal-architecture-libraries/BUFGMUX_CTRL
+`define bsg_macro_clk_mux(inst, data_o, data_i_1, data_i_0, sel_i) \
+BUFGMUX_CTRL BUFGMUX_CTRL_``inst`` (                               \
+   .O  (data_o),                                                   \
+   .I0 (data_i_0),                                                 \
+   .I1 (data_i_1),                                                 \
+   .S  (sel_i)                                                     \
+);
 
 module bsg_mux #(parameter `BSG_INV_PARAM(width_p)
                  , parameter els_p=1
@@ -35,56 +24,26 @@ module bsg_mux #(parameter `BSG_INV_PARAM(width_p)
     ,output [width_p-1:0] data_o
     );
   
-  // the following macro is for clock muxing only
-  if((harden_p == 1) && (balanced_p == 1)
-        && (width_p == 1)
-        && (els_p > 1) && (els_p < 5))
+  // the following macros are for clock muxing only
+  if((harden_p == 1) && (balanced_p == 1) && (width_p == 1) && (els_p == 2))
     begin: macro
-      if(els_p == 2)
-        begin: mux2
-          bsg_macro_clk_mux_2_1 m (.*);
-        end
+      `bsg_macro_clk_mux(m, data_o, data_i[0], data_i[1], sel_i)
+    end
 
-      else if(els_p == 3)
-        begin: mux3
-          wire data_1_0, data_3_2;
-          bsg_macro_clk_mux_2_1 d_1_0
-            (.data_i(data_i[1:0])
-            ,.sel_i(sel_i[0])
-            ,.data_o(data_1_0)
-            );
-          bsg_macro_clk_mux_2_1 d_3_2
-            (.data_i({1'b0, data_i[2]})
-            ,.sel_i(sel_i[0])
-            ,.data_o(data_3_2)
-            );
-          bsg_macro_clk_mux_2_1 m
-            (.data_i({data_3_2, data_1_0})
-            ,.sel_i(sel_i[1])
-            ,.data_o(data_o)
-            );
-        end
-
-      else
-        begin: mux4
-          wire data_1_0, data_3_2;
-          bsg_macro_clk_mux_2_1 d_1_0
-            (.data_i(data_i[1:0])
-            ,.sel_i(sel_i[0])
-            ,.data_o(data_1_0)
-            );
-          bsg_macro_clk_mux_2_1 d_3_2
-            (.data_i(data_i[3:2])
-            ,.sel_i(sel_i[0])
-            ,.data_o(data_3_2)
-            );
-          bsg_macro_clk_mux_2_1 m
-            (.data_i({data_3_2, data_1_0})
-            ,.sel_i(sel_i[1])
-            ,.data_o(data_o)
-            );
-        end
-    end: macro
+  else if((harden_p == 1) && (balanced_p == 1) && (width_p == 1) && (els_p == 3))
+    begin: macro
+      wire data_1_0, data_3_2;
+      `bsg_macro_clk_mux(m_1_0, data_i[1], data_i[0], sel_i[0], data_1_0)
+      `bsg_macro_clk_mux(m_3_2, 1'b0     , data_i[2], sel_i[0], data_3_2)
+      `bsg_macro_clk_mux(m,     data_3_2 , data_1_0 , sel_i[1], data_o  )
+    end
+  else if((harden_p == 1) && (balanced_p == 1) && (width_p == 1) && (els_p == 4))
+    begin: macro
+      wire data_1_0, data_3_2;
+      `bsg_macro_clk_mux(m_1_0, data_i[1], data_i[0], sel_i[0], data_1_0)
+      `bsg_macro_clk_mux(m_3_2, data_i[3], data_i[2], sel_i[0], data_3_2)
+      `bsg_macro_clk_mux(m,     data_3_2 , data_1_0 , sel_i[1], data_o  )
+    end
   else
     begin: notmacro
       if (els_p == 1)
@@ -98,7 +57,7 @@ module bsg_mux #(parameter `BSG_INV_PARAM(width_p)
       // synopsys translate_off
       initial
         assert(balanced_p == 0)
-          else $error("%m warning: synthesizable implementation of bsg_mux does not support width_p, els_p or balanced_p");
+          else $error("%m warning: synthesizable implementation of bsg_mux does not support balanced_p");
       // synopsys translate_on
     end
 
