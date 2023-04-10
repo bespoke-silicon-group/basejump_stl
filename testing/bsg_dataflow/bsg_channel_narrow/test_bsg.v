49063db3e1b1f5c3778570edfa45bbeec4804a97
BrendenPage <brendenpage@gmail.com>
2023-04-10 01:51:01 -0700

    Separated out test cases for parameters lsb_to_msb_p to simplify regression testing

--- testing/bsg_dataflow/bsg_channel_narrow/test_bsg.v
+++ testing/bsg_dataflow/bsg_channel_narrow/test_bsg.v
@@ -1,5 +1,6 @@
-`define WIDTH_IN_P  3
-`define WIDTH_OUT_P 2
+`define WIDTH_IN_P   3
+`define WIDTH_OUT_P  2
+`define LSB_TO_MSB_P 0
 
 /********************************** TEST RATIONALE *************************
 
@@ -23,6 +24,7 @@ module test_bsg
 #(
   parameter width_in_p   = `WIDTH_IN_P,
   parameter width_out_p  = `WIDTH_OUT_P,
+  parameter lsb_to_msb_p = `LSB_TO_MSB_P,
   parameter divisions_p  = ((width_in_p % width_out_p) == 0)
                              ? (width_in_p / width_out_p)
                              : (width_in_p / width_out_p) + 1,
@@ -54,22 +56,23 @@ module test_bsg
     /*$monitor("\n@%0t ps: ", $time
               , "test_input_data: %b, test_input_deque: %b"
               , test_input_data, test_input_deque
-              , ", test_output_data_1: %b, test_output_deque_1: %b"
-              , test_output_data_1, test_output_deque_1);*/
+              , ", test_output_data: %b, test_output_deque: %b"
+              , test_output_data, test_output_deque);*/
 
     $display("\n\n\n");
     $display("===========================================================");
     $display("testing bsg_channel_narrow with ...");
-    $display("WIDTH_IN_P : %d", width_in_p);
-    $display("WIDTH_OUT_P: %d\n", width_out_p);
+    $display("WIDTH_IN_P :  %d", width_in_p);
+    $display("WIDTH_OUT_P:  %d", width_out_p);
+    $display("LSB_TO_MSB_P: %d\n", lsb_to_msb_p);
   end
 
   logic [width_in_p-1:0]  test_input_data;
   logic test_input_deque;
 
   // two uuts are instantiated; one with lsb_to_msb_p = 0 and the other with 1
-  logic [width_out_p-1:0] test_output_data_0, test_output_data_1;
-  logic test_output_deque_0, test_output_deque_1;
+  logic [width_out_p-1:0] test_output_data;
+  logic test_output_deque;
 
   logic [$clog2(divisions_p)-1:0] count_r;
   logic finish_r;
@@ -119,27 +122,27 @@ module test_bsg
       begin
          // this craziness is because of a vivado bug that has issues mixing
          // casting and asserts
-         temp = (test_output_data_1 ==
+         temp = (!lsb_to_msb_p || test_output_data ==
                  (width_out_p ' (test_input_data >> (width_out_p*count_r))));
 
          assert(temp)
            else $error("1 lsb_to_msb_data: mismatch on input=%x output=%x count_r=%x"
-                       , test_input_data, test_output_data_1, count_r,
+                       , test_input_data, test_output_data, count_r,
                        );
 
-         temp = (test_output_data_0 ==
+         temp = (lsb_to_msb_p || test_output_data ==
                  width_out_p ' (test_input_data >> (width_out_p*(divisions_p - count_r - 1))));
 
          assert(temp)
            else $error("2 msb_to_lsb_data: mismatch on input %x ", test_input_data
                        , "division %x", count_r);
 
-         temp = (test_output_deque_1 == (count_r == divisions_p-1));
+         temp = (!lsb_to_msb_p || test_output_deque == (count_r == divisions_p-1));
          assert(temp)
            else $error("3 lsb_to_msb_deque: mismatch on input %x ", test_input_data
                        , "division %x", count_r);
 
-         temp = (test_output_deque_0 == (count_r == divisions_p-1));
+         temp = (lsb_to_msb_p || test_output_deque == (count_r == divisions_p-1));
          assert(temp)
           else $error("4 msb_to_lsb_deque: mismatch on input %x ", test_input_data
                          , "division %x", count_r);
@@ -149,25 +152,13 @@ module test_bsg
 
   bsg_channel_narrow #(  .width_in_p  (width_in_p)
                        , .width_out_p (width_out_p)
-                       , .lsb_to_msb_p(0)
-                      )  DUT0
+                       , .lsb_to_msb_p(lsb_to_msb_p)
+                      )  DUT
                       (  .clk_i    (clk)
                        , .reset_i  (reset)
                        , .data_i (test_input_data)
-                       , .deque_o(test_output_deque_0)
-                       , .data_o (test_output_data_0)
-                       , .deque_i(test_input_deque)
-                      );
-
-  bsg_channel_narrow #(  .width_in_p  (width_in_p)
-                       , .width_out_p (width_out_p)
-                       , .lsb_to_msb_p(1)
-                      )  DUT1
-                      (  .clk_i    (clk)
-                       , .reset_i  (reset)
-                       , .data_i (test_input_data)
-                       , .deque_o(test_output_deque_1)
-                       , .data_o (test_output_data_1)
+                       , .deque_o(test_output_deque)
+                       , .data_o (test_output_data)
                        , .deque_i(test_input_deque)
                       );
 
@@ -177,7 +168,7 @@ module test_bsg
                             : (width_out_p);
   logic [3*log_width_p-1:0] log;
 
-  assign log = {  log_width_p ' (test_output_data_1)
+  assign log = {  log_width_p ' (test_output_data)
                 , log_width_p ' (
                     width_out_p'(test_input_data >> (width_out_p*count_r)))
                 , log_width_p ' (test_input_data)
