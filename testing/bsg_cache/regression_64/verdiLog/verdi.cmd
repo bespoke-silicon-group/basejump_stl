862ab112955906744e55ca76b9410eeeb357867f
Robert Crist <rcrist@linux-lab-042.ece.uw.edu>
2023-04-29 16:16:22 -0700

    Made changes to add verdi but still not able to flush waveforms when the sim is aborted

---
+++ testing/bsg_cache/regression_64/verdiLog/verdi.cmd
@@ -0,0 +1,5 @@
+wvCreateWindow
+wvSetPosition -win $_nWave2 {("G1" 0)}
+wvOpenFile -win $_nWave2 \
+           {/home/rcrist/bsg_cache_research/basejump_stl/testing/bsg_cache/regression_64/out/test_random1/waveform.fsdb}
+debExit
