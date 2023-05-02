2985a4a16b3c5a1316cbf58a69fdd5c6c05e7c27
Robert Crist <rcrist@linux-lab-042.ece.uw.edu>
2023-05-02 01:09:12 -0700

    Verdi working

--- testing/bsg_cache/regression_64/verdiLog/verdi.cmd
+++ testing/bsg_cache/regression_64/verdiLog/verdi.cmd
@@ -1,5 +1,9 @@
+simSetSimulator "-vcssv" -exec \
+           "/home/rcrist/bsg_cache_research/basejump_stl/testing/bsg_cache/regression_64/simv" \
+           -args "-reportstats +wave=1 +checker=basic"
+debImport "-dbdir" \
+          "/home/rcrist/bsg_cache_research/basejump_stl/testing/bsg_cache/regression_64/simv.daidir"
+debLoadSimResult \
+           /home/rcrist/bsg_cache_research/basejump_stl/testing/bsg_cache/regression_64/out/test_random1/waveform.fsdb
 wvCreateWindow
-wvSetPosition -win $_nWave2 {("G1" 0)}
-wvOpenFile -win $_nWave2 \
-           {/home/rcrist/bsg_cache_research/basejump_stl/testing/bsg_cache/regression_64/out/test_random1/waveform.fsdb}
 debExit
