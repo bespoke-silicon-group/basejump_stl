c10cc39b764b1bf68687067c34e549941b33bfe3
Robert Crist <rcrist@linux-lab-045.ece.uw.edu>
2023-05-20 01:46:29 -0700

    Before making changes to cache

---
+++ testing/bsg_cache/regression_64_working/test_atomic1.py
@@ -0,0 +1,33 @@
+import sys
+import random
+from test_base import *
+
+class TestAtomic1(TestBase):
+
+  def generate(self):
+    self.clear_tag()
+
+    for n in range(10000):
+      tag = random.randint(0,9)
+      index = random.randint(0,1)
+      block_offset = random.randint(0, self.block_size_in_words_p-1)
+      byte_offset = 0
+      taddr = self.get_addr(tag, index, block_offset, byte_offset)
+      op = random.randint(0,2)
+      if op == 0:
+        self.send_amoswap_w(taddr)
+      elif op == 1:
+        self.send_sw(taddr)
+      elif op == 2:
+        self.send_lw(taddr)
+
+    self.tg.done()
+
+
+
+
+
+# main()
+if __name__ == "__main__":
+  t = TestAtomic1()
+  t.generate()
