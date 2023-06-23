9ba0f02fcaa4117f6a5c9699bdab14c59487c2b2
Robert Crist <robertcrist99@gmail.com>
2023-06-22 21:13:56 -0700

    direct mapped cache working and style changes for PR

--- bsg_cache/bsg_cache.v
+++ bsg_cache/bsg_cache.v
@@ -109,10 +109,15 @@ module bsg_cache
     ,.decode_o(decode)
   );
 
-  assign addr_way
-    = cache_pkt.addr[way_offset_width_lp+:lg_ways_lp];
+  if(ways_p == 1) begin
+    assign addr_way = '0;
+  end else begin
+    assign addr_way
+      = cache_pkt.addr[way_offset_width_lp+:lg_ways_lp];
+  end
+
   assign addr_index
-    = cache_pkt.addr[block_offset_width_lp+:lg_sets_lp];
+      = cache_pkt.addr[block_offset_width_lp+:lg_sets_lp];
 
   logic [lg_data_mem_els_lp-1:0] ld_data_mem_addr;
 
@@ -331,8 +336,14 @@ end
     addr_v_r[way_offset_width_lp+:tag_width_lp];
   assign addr_index_v =
     addr_v_r[block_offset_width_lp+:lg_sets_lp];
-  assign addr_way_v =
-    addr_v_r[way_offset_width_lp+:lg_ways_lp];
+  
+  if(ways_p == 1) begin
+    assign addr_way_v = '0;
+  end else begin
+    assign addr_way_v =
+      addr_v_r[way_offset_width_lp+:lg_ways_lp];
+  end
+  
   assign addr_block_offset_v = (block_size_in_words_p > 1)
     ? addr_v_r[lg_data_mask_width_lp+:lg_block_size_in_words_lp]
     : 1'b0;
@@ -1104,9 +1115,9 @@ end
   // If it's load or store, and there is a hit, it updates the dirty bits and LRU.
   // If there is a miss, stat_mem may be modified by the miss handler.
 
-  logic [ways_p-2:0] plru_decode_data_lo;
-  logic [ways_p-2:0] plru_decode_mask_lo;
-  
+  logic [((ways_p>1) ? ways_p-2:0):0] plru_decode_data_lo;
+  logic [((ways_p>1) ? ways_p-2:0):0] plru_decode_mask_lo;
+    
   bsg_lru_pseudo_tree_decode #(
     .ways_p(ways_p)
   ) plru_decode (
@@ -1131,9 +1142,9 @@ end
       if (decode_v_r.tagst_op) begin
         // for TAGST
         stat_mem_data_li.dirty = {ways_p{1'b0}};
-        stat_mem_data_li.lru_bits = {(ways_p-1){1'b0}};
-        stat_mem_w_mask_li.dirty = {ways_p{1'b1}};
-        stat_mem_w_mask_li.lru_bits = {(ways_p-1){1'b1}};
+        stat_mem_data_li.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b0}};
+        stat_mem_w_mask_li.dirty = {ways_p{1'b1}};        
+        stat_mem_w_mask_li.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b1}};
       end
       else begin
         // for LD, ST
