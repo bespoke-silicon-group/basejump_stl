0293d2cadf5683f6fff32dac533829404bcab3e3
Robert Crist <robertcrist99@gmail.com>
2023-06-23 15:07:45 -0700

    updated changes for PR

--- bsg_cache/bsg_cache.v
+++ bsg_cache/bsg_cache.v
@@ -1114,8 +1114,8 @@ end
   // If it's load or store, and there is a hit, it updates the dirty bits and LRU.
   // If there is a miss, stat_mem may be modified by the miss handler.
 
-  logic [((ways_p>1) ? ways_p-2:0):0] plru_decode_data_lo;
-  logic [((ways_p>1) ? ways_p-2:0):0] plru_decode_mask_lo;
+  logic [`BSG_SAFE_MINUS(ways_p,2):0] plru_decode_data_lo;
+  logic [`BSG_SAFE_MINUS(ways_p,2):0] plru_decode_mask_lo;
     
   bsg_lru_pseudo_tree_decode #(
     .ways_p(ways_p)
@@ -1140,10 +1140,10 @@ end
 
       if (decode_v_r.tagst_op) begin
         // for TAGST
-        stat_mem_data_li.dirty = {ways_p{1'b0}};
-        stat_mem_data_li.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b0}};
-        stat_mem_w_mask_li.dirty = {ways_p{1'b1}};        
-        stat_mem_w_mask_li.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b1}};
+        stat_mem_data_li.dirty = '0;
+        stat_mem_data_li.lru_bits = '0;
+        stat_mem_w_mask_li.dirty = '1;        
+        stat_mem_w_mask_li.lru_bits = '1;
       end
       else begin
         // for LD, ST
