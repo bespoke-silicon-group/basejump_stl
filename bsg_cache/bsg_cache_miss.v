261ad3bef9cc298da774701a32070e5e7bd3a9a7
Robert Crist <robertcrist99@gmail.com>
2023-06-22 21:13:56 -0700

    direct mapped cache working and style changes for PR

--- bsg_cache/bsg_cache_miss.v
+++ bsg_cache/bsg_cache_miss.v
@@ -169,8 +169,14 @@ module bsg_cache_miss
     = addr_v_i[block_offset_width_lp+:lg_sets_lp];
   assign addr_tag_v
     = addr_v_i[tag_offset_width_lp+:tag_width_lp];
-  assign addr_way_v
-    = addr_v_i[tag_offset_width_lp+:lg_ways_lp];
+
+  if (ways_p == 1) begin
+    assign addr_way_v = '0;
+  end else begin 
+    assign addr_way_v
+      = addr_v_i[tag_offset_width_lp+:lg_ways_lp];
+  end
+
   assign addr_block_offset_v
     = addr_v_i[lg_data_mask_width_lp+:lg_block_size_in_words_lp];
 
@@ -186,8 +192,8 @@ module bsg_cache_miss
 
   // chosen way lru decode
   //
-  logic [`BSG_SAFE_MINUS(ways_p,2):0] chosen_way_lru_data;
-  logic [`BSG_SAFE_MINUS(ways_p,2):0] chosen_way_lru_mask;
+  logic [((ways_p>1) ? ways_p-2:0):0] chosen_way_lru_data;
+  logic [((ways_p>1) ? ways_p-2:0):0] chosen_way_lru_mask;
 
   bsg_lru_pseudo_tree_decode #(
     .ways_p(ways_p)
@@ -210,9 +216,9 @@ module bsg_cache_miss
   // that the LRU way falls back to the same locked way soon and then forms "LRU trap"
   logic [lg_ways_lp-1:0] lru_way_id;
 
-  logic [`BSG_SAFE_MINUS(ways_p,2):0] modify_mask_lo;
-  logic [`BSG_SAFE_MINUS(ways_p,2):0] modify_data_lo;
-  logic [`BSG_SAFE_MINUS(ways_p,2):0] modified_lru_bits;
+  logic [((ways_p>1) ? ways_p-2:0):0] modify_mask_lo;
+  logic [((ways_p>1) ? ways_p-2:0):0] modify_data_lo;
+  logic [((ways_p>1) ? ways_p-2:0):0] modified_lru_bits;
   
   bsg_lru_pseudo_tree_backup #(
     .ways_p(ways_p)
@@ -223,7 +229,7 @@ module bsg_cache_miss
   );
 
   bsg_mux_bitwise #(
-    .width_p(ways_p-1)
+    .width_p(((ways_p>1) ? (ways_p-1):ways_p))
   ) lru_bit_mux (
     .data0_i(stat_info_in.lru_bits)
     ,.data1_i(modify_data_lo)
@@ -351,10 +357,13 @@ module bsg_cache_miss
         // LRU bit does not need to be updated.
         stat_mem_v_o = 1'b1;
         stat_mem_w_o = 1'b1;
-        stat_mem_data_out.dirty = '0;
-        stat_mem_data_out.lru_bits = '0;
+        stat_mem_data_out.dirty = {ways_p{1'b0}};
+
+        stat_mem_data_out.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b0}};
+
         stat_mem_w_mask_out.dirty = flush_way_decode;
-        stat_mem_w_mask_out.lru_bits = '0;
+
+        stat_mem_w_mask_out.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b0}};
 
         // If it's invalidate op, then clear the valid bit for the chosen way.
         // Otherwise, do not touch the valid bits.
