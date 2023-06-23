20e16caa1930a0935c7cf9e6ae914949a9b43f5a
Robert Crist <robertcrist99@gmail.com>
2023-06-23 15:07:45 -0700

    updated changes for PR

--- bsg_cache/bsg_cache_miss.v
+++ bsg_cache/bsg_cache_miss.v
@@ -192,8 +192,8 @@ module bsg_cache_miss
 
   // chosen way lru decode
   //
-  logic [((ways_p>1) ? ways_p-2:0):0] chosen_way_lru_data;
-  logic [((ways_p>1) ? ways_p-2:0):0] chosen_way_lru_mask;
+  logic [`BSG_SAFE_MINUS(ways_p,2):0] chosen_way_lru_data;
+  logic [`BSG_SAFE_MINUS(ways_p,2):0] chosen_way_lru_mask;
 
   bsg_lru_pseudo_tree_decode #(
     .ways_p(ways_p)
@@ -216,9 +216,9 @@ module bsg_cache_miss
   // that the LRU way falls back to the same locked way soon and then forms "LRU trap"
   logic [lg_ways_lp-1:0] lru_way_id;
 
-  logic [((ways_p>1) ? ways_p-2:0):0] modify_mask_lo;
-  logic [((ways_p>1) ? ways_p-2:0):0] modify_data_lo;
-  logic [((ways_p>1) ? ways_p-2:0):0] modified_lru_bits;
+  logic [`BSG_SAFE_MINUS(ways_p,2):0] modify_mask_lo;
+  logic [`BSG_SAFE_MINUS(ways_p,2):0] modify_data_lo;
+  logic [`BSG_SAFE_MINUS(ways_p,2):0] modified_lru_bits;
   
   bsg_lru_pseudo_tree_backup #(
     .ways_p(ways_p)
@@ -357,13 +357,10 @@ module bsg_cache_miss
         // LRU bit does not need to be updated.
         stat_mem_v_o = 1'b1;
         stat_mem_w_o = 1'b1;
-        stat_mem_data_out.dirty = {ways_p{1'b0}};
-
-        stat_mem_data_out.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b0}};
-
+        stat_mem_data_out.dirty = '0;
+        stat_mem_data_out.lru_bits = '0;
         stat_mem_w_mask_out.dirty = flush_way_decode;
-
-        stat_mem_w_mask_out.lru_bits = {(ways_p>1 ? ways_p-1:1){1'b0}};
+        stat_mem_w_mask_out.lru_bits = '0;
 
         // If it's invalidate op, then clear the valid bit for the chosen way.
         // Otherwise, do not touch the valid bits.
@@ -430,7 +427,7 @@ module bsg_cache_miss
         // set stat mem entry on store tag miss.
         stat_mem_v_o = dma_done_i & st_tag_miss_op;
         stat_mem_w_o = 1'b1;
-        stat_mem_data_out.dirty = {ways_p{1'b1}};
+        stat_mem_data_out.dirty = '1;
         stat_mem_data_out.lru_bits = chosen_way_lru_data;
         stat_mem_w_mask_out.dirty = chosen_way_decode;
         stat_mem_w_mask_out.lru_bits = chosen_way_lru_mask;
