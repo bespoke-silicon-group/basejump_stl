3b2b40c2f45b006991f57df729911dbcddeefb17
Robert Crist <robertcrist99@gmail.com>
2023-06-30 16:47:21 -0700

    Adding in mux changes

--- bsg_cache/bsg_cache_miss.v
+++ bsg_cache/bsg_cache_miss.v
@@ -229,7 +229,6 @@ module bsg_cache_miss
   );
 
   bsg_mux_bitwise #(
-    //.width_p(((ways_p>1) ? (ways_p-1):ways_p))
     .width_p(ways_p-1)
   ) lru_bit_mux (
     .data0_i(stat_info_in.lru_bits)
