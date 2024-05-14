0bf5125837be8641bc1a51845feb6026949939a3
Dan Petrisko <petrisko@cs.washington.edu>
2024-05-14 01:35:19 -0700

    Adding sky130

---
+++ hard/sky_130/bsg_misc/bsg_buf.sv
@@ -0,0 +1,20 @@
+
+module bsg_buf #(parameter width_p=1
+                 , parameter harden_p=1
+                 )
+   (input    [width_p-1:0] i
+    , output [width_p-1:0] o
+    );
+
+  if (harden_p)
+    begin : macro
+      for (genvar j = 0; j < width_p; j++)
+        sky130_fd_sc_hd__buf b (.X(o[j]), .A(i[j]));
+    end
+  else
+    begin : notmacro
+        assign o = i;
+    end
+
+endmodule
+
