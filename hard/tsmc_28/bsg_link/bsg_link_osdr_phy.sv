9343f269cb50bcc62bdf80049417045bf876b66e
Paul Gao <gaozihou@uw.edu>
2024-11-04 00:48:46 -0800

    add explicit conditional generate constructs

--- hard/tsmc_28/bsg_link/bsg_link_osdr_phy.sv
+++ hard/tsmc_28/bsg_link/bsg_link_osdr_phy.sv
@@ -37,7 +37,7 @@ module bsg_link_osdr_phy
   CKXOR2D2BWP7T40P140 BSG_OSDR_CKXOR2_BSG_DONT_TOUCH
   (.Z(clk_o_buf),.A1(clk_r_p),.A2(clk_r_n));
   `BSG_LINK_OSDR_PHY_CKBUF_MACRO(BSG_OSDR_CKBUF_BSG_DONT_TOUCH, clk_o_buf, clk_o)
-  begin: token
+  if (1) begin: token
   `BSG_LINK_OSDR_PHY_CKBUF_MACRO(BSG_OSDR_TKNBUF_BSG_DONT_TOUCH, token_i, token_o)
   end
 
