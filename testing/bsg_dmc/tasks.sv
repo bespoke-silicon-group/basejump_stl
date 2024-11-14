86a2eeb80e0f70d92ecc0642f3eb5d444af36368
Paul Gao <gaozihou@uw.edu>
2024-11-13 17:03:50 -0800

    add randomized traffic generator

--- testing/bsg_dmc/tasks.sv
+++ testing/bsg_dmc/tasks.sv
@@ -6,12 +6,17 @@
     `define WRITE_GAP_CYCLES 0
 `endif
 
+`ifndef RANDOM_THRESHOLD
+    `define RANDOM_THRESHOLD 0
+`endif
+
 task ui_write;
   input [ui_mask_width_lp*ui_burst_length_lp-1:0] wmask;
   input  [ui_data_width_p*ui_burst_length_lp-1:0] wdata;
   integer i;
   begin
     for(i=0;i<ui_burst_length_lp;i++) begin
+      while(($urandom_range(0,99) < `RANDOM_THRESHOLD)) @(posedge ui_clk);
       app_wdf_wren <= 1;
       app_wdf_data <= wdata >> (ui_data_width_p * i);
       app_wdf_mask <= wmask >> (ui_mask_width_lp * i);
@@ -32,6 +37,7 @@ task ui_cmd;
   input app_cmd_e             cmd;
   input [ui_addr_width_p-1:0] addr;
   begin
+      while(($urandom_range(0,99) < `RANDOM_THRESHOLD)) repeat(ui_burst_length_lp) @(posedge ui_clk);
       app_en <= 1'b1;
       app_addr <= addr;
       app_cmd <= cmd;
