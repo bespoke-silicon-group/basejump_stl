e9868497fd5fc98d02afa07583612be54ea764d0
Paul Gao <gaozihou@uw.edu>
2024-11-14 12:44:34 -0800

    [BUG FIX] prevent rfifo from overflowing

--- bsg_dmc/bsg_dmc_xilinx_ui_trace_replay.sv
+++ bsg_dmc/bsg_dmc_xilinx_ui_trace_replay.sv
@@ -97,16 +97,7 @@ module bsg_dmc_xilinx_ui_trace_replay
                     );
 
     logic [`BSG_WIDTH(rfifo_depth_p)-1:0] read_credit;
-    bsg_flow_counter #(.els_p(rfifo_depth_p), .count_free_p(1)) fc
-      (.clk_i(clk_i)
-       ,.reset_i(reset_i)
 
-       ,.v_i(app_rd_data_valid_i)
-       ,.ready_param_i(1'b1)
-       ,.yumi_i(read_data_credit)
-
-       ,.count_o(read_credit)
-       );
     wire read_avail = (read_credit >= burst_len_p);
 
     wire trace_is_write = trace_data_lo.app_cmd inside {WP, WR};
@@ -114,6 +105,23 @@ module bsg_dmc_xilinx_ui_trace_replay
     wire trace_is_wdone = trace_data_lo.app_cmd inside {TWT};
     wire trace_is_read = trace_data_lo.app_cmd inside {RP, RD};
     wire trace_is_nop = trace_data_lo.app_cmd inside {TNP};
+
+    // Count down when sending out read command to precent rfifo from overflowing
+    // Assume all reads are burst reads
+    bsg_counter_up_down #( .max_val_p(rfifo_depth_p)
+                         , .init_val_p(rfifo_depth_p)
+                         , .max_step_p(burst_len_p)
+                         ) counter
+
+        ( .clk_i(clk_i)
+        , .reset_i(reset_i)
+
+        , .up_i(`BSG_WIDTH(burst_len_p)'(read_data_credit))
+        , .down_i(`BSG_WIDTH(burst_len_p)'((app_en_o & app_rdy_i & trace_is_read)*burst_len_p))
+
+        , .count_o(read_credit)
+        );
+
     always_comb begin
       state_n = state_r;
 
