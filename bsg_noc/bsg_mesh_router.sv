46fb1fb4d4b053fa32db658248fc2f5119c38aa7
taylor-bsg <taylor-bsg@users.noreply.github.com>
2024-11-14 21:20:15 -0800

    Update bsg_mesh_router.sv

--- bsg_noc/bsg_mesh_router.sv
+++ bsg_noc/bsg_mesh_router.sv
@@ -15,7 +15,7 @@
  *      - 2-D mesh
  *      - 2-D mesh + half ruche x
  *
- *   In theory, we could drop data bits as we switch to different dimensions, but the code would be a little complicated.
+ *   In theory, we could drop data bits (e.g. the X coordinate) as we switch to different dimensions, but the code would be a little complicated.
  */  
 
 `include "bsg_defines.sv"
