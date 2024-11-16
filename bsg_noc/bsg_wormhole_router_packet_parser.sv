2a870972c70c99cf38128c412965035f6fefeca6
taylor-bsg <taylor-bsg@users.noreply.github.com>
2024-11-16 12:55:16 -0800

    Update bsg_wormhole_router_packet_parser.sv

--- bsg_noc/bsg_wormhole_router_packet_parser.sv
+++ bsg_noc/bsg_wormhole_router_packet_parser.sv
@@ -1,3 +1,9 @@
+// this module parses a packet as it comes in from a fifo
+// indicating where the first and last word are
+//
+// a typical use case is to build an endpoint for a wormhole network
+// which might deque the header (first word) 
+
 module bsg_wormhole_router_packet_parser #(parameter `BSG_INV_PARAM(payload_len_bits_p))
    (input clk_i
     , input reset_i
@@ -7,11 +13,11 @@ module bsg_wormhole_router_packet_parser #(parameter `BSG_INV_PARAM(payload_len_
     // this cycle's word was requested to deque
     , input fifo_yumi_i
 
-    // we transferred all of the words on the previous cycle
-    , output last_word_r_o
+    // we completed transferring all of the flits on the previous cycle
+    , output last_flit_r_o
 
-    // detected first word of packet
-    , output first_word_o
+    // detected first flit of packet
+    , output first_flit_o
     );
 
   bsg_wormhole_router_input_control #(.output_dirs_p(1)
