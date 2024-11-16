6605c789c46c1e7be47f65e2c1f56d4d9582fe0a
taylor-bsg <taylor-bsg@users.noreply.github.com>
2024-11-16 12:59:47 -0800

    Update bsg_wormhole_router_packet_parser.sv

--- bsg_noc/bsg_wormhole_router_packet_parser.sv
+++ bsg_noc/bsg_wormhole_router_packet_parser.sv
@@ -1,8 +1,16 @@
-// this module parses a packet as it comes in from a fifo
-// indicating where the first and last word are
+// This module parses a packet as it comes in from a fifo
+// indicating where the first and last flits are
 //
-// a typical use case is to build an endpoint for a wormhole network
-// which might deque the header (first word) 
+// A typical use case is to build an endpoint for a wormhole network
+// which might deque the header (first flit) and put it in a register
+// or fifo, and then deque some data words and process them in a streaming
+// fashion rather than reading out the entire packet into a wide register, such
+// as done in bsg_wormhole_router_adapter_out.
+//
+// Another common pattern is to use a bsg_serial_in_parallel_out_fifo_full or
+// bsg_serial_in_parallel_out_fifo_passthrough if the header info is more than 1 flit
+// or if the data words are going to get paired up for a wide write into a memory or
+// wide processing.
 
 module bsg_wormhole_router_packet_parser #(parameter `BSG_INV_PARAM(payload_len_bits_p))
    (input clk_i
