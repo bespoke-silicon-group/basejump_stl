e7b3691242e506ae1566683e4077305f055222cb
taylor-bsg <taylor-bsg@users.noreply.github.com>
2024-11-16 10:51:56 -0800

    Update bsg_serial_in_parallel_out_full.sv

--- bsg_dataflow/bsg_serial_in_parallel_out_full.sv
+++ bsg_dataflow/bsg_serial_in_parallel_out_full.sv
@@ -10,9 +10,16 @@
  *
  *  Output is only valid, when the output array is fully assembled.
  *
- *  By default, this version has zero bubble.
- *  Minimize hardware by setting use_minimal_buffering_p=1, which introduces
- *  1-cycle bubble for every data array received.
+ *  This module allows for the setting of extra_buffers_p, which allows for more
+ *  than minimum number of buffers. If this is set to 0, there will be 1-cycle bubbles
+ *  between parallel deques. By default, extra_buffers_p is set to 1, which allows 
+ *  for no bubbles during standard propagation. The value may also be set to a greater number,
+ *  to allow for greater amounts of buffering.
+ *
+ *  The module currently does not support extra_buffers_p > els_p.
+ *
+ *  use_minimal_buffering_p is a backwards compatible interface that sets extra_buffers_p to 0
+ *  it is ignored if extra_buffers_p is also set.
  *
  *  @author tommy   02/2019
  *  Paul Gao        06/2019
@@ -28,6 +35,7 @@ module bsg_serial_in_parallel_out_full
    ,parameter `BSG_INV_PARAM(els_p)
   ,parameter hi_to_lo_p              = 0
   ,parameter use_minimal_buffering_p = 0
+  ,parameter extra_buffers_p         = (use_minimal_buffering_p ? 0 : 1)
   )
   
   (input clk_i
@@ -41,7 +49,13 @@ module bsg_serial_in_parallel_out_full
   ,output logic                          v_o
   ,input                                 yumi_i
   );
-  
+
+`ifndef BSG_HIDE_FROM_SYNTHESIS 
+  initial
+   assert (extra_buffers_p <= els_p)
+    else $error("%m bsg_serial_in_parallel_out_full does not support extra_buffers_p (%d) > els_p (%d)",extra_buffers_p, els_p);
+`endif
+    
   localparam lg_els_lp = `BSG_SAFE_CLOG2(els_p);
    
   // If send hi_to_lo, reverse the output data array
@@ -88,7 +102,7 @@ module bsg_serial_in_parallel_out_full
   for (i = 0; i < els_p; i++) 
   begin: fifos
     // Lowest word fifo selection depends on use_minimal_buffering_p
-    if (i == 0 && use_minimal_buffering_p == 0)
+   if (i < extra_buffers_p)  //(i == 0 && use_minimal_buffering_p == 0)
       begin: twofifo
         // Use two element fifo to avoid bubble
         bsg_two_fifo
