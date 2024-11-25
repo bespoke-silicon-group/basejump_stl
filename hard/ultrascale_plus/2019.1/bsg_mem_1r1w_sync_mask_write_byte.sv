cb7871a8d28353a9b0789e51e6c50babea73ea5e
Paul Gao <gaozihou@uw.edu>
2024-11-24 17:37:17 -0800

    fix v -> sv

--- hard/ultrascale_plus/2019.1/bsg_mem_1r1w_sync_mask_write_byte.sv
+++ hard/ultrascale_plus/2019.1/bsg_mem_1r1w_sync_mask_write_byte.sv
@@ -1,7 +1,7 @@
 /*
-* bsg_mem_1rw_sync_mask_write_byte.v
+* bsg_mem_1r1w_sync_mask_write_byte.sv
 *
-* 1-port byte write mask block ram for xilinx ultrascale or ultrascale plus FPGA
+* 2-ports byte write mask block ram for xilinx ultrascale or ultrascale plus FPGA
 * Write mode: No-change | Read mode: No-change
 * Note:
 * There are 2 basic BRAM library primitives, RAMB18E2 and RAMB36E2 in Vivado
@@ -10,6 +10,8 @@
 *
 * Refer to Vivado Design Suite User Guide: Synthesis (UG901), Byte Write Enable (Block RAM)
 * https://docs.xilinx.com/v/u/2019.1-English/ug901-vivado-synthesis
+*
+* reads are synchronous
 *
 *
 * By default, the tool selects which type of RAM to infer based upon heuristics
@@ -20,11 +22,11 @@
 * ├───────────┼──────────┼──────┼─────┼─────┼─────┼──────────────────────────────┤
 * │Distributed│  LUTRAM  │ N/A  │ N/A │ N/A │ N/A │Memory with small depth       │
 * ├───────────┼──────────┼──────┼─────┼─────┼─────┼──────────────────────────────┤
-* │           │ RAMB18E2 │ 18Kb │ W R │  -  │  Y  │Width narrower than 2 bytes   │
+* │           │ RAMB18E2 │ 18Kb │  W  │  R  │  Y  │Width narrower than 2 bytes   │
 * │ Block RAM ├──────────┼──────┼─────┼─────┼─────┼──────────────────────────────┤
-* │           │ RAMB36E2 │ 36Kb │ W R │  -  │  Y  │Width narrower than 4 bytes   │
+* │           │ RAMB36E2 │ 36Kb │  W  │  R  │  Y  │Width narrower than 4 bytes   │
 * ├───────────┼──────────┼──────┼─────┼─────┼─────┼──────────────────────────────┤
-* │ Ultra RAM │ URAM288  │288Kb │ W R │  -  │  Y  │Width wider than 4 bytes      │
+* │ Ultra RAM │ URAM288  │288Kb │  W  │  R  │  Y  │Width wider than 4 bytes      │
 * └───────────┴──────────┴──────┴─────┴─────┴─────┴──────────────────────────────┘
 *
 * To force the RAM into a specific type, use the RAM_STYLE attribute to tell
@@ -35,36 +37,44 @@
 *
 */
 
-`include "bsg_defines.v"
+`include "bsg_defines.sv"
 
-module bsg_mem_1rw_sync_mask_write_byte #(parameter `BSG_INV_PARAM(els_p)
-                                          ,parameter addr_width_lp = `BSG_SAFE_CLOG2(els_p)
-
-                                          ,parameter `BSG_INV_PARAM(data_width_p )
-                                          ,parameter latch_last_read_p=0
-                                          ,parameter write_mask_width_lp = data_width_p>>3
-                                          ,parameter enable_clock_gating_p=0
+module bsg_mem_1r1w_sync_mask_write_byte #(parameter `BSG_INV_PARAM(width_p)
+                                         , parameter `BSG_INV_PARAM(els_p)
+                                         // semantics of "1" are write occurs, then read
+                                         // the other semantics cannot be simulated on a hardened, non-simultaneous
+                                         // 1r1w SRAM without changing timing.
+                                         // fixme: change to write_then_read_same_addr_p
+                                         , parameter read_write_same_addr_p=0
+                                         , parameter latch_last_read_p=0 
+                                         , parameter addr_width_lp=`BSG_SAFE_CLOG2(els_p)
+                                         , parameter harden_p=0
+                                         , parameter disable_collision_warning_p=0
+                                         , parameter write_mask_width_lp = width_p>>3
+                                         , parameter enable_clock_gating_p=0
                                          )
-  ( input clk_i
-   ,input reset_i
+   (input   clk_i
+    , input reset_i
 
-   ,input v_i
-   ,input w_i
-
-   ,input [addr_width_lp-1:0]       addr_i
-   ,input [`BSG_SAFE_MINUS(data_width_p, 1):0]        data_i
+    , input                     w_v_i
     // for each bit set in the mask, a byte is written
-   ,input [`BSG_SAFE_MINUS(write_mask_width_lp, 1):0] write_mask_i
+   ,input [`BSG_SAFE_MINUS(write_mask_width_lp, 1):0] w_mask_i
+    , input [addr_width_lp-1:0] w_addr_i
+    , input [`BSG_SAFE_MINUS(width_p, 1):0]       w_data_i
 
-   ,output logic [`BSG_SAFE_MINUS(data_width_p, 1):0] data_o
-  );
+    , input                      r_v_i
+    , input [addr_width_lp-1:0]  r_addr_i
+
+    , output logic [`BSG_SAFE_MINUS(width_p, 1):0] r_data_o
+    );
+
 
   wire unused = reset_i;
 
-  if (data_width_p == 0)
+  if (width_p == 0)
   begin: z
-    wire unused0 = &{clk_i, v_i, w_i, addr_i, data_i, write_mask_i};
-    assign data_o = '0;
+    wire unused0 = &{clk_i, w_v_i, w_mask_i, w_addr_i, w_data_i, r_v_i, r_addr_i};
+    assign r_data_o = '0;
   end
   else
   begin: nz
@@ -77,8 +87,7 @@ module bsg_mem_1rw_sync_mask_write_byte #(parameter `BSG_INV_PARAM(els_p)
    * BRAM and URAM inference based on depth and width parameterizations.
    */
 
-    logic [data_width_p-1:0] mem [els_p-1:0];
-    logic [write_mask_width_lp-1:0] write_enable;
+    logic [width_p-1:0] mem [els_p-1:0];
 
   /* In order to synthesize into a byte masked BRAM/URAM, follow instruction in
    * Xilinx doc "UG901", Section "Byte Write Enable (Block RAM)"
@@ -88,19 +97,18 @@ module bsg_mem_1rw_sync_mask_write_byte #(parameter `BSG_INV_PARAM(els_p)
 
     for(genvar i = 0; i < write_mask_width_lp; i++)
       begin: write
-        assign write_enable[i] = w_i & write_mask_i[i];
         always_ff @(posedge clk_i)
-            if(v_i)
-                if(write_enable[i])
-                    mem[addr_i][i*8+:8] <= data_i[i*8+:8];
+            if(w_v_i)
+                if(w_mask_i[i])
+                    mem[w_addr_i][i*8+:8] <= w_data_i[i*8+:8];
       end
 
     always_ff @(posedge clk_i)
-        if(v_i)
-            if(~|write_enable)
-                data_o <= mem[addr_i];
+        if(r_v_i)
+            r_data_o <= mem[r_addr_i];
 
   end
 
 endmodule
-`BSG_ABSTRACT_MODULE(bsg_mem_1rw_sync_mask_write_byte)
+
+`BSG_ABSTRACT_MODULE(bsg_mem_1r1w_sync_mask_write_byte)
