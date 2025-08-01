0221a44dfe781aab4b753e0c8ceaa7ace475e79e
taylor-bsg <prof.taylor@gmail.com>
2025-08-01 12:48:31 -0700

    add skip parameter

--- bsg_dataflow/bsg_pipeline_stall_collapse.sv
+++ bsg_dataflow/bsg_pipeline_stall_collapse.sv
@@ -2,8 +2,9 @@
 
 module bsg_pipeline_stall_collapse #(
 			     `BSG_INV_PARAM(stages_p)
-			     parameter int widths_p[stages_p-1:0] = '{default: 1}
-			     parameter int max_width_p = widths_p.max()
+			     , parameter int widths_p[stages_p-1:0] = '{default: 1}
+			     , parameter int max_width_p = widths_p.max()
+		             , parameter logic [stages_p-1:0] skip_p = '0
 			     )
    (input clk_i
     ,input reset_i
@@ -35,7 +36,7 @@ module bsg_pipeline_stall_collapse #(
    assign ready_o = ready_adj[0];
    
    for (i = 0; i < stages_p; i++)
-     begin: stg
+     begin: s
 	// enable register if we are shifting, or if there
 	// was nothing in the register to begin with
 	
@@ -48,24 +49,32 @@ module bsg_pipeline_stall_collapse #(
 
 	// enable only if we are writing data into a register
 	wire shift_data = v_li[i] & ready_adj[i];	
- 
-	bsg_dff_reset_en #(.width_p(1))
-	(.clk_i(clk_i)
-	 ,.reset_i(reset_i)
-	 ,.en_i(shift_v)
-	 ,.data_i(v_li[i])
-	 ,.data_o(v_r_lo[i])
-	 );
-		  
-	// note: if v_r_lo[i] is invalid, data is don't care
-	bsg_dff_en #(.width_p(widths_p[i]))
-	(.clk_i(clk_i)
-	 ,.reset_i(reset_i)
 
-	 ,.en_i   (shift_data)
-	 ,.data_i (data_i[i][widths_p[i]-1:0])
-	 ,.data_o (data_o[i][widths_p[i]-1:0])
-	 );
+	if (skip_p[i]) 
+        begin : kip
+	   assign v_r_lo[i] = v_li[i];
+	   assign data_o[i] = data_i[i];
+	end
+	else
+	  begin : tage
+	     bsg_dff_reset_en #(.width_p(1))
+	     (.clk_i(clk_i)
+	      ,.reset_i(reset_i)
+	      ,.en_i(shift_v)
+	      ,.data_i(v_li[i])
+	      ,.data_o(v_r_lo[i])
+	      );
+		  
+	     // note: if v_r_lo[i] is invalid, data is don't care
+	     bsg_dff_en #(.width_p(widths_p[i]))
+	     (.clk_i(clk_i)
+	      ,.reset_i(reset_i)
+
+	      ,.en_i   (shift_data)
+	      ,.data_i (data_i[i][widths_p[i]-1:0])
+	      ,.data_o (data_o[i][widths_p[i]-1:0])
+	      );
+	  end
      end
 
 endmodule
