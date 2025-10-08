2df6f09dcada3f1e74b500127c1ee49b28b87a55
Michael Bedford Taylor <taylor-bsg@users.noreply.github.com>
2025-10-08 07:14:15 -0700

    Enhance comments for output processing guidance
    Added comments regarding output processing and recommended usage.

--- bsg_dataflow/bsg_round_robin_n_to_1.sv
+++ bsg_dataflow/bsg_round_robin_n_to_1.sv
@@ -14,6 +14,8 @@
 // watch out for: if you plan to sit on the output, processing, for a number of 
 // cycles before dequeing, the arrival of a new item on one of the
 // input channels can cause the value to change, resulting in an error.
+//
+// for this use case, an output bsg_one_fifo_bypass is recommended
 
 `include "bsg_defines.sv"
 
