43b510a5729e422932964fac7c3e783314288732
Michael Bedford Taylor <taylor-bsg@users.noreply.github.com>
2025-11-13 13:06:05 -0800

    Remove 'en' signal from bsg_tag_s structure
    Removed the 'en' signal from the bsg_tag_s structure.

--- bsg_tag/bsg_tag_pkg.sv
+++ bsg_tag/bsg_tag_pkg.sv
@@ -5,8 +5,6 @@ package bsg_tag_pkg;
       logic op;    //   1   D   shift D
       logic param; //   0   0   nop (also "send", immediately after shift operation)
                    //   0   1   reset
-
-      logic en;    // this signal disables thru-transmit of new values
 } bsg_tag_s;
 
 
